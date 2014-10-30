cbe6d49c35089bb8f76dc887bba04ad7e6b11803
Tony Curtis <tonyc@uh.edu>
2014-10-29 22:00:29 -0500

    Use better namespace for functions

--- contrib/ucx-style.el
+++ contrib/ucx-style.el
@@ -1,29 +1,29 @@
 ;; Usage:
 ;; add our tweaks to c-mode
 ;; The basic indentation
-;;   (add-hook 'c-mode-hook 'tc-indent-config)
+;;   (add-hook 'c-mode-hook 'ucx-indent-config)
 ;;
 ;; Warn when we're getting close to exceeding line length
-;;  (add-hook 'c-mode-hook 'tc-mark-80-column-rule)
+;;  (add-hook 'c-mode-hook 'ucx-mark-80-column-rule)
 ;;
 ;; On-the-fly indentation of lines
 ;;   (add-hook 'c-mode-hook 'electric-indent-mode)
 ;;
 ;; Indent opened lines immediately rather than TABbing across
-;;   (add-hook 'c-mode-hook 'tc-c-nl-indent)
+;;   (add-hook 'c-mode-hook 'ucx-c-nl-indent)
 ;;
 
 (require 'cc-mode)
 (require 'whitespace)
 
-(defun tc-mark-80-column-rule ()
+(defun ucx-mark-80-column-rule ()
   "Highlight lines that are about to get too long"
   (setq whitespace-line-column 78)
   (setq whitespace-style '(face empty tabs lines-tail trailing))
   (whitespace-mode)
   )
 
-(defun tc-indent-config ()
+(defun ucx-indent-config ()
   "Set the required layout"
   (setq c-basic-offset 4
 	tab-width 4
@@ -31,7 +31,7 @@
   )
 
 ;; add our tweaks to c-mode
-(defun tc-c-nl-indent ()
+(defun ucx-c-nl-indent ()
   "Make newline also immediately indent next line"
   (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
   )
