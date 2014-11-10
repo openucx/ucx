60a68637ebf1b54a66a5f67b12001d080baccf97
yosefe <yosefe@mellanox.com>
2014-11-10 10:01:24 +0200

    ALL: Fix copyright notice to 2014

--- src/ucs/datastruct/list.h
+++ src/ucs/datastruct/list.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) Mellanox Technologies Ltd. 2001-2011.  ALL RIGHTS RESERVED.
+* Copyright (C) Mellanox Technologies Ltd. 2001-2014.  ALL RIGHTS RESERVED.
 *
 * $COPYRIGHT$
 * $HEADER$
