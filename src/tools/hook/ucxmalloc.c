83350f7a68f3e13f26bc403b6c40b768221a0a14
yosefe <yosefe@mellanox.com>
2015-01-21 10:38:34 +0200

    TOOLS: Add skeleton for libucxmalloc helper library.
    This library is intended to intercept malloc() calls in order to
    make user-allocated memory more HPC friendly.

---
+++ src/tools/hook/ucxmalloc.c
@@ -0,0 +1,17 @@
+/**
+* Copyright (C) Mellanox Technologies Ltd. 2001-2015.  ALL RIGHTS RESERVED.
+*
+* $COPYRIGHT$
+* $HEADER$
+*/
+
+#include <ucs/sys/compiler.h>
+
+
+static void UCS_F_CTOR ucxmalloc_init()
+{
+}
+
+static void UCS_F_DTOR ucxmalloc_fini()
+{
+}
