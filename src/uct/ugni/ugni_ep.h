25dc3280fde794c3d083750834892793794a0666
yosefe <yosefe@mellanox.com>
2015-03-25 19:58:36 +0200

    SYSV/UGNI: Fix copyright to 2015.

--- src/uct/ugni/ugni_ep.h
+++ src/uct/ugni/ugni_ep.h
@@ -1,6 +1,6 @@
 /**
 * Copyright (C) UT-Battelle, LLC. 2015. ALL RIGHTS RESERVED.
-* Copyright (C) Mellanox Technologies Ltd. 2001-2014.  ALL RIGHTS RESERVED.
+* Copyright (C) Mellanox Technologies Ltd. 2001-2015.  ALL RIGHTS RESERVED.
 * $COPYRIGHT$
 * $HEADER$
 */
