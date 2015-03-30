cb4a0b7783d9e5da058da9e92d55b33e200edb89
Alex Mikheev <alexm@mellanox.com>
2015-03-30 16:23:16 +0300

    GTEST/UD: increase time of progress loop

--- test/gtest/uct/test_ud.cc
+++ test/gtest/uct/test_ud.cc
@@ -34,7 +34,7 @@ public:
     }
 
     void short_progress_loop() {
-        ucs_time_t end_time = ucs_get_time() + ucs_time_from_msec(1.0);
+        ucs_time_t end_time = ucs_get_time() + ucs_time_from_msec(10.0);
         while (ucs_get_time() < end_time) {
             progress();
         }
