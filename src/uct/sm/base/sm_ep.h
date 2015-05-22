5d1b0757783ea6c5bc8a0bda2279f87f3b4edb76
Graham Lopez <lopezmg@ornl.gov>
2015-05-22 09:55:28 -0400

    COMP. API: proposed updates compile. tests do not compile

--- src/uct/sm/base/sm_ep.h
+++ src/uct/sm/base/sm_ep.h
@@ -45,25 +45,27 @@ ucs_status_t uct_sm_ep_atomic_add64(uct_ep_h tl_ep, uint64_t add,
                                     uint64_t remote_addr, uct_rkey_t rkey);
 ucs_status_t uct_sm_ep_atomic_fadd64(uct_ep_h tl_ep, uint64_t add,
                                      uint64_t remote_addr, uct_rkey_t rkey,
-                                     uct_completion_t *comp);
+                                     uint64_t *result, uct_completion_t *comp);
 ucs_status_t uct_sm_ep_atomic_swap64(uct_ep_h tl_ep, uint64_t swap,
                                      uint64_t remote_addr, uct_rkey_t rkey,
-                                     uct_completion_t *comp);
+                                     uint64_t *result, uct_completion_t *comp);
 ucs_status_t uct_sm_ep_atomic_cswap64(uct_ep_h tl_ep, uint64_t compare, 
                                       uint64_t swap, uint64_t remote_addr, 
-                                      uct_rkey_t rkey, uct_completion_t *comp);
+                                      uct_rkey_t rkey, uint64_t *result,
+                                      uct_completion_t *comp);
 ucs_status_t uct_sm_ep_atomic_add32(uct_ep_h tl_ep, uint32_t add,
                                     uint64_t remote_addr, uct_rkey_t rkey);
 ucs_status_t uct_sm_ep_atomic_fadd32(uct_ep_h tl_ep, uint32_t add,
                                      uint64_t remote_addr, uct_rkey_t rkey,
-                                     uct_completion_t *comp);
+                                     uint32_t *result, uct_completion_t *comp);
 ucs_status_t uct_sm_ep_atomic_swap32(uct_ep_h tl_ep, uint32_t swap,
                                      uint64_t remote_addr, uct_rkey_t rkey,
-                                     uct_completion_t *comp);
+                                     uint32_t *result, uct_completion_t *comp);
 ucs_status_t uct_sm_ep_atomic_cswap32(uct_ep_h tl_ep, uint32_t compare, 
                                       uint32_t swap, uint64_t remote_addr, 
-                                      uct_rkey_t rkey, uct_completion_t *comp);
-ucs_status_t uct_sm_ep_get_bcopy(uct_ep_h tl_ep, size_t length, 
+                                      uct_rkey_t rkey, uint32_t *result,
+                                      uct_completion_t *comp);
+ucs_status_t uct_sm_ep_get_bcopy(uct_ep_h tl_ep, void *buffer, size_t length, 
                                  uint64_t remote_addr, uct_rkey_t rkey, 
                                  uct_completion_t *comp);
 ucs_status_t uct_sm_ep_get_zcopy(uct_ep_h tl_ep, void *buffer, size_t length,
