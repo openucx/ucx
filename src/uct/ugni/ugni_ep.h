1b489577516c2fd209de63ec3c93817b3e93821c
Pavel Shamis (Pasha) <shamisp@ornl.gov>
2015-06-15 23:51:29 -0400

    UGNI: Updating function signatures

--- src/uct/ugni/ugni_ep.h
+++ src/uct/ugni/ugni_ep.h
@@ -41,14 +41,15 @@ ucs_status_t uct_ugni_ep_atomic_add64(uct_ep_h tl_ep, uint64_t add,
                                       uint64_t remote_addr, uct_rkey_t rkey);
 ucs_status_t uct_ugni_ep_atomic_fadd64(uct_ep_h tl_ep, uint64_t add,
                                        uint64_t remote_addr, uct_rkey_t rkey,
-                                       uct_completion_t *comp);
+                                       uint64_t *result, uct_completion_t *comp);
 ucs_status_t uct_ugni_ep_atomic_swap64(uct_ep_h tl_ep, uint64_t swap,
                                        uint64_t remote_addr, uct_rkey_t rkey,
-                                       uct_completion_t *comp);
+                                       uint64_t *result, uct_completion_t *comp);
 ucs_status_t uct_ugni_ep_atomic_cswap64(uct_ep_h tl_ep, uint64_t compare, uint64_t swap,
                                         uint64_t remote_addr, uct_rkey_t rkey,
-                                        uct_completion_t *comp);
-ucs_status_t uct_ugni_ep_get_bcopy(uct_ep_h tl_ep, size_t length, uint64_t remote_addr,
+                                        uint64_t *result, uct_completion_t *comp);
+ucs_status_t uct_ugni_ep_get_bcopy(uct_ep_h tl_ep, void *buffer, 
+                                   size_t length, uint64_t remote_addr,
                                    uct_rkey_t rkey, uct_completion_t *comp);
 ucs_status_t uct_ugni_ep_get_zcopy(uct_ep_h tl_ep, void *buffer, size_t length,
                                    uct_mem_h memh, uint64_t remote_addr,
