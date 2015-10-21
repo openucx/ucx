1615101dc164c5a35e4b0a8034b5091e08668d81
Matt Baker <bakermb@ornl.gov>
2015-10-21 19:15:23 -0400

    Add jemalloc

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 rm -rf autom4te.cache
-mkdir -p config/m4 config/aux 
-autoreconf -v --install || exit 1
+mkdir -p config/m4 config/aux external/jemalloc
+
+pushd external/jemalloc
+autoconf
+popd
+
+autoreconf -v --install --no-recursive || exit 1
 rm -rf autom4te.cache
