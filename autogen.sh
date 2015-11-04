57cd998b4e2fba9e285fa2319d3bfd5b23f3c4d0
Matt Baker <bakermb@ornl.gov>
2015-11-04 17:19:34 -0500

    autogen.sh will now pull in external repositories defined as git submodules if git is present.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,15 @@
 rm -rf autom4te.cache
 mkdir -p config/m4 config/aux external/jemalloc
 
+GIT_FOUND=1
+command git --help 2>&1 > /dev/null || { echo "Git not found, not retrieving external packages"; export GIT_FOUND=0; }
+
+if [ $GIT_FOUND -eq 1 ]
+then
+git submodule init
+git submodule update
+fi
+
 if [ -e external/jemalloc/configure.ac ]
 then
 pushd external/jemalloc
