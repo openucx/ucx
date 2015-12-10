6fe20a20c3e29a675b036c63f129b38bc1c72f03
Matthew Baker <bakermb@ornl.gov>
2015-12-10 10:46:17 -0500

    Fix up indentations and clean up messages and comments.

--- autogen.sh
+++ autogen.sh
@@ -5,20 +5,21 @@ mkdir -p config/m4 config/aux external/jemalloc
 
 GIT_FOUND=1
 command git --help 2>&1 > /dev/null || { \
-    echo "Git not found, will not retrieve jemalloc"; \
-    GIT_FOUND=0; }
+    echo "Git not found, will not retrieve external sub-projects"; \
+    GIT_FOUND=0; \
+}
 
 if [ $GIT_FOUND -eq 1 -a -d .git ]
 then
-git submodule init
-git submodule update
+    git submodule init
+    git submodule update
 fi
 
 if [ -e external/jemalloc/configure.ac ]
 then
-pushd external/jemalloc
-autoconf || exit 2
-popd
+    pushd external/jemalloc
+    autoconf || exit 2
+    popd
 fi
 
 autoreconf -v --install --no-recursive || exit 1
