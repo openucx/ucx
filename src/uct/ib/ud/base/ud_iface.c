083c9d9ab0fe5de2e1e531f971a16b5b712d08f9
Alex Mikheev <alexm@mellanox.com>
2016-01-05 11:41:34 +0200

    IB/UD: temporarily returned  UCT_IFACE_FLAG_AM_THREAD_SINGLE
    to prevent ud being used for connection establishment
    
    flag will be removed when ud disconnect protocol is implemented
    removed unrelevant comment in cm

--- src/uct/ib/ud/base/ud_iface.c
+++ src/uct/ib/ud/base/ud_iface.c
@@ -431,7 +431,10 @@ void uct_ud_iface_query(uct_ud_iface_t *iface, uct_iface_attr_t *iface_attr)
                                         UCT_IFACE_FLAG_AM_BCOPY | 
                                         UCT_IFACE_FLAG_CONNECT_TO_EP |
                                         UCT_IFACE_FLAG_CONNECT_TO_IFACE |
-                                        UCT_IFACE_FLAG_PENDING;
+                                        UCT_IFACE_FLAG_PENDING |
+                                        /* TODO: remove once ud disconnect is fixed and ud is a default 
+                                         * tl for connection establishment */
+                                        UCT_IFACE_FLAG_AM_THREAD_SINGLE; 
 
     iface_attr->cap.am.max_short      = iface->config.max_inline - sizeof(uct_ud_neth_t);
     iface_attr->cap.am.max_bcopy      = mtu - sizeof(uct_ud_neth_t);
