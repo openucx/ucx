547b8e54daa36b6cba7632a1b0e39cebeb8a12d6
Eugene Voronov <eugene@mellanox.com>
2016-01-31 09:15:09 +0200

    CONTRIB: added gtest execution on PPC servers in Jenkins script

--- contrib/test_jenkins.sh
+++ contrib/test_jenkins.sh
@@ -58,7 +58,7 @@ echo "Build without IB verbs"
 ../contrib/configure-release --without-verbs
 make $make_opt
 
-if [ -n "$JENKINS_PPC_SERVER" ]; then
+if [[ -n "$JENKINS_PPC_SERVER" ]] && [[ -n "$JENKINS_RUN_TESTS" ]] ; then
     # Set CPU affinity to 2 cores, for performance tests
     if [ -n "$EXECUTOR_NUMBER" ]; then
         AFFINITY="taskset -c $(( 2 * EXECUTOR_NUMBER ))","$(( 2 * EXECUTOR_NUMBER + 1))"
@@ -134,79 +134,6 @@ if [ -n "$JENKINS_PPC_SERVER" ]; then
 fi
 
 if [ -n "$JENKINS_RUN_TESTS" ]; then
-    # Set CPU affinity to 2 cores, for performance tests
-    if [ -n "$EXECUTOR_NUMBER" ]; then
-        AFFINITY="taskset -c $(( 2 * EXECUTOR_NUMBER ))","$(( 2 * EXECUTOR_NUMBER + 1))"
-        TIMEOUT="timeout 80m"
-    else
-        AFFINITY=""
-        TIMEOUT=""
-    fi
-
-    echo "Build gtest"
-    module load hpcx-gcc
-    unset LD_LIBRARY_PATH # Prevent out tests from using installed UCX libraries
-
-    make $make_opt clean
-
-    # todo: check in -devel mode as well
-    ../contrib/configure-release --with-mpi --prefix=$ucx_inst
-    make $make_opt install
-
-    ucx_inst_ptest=$ucx_inst/share/ucx/perftest
-
-    # hack for perftest, no way to override params used in batch
-    # todo: fix in perftest
-    sed -s 's,-n [0-9]*,-n 1000,g' $ucx_inst_ptest/msg_pow2 | sort -R > $ucx_inst_ptest/msg_pow2_short
-    cat $ucx_inst_ptest/test_types | sort -R > $ucx_inst_ptest/test_types_short
-
-    opt_perftest_common="-b $ucx_inst_ptest/test_types_short -b $ucx_inst_ptest/msg_pow2_short -w 1"
-
-    for dev in $(ibstat -l); do
-        hca="${dev}:1"
-
-        if [[ $dev =~ .*mlx5.* ]]; then
-            opt_perftest="$opt_perftest_common -b $ucx_inst_ptest/transports"
-        else
-            opt_perftest="$opt_perftest_common -x rc"
-        fi
-
-        echo Running ucx_perf kit on $hca
-        mpirun -np 2 -mca pml ob1 -mca btl sm,self $AFFINITY $ucx_inst/bin/ucx_perftest -d $hca $opt_perftest
-
-        # todo: add csv generation
-
-    done
-
-    echo "Running MPI tests"
-    mpirun -np 1 -mca pml ob1 -mca btl sm,self -mca coll ^hcoll,ml $AFFINITY ./test/mpi/test_memhooks
-
-    module unload hpcx-gcc
-
-    module load intel/ics
-    ../contrib/configure-devel --prefix=$ucx_inst CC=icc
-    make $make_opt clean
-    make $make_opt all
-    make $make_opt distclean
-    module unload intel/ics
-
-    ../contrib/configure-devel --prefix=$ucx_inst
-    make $make_opt all
-
-    echo "Running ucx_info"
-    $AFFINITY $TIMEOUT ./src/tools/info/ucx_info -f -c -v -y -d -b
-
-    export GTEST_RANDOM_SEED=0
-    export GTEST_SHUFFLE=1
-    export GTEST_TAP=2
-
-    export GTEST_REPORT_DIR=$WORKSPACE/reports/tap
-    mkdir -p $GTEST_REPORT_DIR
-
-    echo "Running unit tests"
-    $AFFINITY $TIMEOUT make -C test/gtest test UCS_HANDLE_ERRORS=bt
-    (cd test/gtest && rename .tap _gtest.tap *.tap && mv *.tap $GTEST_REPORT_DIR)
-
     echo "Running valgrind tests"
     module load tools/valgrind-latest
     $AFFINITY $TIMEOUT make -C test/gtest UCS_HANDLE_ERRORS=bt VALGRIND_EXTRA_ARGS="--xml=yes --xml-file=valgrind.xml --child-silent-after-fork=yes" test_valgrind
