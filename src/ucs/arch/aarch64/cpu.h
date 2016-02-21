77b26c5a32ad63bd028ddff5a2bfc5b19cc71c33
Pavel Shamis (Pasha) <pasharesearch@gmail.com>
2016-02-21 09:33:39 -0500

    UCS: Adding support for ARM64
    Signed-off-by: Pavel Shamis (Pasha) <pasharesearch@gmail.com>

--- src/ucs/arch/aarch64/cpu.h
+++ src/ucs/arch/aarch64/cpu.h
@@ -1,5 +1,6 @@
 /**
 * Copyright (C) Mellanox Technologies Ltd. 2001-2015.  ALL RIGHTS RESERVED.
+* Copyright (C) ARM Ltd. 2016.  ALL RIGHTS RESERVED.
 *
 * See file LICENSE for terms.
 */
@@ -8,6 +9,8 @@
 #define UCS_AARCH64_CPU_H_
 
 #include <ucs/debug/log.h>
+#include <time.h>
+#include <sys/times.h>
 
 
 #define UCS_SYS_CACHE_LINE_SIZE    64
@@ -15,29 +18,32 @@
 /**
  * Assume the worst - weak memory ordering.
  */
-#define ucs_memory_bus_fence()        ucs_fatal("unimplemented");
-#define ucs_memory_bus_store_fence()  ucs_memory_bus_fence()
-#define ucs_memory_bus_load_fence()   ucs_memory_bus_fence()
-#define ucs_memory_cpu_fence()        ucs_memory_bus_fence()
-#define ucs_memory_cpu_store_fence()  ucs_memory_bus_fence()
-#define ucs_memory_cpu_load_fence()   ucs_memory_bus_fence()
+#define ucs_memory_bus_fence()        asm volatile ("dsb sy" ::: "memory");
+#define ucs_memory_bus_store_fence()  asm volatile ("dsb st" ::: "memory");
+#define ucs_memory_bus_load_fence()   asm volatile ("dsb ld" ::: "memory");
+#define ucs_memory_cpu_fence()        asm volatile ("dmb ish" ::: "memory");
+#define ucs_memory_cpu_store_fence()  asm volatile ("dmb ishst" ::: "memory");
+#define ucs_memory_cpu_load_fence()   asm volatile ("dmb ishld" ::: "memory");
 
 
-static inline clock_t ucs_arch_read_hres_clock(void)
+static inline uint64_t ucs_arch_read_hres_clock(void)
 {
-    struct tms accurate_clock;
-    times(&accurate_clock);
-    return accurate_clock.tms_utime + accurate_clock.tms_stime;
+    uint64_t ticks;
+    asm volatile("isb" : : : "memory");
+    asm volatile("mrs %0, cntvct_el0" : "=r" (ticks));
+    return ticks;
 }
 
 static inline double ucs_arch_get_clocks_per_sec()
 {
-    return CLOCKS_PER_SEC;
+    uint32_t freq;
+    asm volatile("mrs %0, cntfrq_el0" : "=r" (freq));
+    return (double) freq;
 }
 
 static inline ucs_cpu_model_t ucs_arch_get_cpu_model()
 {
-    return UCS_CPU_MODEL_UNKNOWN;
+    return UCS_CPU_MODEL_ARM_AARCH64;
 }
 
 static inline int ucs_arch_get_cpu_flag()
