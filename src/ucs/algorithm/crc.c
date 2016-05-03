7d36846aee4d56c050fea61f1a1f1224a3e6653f
yosefe <yosefe@mellanox.com>
2016-05-03 19:14:11 +0300

    UCS: Add crc16 algorithm.

---
+++ src/ucs/algorithm/crc.c
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) Mellanox Technologies Ltd. 2001-2016.  ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#include "crc.h"
+
+#include <string.h>
+
+
+uint16_t ucs_crc16(const void *buffer, size_t size)
+{
+    const uint8_t *p;
+    uint16_t result;
+    uint8_t data;
+    int bit;
+
+    if (size == 0) {
+        return 0;
+    }
+
+    result = -1;
+    for (p = buffer; p < (const uint8_t*)(buffer + size); ++p) {
+        data = *p;
+        for (bit = 0; bit < 8; ++bit) {
+            result >>= 1;
+            if ((result ^ data) & 1) {
+                result = result ^ 0x8048;
+            }
+            data >>= 1;
+        }
+    };
+
+    result = (result << 8) | ((result >> 8) & 0xff);
+    return ~result;
+}
+
+uint16_t ucs_crc16_string(const char *s)
+{
+    return ucs_crc16((char*)s, strlen(s));
+}
---
+++ src/ucs/algorithm/crc.h
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) Mellanox Technologies Ltd. 2001-2016.  ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#ifndef UCS_ALGORITHM_CRC_H_
+#define UCS_ALGORITHM_CRC_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * Calculate CRC16 of an arbitrary buffer.
+ *
+ * @param [in]  buffer  Buffer to compute crc for.
+ * @param [in]  size    Buffer size.
+ *
+ * @return crc16() function of the buffer.
+ */
+uint16_t ucs_crc16(const void *buffer, size_t size);
+
+
+/**
+ * Calculate CRC16 of a NULL-terminated string.
+ */
+uint16_t ucs_crc16_string(const char *s);
+
+
+#endif
