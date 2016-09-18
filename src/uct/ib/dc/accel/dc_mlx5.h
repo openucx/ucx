6a13c043272ea836274d5e44e18cd3eb767e2077
Yossi Itigin <yosefe@mellanox.com>
2016-09-18 18:04:10 +0300

    UCT/DC: Use inline arrays for DCIs.

--- src/uct/ib/dc/accel/dc_mlx5.h
+++ src/uct/ib/dc/accel/dc_mlx5.h
@@ -11,15 +11,18 @@
 #include <uct/ib/dc/base/dc_ep.h>
 #include <uct/ib/rc/accel/rc_mlx5_common.h>
 
+
 typedef struct uct_dc_mlx5_iface { 
-    uct_dc_iface_t              super;
-    uct_rc_mlx5_iface_common_t  mlx5_common;
-    uct_ib_mlx5_txwq_t         *dci_wqs;
+    uct_dc_iface_t                 super;
+    uct_rc_mlx5_iface_common_t     mlx5_common;
+    uct_ib_mlx5_txwq_t             dci_wqs[UCT_DC_IFACE_MAX_DCIS];
 } uct_dc_mlx5_iface_t;
 
+
 typedef struct uct_dc_mlx5_ep {
-    uct_dc_ep_t          super;
-    struct mlx5_wqe_av   av;
+    uct_dc_ep_t                    super;
+    struct mlx5_wqe_av             av;
 } uct_dc_mlx5_ep_t;
+
 
 #endif
