eb3825d94c2d14cd86af82fb9ea27da260274667
Sergey Shalnov <sergeysh@mellanox.com>
2016-10-01 09:01:20 +0300

    UCT/SM/KNEM: Make support of zero copy APIs in sm_knem

--- src/uct/sm/knem/knem_ep.c
+++ src/uct/sm/knem/knem_ep.c
@@ -8,6 +8,7 @@
 
 #include "knem_ep.h"
 #include "knem_md.h"
+#include <uct/sm/base/sm_iface.h>
 #include <ucs/debug/log.h>
 
 static UCS_CLASS_INIT_FUNC(uct_knem_ep_t, uct_iface_t *tl_iface,
@@ -40,23 +41,32 @@ UCS_CLASS_DEFINE_DELETE_FUNC(uct_knem_ep_t, uct_ep_t);
         return UCS_OK;                                  \
     }
 
-static inline ucs_status_t uct_knem_rma(uct_ep_h tl_ep, const void *buffer,
-                                        size_t length,  uint64_t remote_addr,
+static inline ucs_status_t uct_knem_rma(uct_ep_h tl_ep, const uct_iov_t *iov,
+                                        size_t iovcnt, uint64_t remote_addr,
                                         uct_knem_key_t *key, int write)
 {
     struct knem_cmd_inline_copy icopy;
-    struct knem_cmd_param_iovec knem_iov[1];
+    struct knem_cmd_param_iovec knem_iov[UCT_SM_MAX_IOV];
     uct_knem_iface_t *knem_iface = ucs_derived_of(tl_ep->iface, uct_knem_iface_t);
     int knem_fd = knem_iface->knem_md->knem_fd;
     int rc;
+    size_t iov_it;
+    size_t knem_iov_it = 0;
 
-    UCT_KNEM_ZERO_LENGTH_POST(length);
+    for (iov_it = 0; iov_it < ucs_min(UCT_SM_MAX_IOV, iovcnt); ++iov_it) {
+        knem_iov[knem_iov_it].base = (uintptr_t)iov[iov_it].buffer;
+        knem_iov[knem_iov_it].len = uct_iov_get_length(iov + iov_it);
+        if (knem_iov[knem_iov_it].len) {
+            ++knem_iov_it;
+        } else {
+            continue; /* Skip zero length buffers */
+        }
+    }
 
-    knem_iov[0].base = (uintptr_t)buffer;
-    knem_iov[0].len = length;
+    UCT_KNEM_ZERO_LENGTH_POST(knem_iov_it);
 
-    icopy.local_iovec_array = (uintptr_t) &knem_iov[0];
-    icopy.local_iovec_nr = 1;
+    icopy.local_iovec_array = (uintptr_t) knem_iov;
+    icopy.local_iovec_nr = knem_iov_it;
     icopy.remote_cookie = key->cookie;
     ucs_assert(remote_addr >= key->address);
     icopy.remote_offset = remote_addr - key->address;
@@ -77,7 +87,7 @@ static inline ucs_status_t uct_knem_rma(uct_ep_h tl_ep, const void *buffer,
 
     uct_knem_trace_data(remote_addr, (uintptr_t)key, "%s [length %zu]",
                         write?"PUT_ZCOPY":"GET_ZCOPY",
-                        length);
+                        uct_iov_total_length(iov, iovcnt));
     return UCS_OK;
 }
 
@@ -88,11 +98,11 @@ ucs_status_t uct_knem_ep_put_zcopy(uct_ep_h tl_ep, const uct_iov_t *iov, size_t 
     uct_knem_key_t *key = (uct_knem_key_t *)rkey;
     ucs_status_t status;
 
-    UCT_CHECK_PARAM(1 == iovcnt, "iov[iovcnt] has to be 1 at this time");
-    size_t length = iov[0].length;
+    UCT_CHECK_IOV_SIZE(iovcnt, uct_sm_get_max_iov(), "uct_knem_ep_put_zcopy");
 
-    status = uct_knem_rma(tl_ep, iov[0].buffer, length, remote_addr, key, 1);
-    UCT_TL_EP_STAT_OP_IF_SUCCESS(status, ucs_derived_of(tl_ep, uct_base_ep_t), PUT, ZCOPY, length);
+    status = uct_knem_rma(tl_ep, iov, iovcnt, remote_addr, key, 1);
+    UCT_TL_EP_STAT_OP_IF_SUCCESS(status, ucs_derived_of(tl_ep, uct_base_ep_t),
+                                 PUT, ZCOPY, uct_iov_total_length(iov, iovcnt));
     return status;
 }
 
@@ -103,10 +113,10 @@ ucs_status_t uct_knem_ep_get_zcopy(uct_ep_h tl_ep, const uct_iov_t *iov, size_t 
     uct_knem_key_t *key = (uct_knem_key_t *)rkey;
     ucs_status_t status;
 
-    UCT_CHECK_PARAM(1 == iovcnt, "iov[iovcnt] has to be 1 at this time");
-    size_t length = iov[0].length;
+    UCT_CHECK_IOV_SIZE(iovcnt, uct_sm_get_max_iov(), "uct_knem_ep_get_zcopy");
 
-    status = uct_knem_rma(tl_ep, iov[0].buffer, length, remote_addr, key, 0);
-    UCT_TL_EP_STAT_OP_IF_SUCCESS(status, ucs_derived_of(tl_ep, uct_base_ep_t), GET, ZCOPY, length);
+    status = uct_knem_rma(tl_ep, iov, iovcnt, remote_addr, key, 0);
+    UCT_TL_EP_STAT_OP_IF_SUCCESS(status, ucs_derived_of(tl_ep, uct_base_ep_t),
+                                 GET, ZCOPY, uct_iov_total_length(iov, iovcnt));
     return status;
 }
