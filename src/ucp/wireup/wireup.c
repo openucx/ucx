bcd5c2fe3ea81cccd5bd34584c7dce71723cc798
Yossi Itigin <yosefe@mellanox.com>
2016-11-01 14:24:52 +0200

    UCP/WIREUP: Fix leak in error flow found by coverity.

--- src/ucp/wireup/wireup.c
+++ src/ucp/wireup/wireup.c
@@ -105,6 +105,7 @@ static ucs_status_t ucp_wireup_msg_send(ucp_ep_h ep, uint8_t type,
     status = ucp_address_pack(ep->worker, ep, tl_bitmap, order,
                               &req->send.length, &address);
     if (status != UCS_OK) {
+        ucs_free(req);
         ucs_error("failed to pack address: %s", ucs_status_string(status));
         return status;
     }
