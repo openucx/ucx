f5ebe10df6c8873899925cfbd3b916d5f62f91cd
Mikhail Brinskii <mikhailb@mellanox.com>
2017-01-18 22:02:07 +0200

    UCT/DC: Fix compact AV issue found by tests

--- src/uct/ib/ud/accel/ud_mlx5_common.c
+++ src/uct/ib/ud/accel/ud_mlx5_common.c
@@ -52,9 +52,8 @@ ucs_status_t uct_ud_mlx5_iface_get_av(uct_ib_iface_t *iface,
     base_av->fl_mlid      = mlx5_av_base(&mlx5_av)->fl_mlid;
     base_av->rlid         = mlx5_av_base(&mlx5_av)->rlid;
 
-    if (!ud_common_iface->config.compact_av) {
-        base_av->dqp_dct  = UCT_IB_MLX5_EXTENDED_UD_AV;
-    }
+    base_av->dqp_dct = (ud_common_iface->config.compact_av) ? 0 :
+                        UCT_IB_MLX5_EXTENDED_UD_AV;
 
     ucs_assertv_always((UCT_IB_MLX5_AV_FULL_SIZE > UCT_IB_MLX5_AV_BASE_SIZE) ||
                        (base_av->dqp_dct & UCT_IB_MLX5_EXTENDED_UD_AV),
