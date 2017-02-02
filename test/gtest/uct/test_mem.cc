6bc7cef54d0f962720bc3a8d60406ee9ca69da6a
Evgeny Leksikov <evgenylek@mellanox.com>
2017-02-02 15:59:00 +0200

    GTEST/UCT/MEM: add mmap fixed testing

--- test/gtest/uct/test_mem.cc
+++ test/gtest/uct/test_mem.cc
@@ -100,8 +100,80 @@ UCS_TEST_P(test_mem, pd_alloc) {
     uct_release_md_resource_list(md_resources);
 }
 
+UCS_TEST_P(test_mem, mmap_fixed) {
+    uct_md_resource_desc_t  *md_resources;
+    uct_md_attr_t           md_attr;
+    uct_md_config_t         *md_config;
+    uct_md_h                pd;
+    unsigned                num_md_resources, i, j;
+
+    const size_t            page_size   = sysconf(_SC_PAGESIZE);
+    const size_t            n_tryes     = 101;
+    uct_alloc_method_t      meth_mmap   = UCT_ALLOC_METHOD_MMAP;
+    uct_alloc_method_t      meth_heap   = UCT_ALLOC_METHOD_HEAP;
+    uintptr_t               p_addr      = 0;
+    size_t                  n_success   = 0;
+
+    uct_allocated_memory_t  mem_arr[n_tryes + 1];
+    ucs_status_t            status;
+
+    status = uct_query_md_resources(&md_resources, &num_md_resources);
+    ASSERT_UCS_OK(status);
+
+    for (i = 0; i < num_md_resources; ++i) {
+        status = uct_md_config_read(md_resources[i].md_name, NULL, NULL, &md_config);
+        ASSERT_UCS_OK(status);
+
+        status = uct_md_open(md_resources[i].md_name, md_config, &pd);
+        uct_config_release(md_config);
+        ASSERT_UCS_OK(status);
+
+        status = uct_md_query(pd, &md_attr);
+        ASSERT_UCS_OK(status);
+
+        if (md_attr.cap.flags & UCT_MD_FLAG_ALLOC) {
+            status = uct_mem_alloc(n_tryes * page_size * 2, 0, &meth_heap, 1,
+                                   NULL, 0, "test", &mem_arr[n_tryes]);
+            ASSERT_UCS_OK(status);
+            p_addr = (uintptr_t)mem_arr[n_tryes].address;
+            p_addr += page_size - (p_addr % page_size);
+
+            for (j = 0; j < n_tryes; ++j) {
+                EXPECT_EQ(p_addr % page_size, 0);
+                mem_arr[j].address = (void *)p_addr;
+                status = uct_mem_alloc(1, UCT_MD_MEM_FLAG_FIXED, &meth_mmap,
+                                       1, &pd, 1, "test", &mem_arr[j]);
+                ASSERT_UCS_OK(status);
+                ++n_success;
+                EXPECT_EQ(UCT_ALLOC_METHOD_MMAP, mem_arr[j].method);
+                EXPECT_EQ(p_addr, (uintptr_t)mem_arr[j].address);
+                EXPECT_GE(mem_arr[j].length, 1);
+                /* touch the page*/
+                *((char *)mem_arr[j].address) = 'c';
+                EXPECT_EQ(*(char *)p_addr, 'c');
+
+                p_addr += mem_arr[j].length + (mem_arr[j].length % page_size);
+            }
+
+            for (j = 0; j < n_tryes + 1; ++j) {
+                if (j == n_tryes) {
+                    EXPECT_EQ(UCT_ALLOC_METHOD_HEAP, mem_arr[j].method);
+                } else {
+                    EXPECT_EQ(UCT_ALLOC_METHOD_MMAP, mem_arr[j].method);
+                }
+                status = uct_mem_free(&mem_arr[j]);
+                ASSERT_UCS_OK(status);
+            }
+        }
+
+        uct_md_close(pd);
+    }
+
+    EXPECT_GT(n_success, 0);
+    uct_release_md_resource_list(md_resources);
+}
+
 INSTANTIATE_TEST_CASE_P(alloc_methods, test_mem,
                         ::testing::Values(UCT_ALLOC_METHOD_HEAP,
                                           UCT_ALLOC_METHOD_MMAP,
                                           UCT_ALLOC_METHOD_HUGE));
-
