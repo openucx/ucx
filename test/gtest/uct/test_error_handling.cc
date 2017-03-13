2b78adbed23e2b438bfe1a3f9d7672a5ec34b3d9
Evgeny Leksikov <evgenylek@mellanox.com>
2017-03-13 10:56:21 +0200

    UCT: refactor error handling tests

--- test/gtest/uct/test_error_handling.cc
+++ test/gtest/uct/test_error_handling.cc
@@ -15,6 +15,8 @@ extern "C" {
 class test_error_handling : public uct_test {
 public:
     virtual void init() {
+        entity *e1, *e2;
+
         uct_test::init();
 
         set_config("LOG_LEVEL=fatal");
@@ -27,12 +29,13 @@ public:
             set_config("RC_RETRY_COUNT=2");
         }
 
-        m_e1 = uct_test::create_entity(0);
-        m_entities.push_back(m_e1);
+        e1 = uct_test::create_entity(0);
+        m_entities.push_back(e1);
 
-        m_e2 = uct_test::create_entity(0);
+        e2 = uct_test::create_entity(0);
+        m_entities.push_back(e2);
 
-        connect();
+        connect(e1, e2);
     }
 
     static ucs_status_t am_dummy_handler(void *arg, void *data, size_t length, void *desc) {
@@ -50,19 +53,40 @@ public:
         req_count++;
     }
 
-    void connect() {
-        m_e1->connect(0, *m_e2, 0);
-        m_e2->connect(0, *m_e1, 0);
+    static void connect(entity *e1, entity *e2) {
+        e1->connect(0, *e2, 0);
+        e2->connect(0, *e1, 0);
 
-        uct_iface_set_am_handler(m_e1->iface(), 0, am_dummy_handler,
+        uct_iface_set_am_handler(e1->iface(), 0, am_dummy_handler,
                                  NULL, UCT_AM_CB_FLAG_ASYNC);
-        uct_iface_set_am_handler(m_e2->iface(), 0, am_dummy_handler,
+        uct_iface_set_am_handler(e2->iface(), 0, am_dummy_handler,
                                  NULL, UCT_AM_CB_FLAG_ASYNC);
+    }
+
+    void close_peer() {
+        if (is_ep2ep_tl()) {
+            m_entities.back()->destroy_ep(0);
+        } else {
+            size_t n = m_entities.remove(m_entities.back());
+            ucs_assert(n == 1);
+        }
+    }
+
+    uct_ep_h ep() {
+        return m_entities.front()->ep(0);
+    }
+
+    void flush() {
+        return m_entities.front()->flush();
+    }
+
+protected:
+    bool is_ep2ep_tl() const {
+        return GetParam()->tl_name == "rc" || GetParam()->tl_name == "rc_mlx5";
     }
 
 protected:
     static int req_count;
-    entity *m_e1, *m_e2;
 };
 
 int test_error_handling::req_count = 0;
@@ -71,56 +95,47 @@ UCS_TEST_P(test_error_handling, peer_failure)
 {
     check_caps(UCT_IFACE_FLAG_ERRHANDLE_PEER_FAILURE);
 
-    delete m_e2;
-    EXPECT_EQ(uct_ep_put_short(m_e1->ep(0), NULL, 0, 0, 0), UCS_OK);
+    close_peer();
+    EXPECT_EQ(uct_ep_put_short(ep(), NULL, 0, 0, 0), UCS_OK);
 
-    m_e1->flush();
+    flush();
 
     UCS_TEST_GET_BUFFER_IOV(iov, iovcnt, NULL, 0, NULL, 1);
 
     /* Check that all ep operations return pre-defined error code */
-    EXPECT_EQ(uct_ep_am_short(m_e1->ep(0), 0, 0, NULL, 0),
+    EXPECT_EQ(uct_ep_am_short(ep(), 0, 0, NULL, 0), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_am_bcopy(ep(), 0, NULL, NULL), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_am_zcopy(ep(), 0, NULL, 0, iov, iovcnt, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_am_bcopy(m_e1->ep(0), 0, NULL, NULL),
+    EXPECT_EQ(uct_ep_put_short(ep(), NULL, 0, 0, 0), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_put_bcopy(ep(), NULL, NULL, 0, 0), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_put_zcopy(ep(), iov, iovcnt, 0, 0, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_am_zcopy(m_e1->ep(0), 0, NULL, 0, iov, iovcnt, NULL),
+    EXPECT_EQ(uct_ep_get_bcopy(ep(), NULL, NULL, 0, 0, 0, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_put_short(m_e1->ep(0), NULL, 0, 0, 0),
+    EXPECT_EQ(uct_ep_get_zcopy(ep(), iov, iovcnt, 0, 0, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_put_bcopy(m_e1->ep(0), NULL, NULL, 0, 0),
+    EXPECT_EQ(uct_ep_atomic_add64(ep(), 0, 0, 0), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_atomic_add32(ep(), 0, 0, 0), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_atomic_fadd64(ep(), 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_put_zcopy(m_e1->ep(0), iov, iovcnt, 0, 0, NULL),
+    EXPECT_EQ(uct_ep_atomic_fadd32(ep(), 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_get_bcopy(m_e1->ep(0), NULL, NULL, 0, 0, 0, NULL),
+    EXPECT_EQ(uct_ep_atomic_swap64(ep(), 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_get_zcopy(m_e1->ep(0), iov, iovcnt, 0, 0, NULL),
+    EXPECT_EQ(uct_ep_atomic_swap32(ep(), 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_add64(m_e1->ep(0), 0, 0, 0),
+    EXPECT_EQ(uct_ep_atomic_cswap64(ep(), 0, 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_add32(m_e1->ep(0), 0, 0, 0),
+    EXPECT_EQ(uct_ep_atomic_cswap32(ep(), 0, 0, 0, 0, NULL, NULL),
               UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_fadd64(m_e1->ep(0), 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_fadd32(m_e1->ep(0), 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_swap64(m_e1->ep(0), 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_swap32(m_e1->ep(0), 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_cswap64(m_e1->ep(0), 0, 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_atomic_cswap32(m_e1->ep(0), 0, 0, 0, 0, NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_flush(m_e1->ep(0), 0, NULL), UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_get_address(m_e1->ep(0), NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_pending_add(m_e1->ep(0), NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
-    EXPECT_EQ(uct_ep_connect_to_ep(m_e1->ep(0), NULL, NULL),
-              UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_flush(ep(), 0, NULL), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_get_address(ep(), NULL), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_pending_add(ep(), NULL), UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_connect_to_ep(ep(), NULL, NULL), UCS_ERR_ENDPOINT_TIMEOUT);
 }
 
-UCS_TEST_P(test_error_handling, purge_failed_ep)
+UCS_TEST_P(test_error_handling, purge_failed_peer)
 {
     check_caps(UCT_IFACE_FLAG_ERRHANDLE_PEER_FAILURE);
 
@@ -130,23 +145,22 @@ UCS_TEST_P(test_error_handling, purge_failed_ep)
 
     req_count = 0;
 
-    delete m_e2;
+    close_peer();
 
     do {
-          status = uct_ep_put_short(m_e1->ep(0), NULL, 0, 0, 0);
+          status = uct_ep_put_short(ep(), NULL, 0, 0, 0);
     } while (status == UCS_OK);
 
     for (int i = 0; i < num_pend_sends; i ++) {
         reqs[i].func = pending_cb;
-        EXPECT_EQ(uct_ep_pending_add(m_e1->ep(0), &reqs[i]), UCS_OK);
+        EXPECT_EQ(uct_ep_pending_add(ep(), &reqs[i]), UCS_OK);
     }
 
-    m_e1->flush();
+    flush();
 
-    EXPECT_EQ(uct_ep_am_short(m_e1->ep(0), 0, 0, NULL, 0),
-              UCS_ERR_ENDPOINT_TIMEOUT);
+    EXPECT_EQ(uct_ep_am_short(ep(), 0, 0, NULL, 0), UCS_ERR_ENDPOINT_TIMEOUT);
 
-    uct_ep_pending_purge(m_e1->ep(0), purge_cb, NULL);
+    uct_ep_pending_purge(ep(), purge_cb, NULL);
     EXPECT_EQ(num_pend_sends, req_count);
 }
 
