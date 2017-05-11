83f2b2ad3e0745ac5f709348be37e46e93481326
Yossi Itigin <yosefe@mellanox.com>
2017-05-11 17:50:46 +0300

    UCS/TIMERQ: Fix min_interval when timer is removed.

--- test/gtest/ucs/test_time.cc
+++ test/gtest/ucs/test_time.cc
@@ -116,6 +116,7 @@ UCS_TEST_F(test_time, timerq) {
         }
         EXPECT_EQ(0u, counter1);
         EXPECT_NEAR(test_time / interval2, counter2, 1);
+        EXPECT_LE(ucs_timerq_min_interval(&timerq), UCS_TIME_INFINITY);
 
         /*
          * Check that after rescheduling, both timers are invoked again
