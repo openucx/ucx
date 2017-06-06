dbd0cd86af6067dc117d846f5deebebb9af8a65e
Yossi Itigin <yosefe@mellanox.com>
2017-06-06 20:07:22 +0300

    UCT/BASE: Add copyright from uct_md.c to uct_worker.c

--- src/uct/base/uct_worker.c
+++ src/uct/base/uct_worker.c
@@ -1,5 +1,7 @@
 /**
  * Copyright (C) Mellanox Technologies Ltd. 2001-2017.  ALL RIGHTS RESERVED.
+ * Copyright (C) UT-Battelle, LLC. 2017. ALL RIGHTS RESERVED.
+ * Copyright (C) ARM Ltd. 2016-2017. ALL RIGHTS RESERVED.
  *
  * See file LICENSE for terms.
  */
