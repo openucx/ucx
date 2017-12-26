13358d45e96744553c825d1fd785a9b8dc9b052b
Yossi Itigin <yosefe@mellanox.com>
2017-12-26 21:25:10 +0200

    TEST/HASH: Add several retries to hash performance test.

--- test/gtest/ucs/test_hash_perf.cc
+++ test/gtest/ucs/test_hash_perf.cc
@@ -233,9 +233,8 @@ class test_hash_perf : public ucs::test {
 
 UCS_TEST_F(test_hash_perf, perf_compare) {
 
+    const ucs_time_t MAX_LOOKUP_NS_1024 = 400;
     size_t trip_counts[] = {1, 2, 8, 128, 1024, 32768, 262144, 1048576, 0};
-    ucs_time_t khash_lookup_ns_1024 = 0;
-    ucs_time_t sglib_lookup_ns_1024 = 0;
 
     if (ucs::test_time_multiplier() > 1) {
         UCS_TEST_SKIP_R("Long run expected. Skipped.");
@@ -260,6 +259,20 @@ UCS_TEST_F(test_hash_perf, perf_compare) {
 
             ucs_time_t insert_ns = cur_hash->initialize(num_elems);
             ucs_time_t lookup_ns = cur_hash->lookup(num_elems);
+
+            if ((1024 == num_elems) &&
+                ((cur_hash == perf_compare_khash_ptr) ||
+                 (cur_hash == perf_compare_sglib_ptr)))
+            {
+                int count = 1;
+                while ((lookup_ns >= MAX_LOOKUP_NS_1024) && (count < 5)) {
+                    lookup_ns = cur_hash->lookup(num_elems);
+                    ++count;
+                }
+                EXPECT_LT(lookup_ns, MAX_LOOKUP_NS_1024) << cur_hash->get_name()
+                                << " failed after " << count << " attempts";
+            }
+
             ucs_time_t remove_ns = cur_hash->cleanup(num_elems);
 
             UCS_TEST_MESSAGE << FIELD_ALIGN(6) << cur_hash->get_name()
@@ -267,18 +280,7 @@ UCS_TEST_F(test_hash_perf, perf_compare) {
                              << FIELD_ALIGN(6) << insert_ns
                              << FIELD_ALIGN(6) << lookup_ns
                              << FIELD_ALIGN(6) << remove_ns;
-            if (1024 == num_elems) {
-                if (cur_hash == perf_compare_khash_ptr ) {
-                    khash_lookup_ns_1024 = lookup_ns;
-                }
-                if (cur_hash == perf_compare_sglib_ptr ) {
-                    sglib_lookup_ns_1024 = lookup_ns;
-                }
-            }
         }
         delete cur_hash;
     }
-
-    EXPECT_LT(khash_lookup_ns_1024, 400.0);
-    EXPECT_LT(sglib_lookup_ns_1024, 400.0);
 }
