b2fafb8318a90108c2e16d4769eabf6479ce5d12
Yossi Itigin <yosefe@mellanox.com>
2018-04-04 14:12:24 +0300

    UCS/TEST: Add strided allocator data structure
    It allows allocating objects in a strided (instead of contiguous) memory
    layout, which improves cache locality when some strides are used more
    than others. The gap between the strides is compile-time constant for
    faster access to the strides.
    In addition, in order to distribute strided_alloc.h, need to move
    UCS_PTR_BYTE_OFFSET to compiler_def.h

---
+++ src/ucs/datastruct/strided_alloc.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) Mellanox Technologies Ltd. 2001-2018.  ALL RIGHTS RESERVED.
+ * See file LICENSE for terms.
+ */
+
+#ifndef UCS_STRIDED_ALLOC_H_
+#define UCS_STRIDED_ALLOC_H_
+
+#include "queue_types.h"
+
+#include <ucs/type/status.h>
+#include <ucs/sys/compiler_def.h>
+#include <ucs/sys/math.h>
+#include <stddef.h>
+
+
+BEGIN_C_DECLS
+
+
+/* the distance between allocated elements */
+#define UCS_STRIDED_ALLOC_STRIDE (128 * UCS_KBYTE)
+
+
+/**
+ * Get a pointer to another element in the strided object
+ *
+ * Example with stride_count=3:
+ *
+ * chunk
+ * start -+
+ *        |
+ *        \/
+ *        |
+ *        +--------+--   ...   --+--------+--   ...   --+--------+
+ *        | stride |             | stride |             | stride |
+ * obj0:  | elem 0 | <- 128kB -> | elem 1 | <- 128kB -> | elem 2 |
+ *        | (base) |             |        |             |        |
+ *        +--------+--   ...   --+--------+--   ...   --+--------+
+ *                 +--------+--  ...    --+--------+--  ...    --+--------+
+ *                 | stride |             | stride |             | stride |
+ * obj1:           | elem 0 | <- 128kB -> | elem 1 | <- 128kB -> | elem 2 |
+ *                 | (base) |             |        |             |        |
+ *                 +--------+--  ...     -+--------+--  ...    --+--------+
+ *                          +--------+--  ...    --+--------+--   ...   --+--------+
+ *                          | stride |             | stride |             | stride |
+ * obj2:                    | elem 0 | <- 128kB -> | elem 1 | <- 128kB -> | elem 2 |
+ *                          | (base) |             |        |             |        |
+ *                          +--------+--  ...     -+--------+--   ...   --+--------+
+ *
+ * ...
+ *
+ * @param _elem        Pointer to the current element
+ * @param _stride_idx  Stride index of the current element
+ * @param _wanted_idx  Stride index of the desired element
+ *
+ * @return Pointer to the desired element
+ */
+#define ucs_strided_alloc_step(_elem, _stride_idx, _wanted_idx) \
+    UCS_PTR_BYTE_OFFSET(_elem, UCS_STRIDED_ALLOC_STRIDE * \
+                        ((ptrdiff_t)(_wanted_idx) - (ptrdiff_t)(_stride_idx)))
+
+
+/* Forward declaration, used internally */
+typedef struct ucs_strided_alloc_elem ucs_strided_alloc_elem_t;
+
+
+/**
+ * Strided allocator - allows allocating objects which are split to several
+ * memory areas with a constant stride (gap) in-between.
+ * This improves the cache locality when the first memory area is used mostly.
+ */
+typedef struct ucs_strided_alloc {
+    ucs_strided_alloc_elem_t  *freelist;  /* LIFO of free elements */
+    ucs_queue_head_t          chunks;      /* Queue of allocated chunks */
+    size_t                    elem_size; /* Size of a single memory area */
+    unsigned                  stride_count;
+} ucs_strided_alloc_t;
+
+
+/**
+ * Initialize the split allocator context
+ *
+ * @param [in] sa            Strided allocator structure to initialize
+ * @param [in] elem_size     Size of a single stride element
+ * @param [in] stride_count  How many memory strides per object
+ */
+void ucs_strided_alloc_init(ucs_strided_alloc_t *sa, size_t elem_size,
+                            unsigned stride_count);
+
+
+/**
+ * Cleanup the split allocator context
+ *
+ * @param [in] sa           Strided allocator structure to cleanup
+ */
+void ucs_strided_alloc_cleanup(ucs_strided_alloc_t *sa);
+
+
+/**
+ * Allocate an object
+ *
+ * @param [in] sa            Strided allocator to allocate on
+ * @param [in] alloc_name    Debug name of the allocation
+ *
+ * @return Pointer to the first stride of the allocated object.
+ */
+void* ucs_strided_alloc_get(ucs_strided_alloc_t *sa, const char *alloc_name);
+
+
+/**
+ * Release an object
+ *
+ * @param [in] sa            Strided allocator to release the object to
+ * @param [in] base          Pointer to the first stride of the object to release
+ */
+void ucs_strided_alloc_put(ucs_strided_alloc_t *sa, void *base);
+
+
+END_C_DECLS
+
+#endif
--- src/ucs/sys/compiler.h
+++ src/ucs/sys/compiler.h
@@ -36,10 +36,6 @@
 #endif
 
 
-/* Helper macro for address arithmetic in bytes */
-#define UCS_PTR_BYTE_OFFSET(_ptr, _offset) \
-    ((void *)((uintptr_t)(_ptr) + (_offset)))
-
 /**
  * Copy words from _src to _dst.
  *
