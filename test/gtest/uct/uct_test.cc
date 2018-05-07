a78f058c073e949e391e3372f007ed627bf5d3f4
Yossi Itigin <yosefe@mellanox.com>
2018-05-07 15:58:44 +0300

    TEST/SOCKADDR: Apply code-review comments
    (cherry picked from commit 76b341abe2845f07f90aa5caee587d532d90a4c8)

--- test/gtest/uct/uct_test.cc
+++ test/gtest/uct/uct_test.cc
@@ -70,7 +70,7 @@ void uct_test::set_sockaddr_resources(uct_md_h md, char *md_name, cpu_set_t loca
         sock_addr.addr = ifa->ifa_addr;
 
         /* If rdmacm is tested, make sure that this is an IPoIB or RoCE interface */
-        if (!strcmp(md_name, "rdmacm") && (!ucs::is_ib_netdev(ifa->ifa_name))) {
+        if (!strcmp(md_name, "rdmacm") && (!ucs::is_rdmacm_netdev(ifa->ifa_name))) {
             continue;
         }
 
