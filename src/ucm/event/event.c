openeuler-mirror/kucx

UCM/CUDA: Set minimum released region size to 1 instead of 4

src/ucm/event/event.c
@@ -458,7 +458,7 @@ static void ucm_cudafree_dispatch_events(void *dptr)
     ret = cuMemGetAddressRange(&pbase, &psize, (CUdeviceptr) dptr);
     if (ret != CUDA_SUCCESS) {
         ucm_warn("cuMemGetAddressRange(devPtr=%p) failed", (void *)dptr);
-        psize = 4; /* set minimum length */
+        psize = 1; /* set minimum length */
     }
     ucs_assert(dptr == (void *)pbase);
 

openucx/ucx

src/ucm/event/event.c
@@ -364,7 +364,7 @@ cudaError_t ucm_cudaFree(void *devPtr)
     cuerr = cuMemGetAddressRange(&pbase, &psize, (CUdeviceptr) devPtr);
     if (cuerr != CUDA_SUCCESS) {
         ucm_warn("cuMemGetAddressRange(devPtr=%p) failed", devPtr);
-        psize = 4; /* set mininum length */
+        psize = 1; /* set mininum length */
     }
     ucs_assert((void *)pbase == devPtr);
 