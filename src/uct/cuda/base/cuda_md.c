14e367ab98a35d526c7a5f1bb05b2cc8735a942f
Yossi Itigin <yosefe@mellanox.com>
2019-02-13 15:17:38 +0200

    UCT/CUDA: Load gdrcopy module by cuda module

--- src/uct/cuda/base/cuda_md.c
+++ src/uct/cuda/base/cuda_md.c
@@ -3,10 +3,12 @@
  * See file LICENSE for terms.
  */
 
-#include <uct/cuda/base/cuda_md.h>
+#include "cuda_md.h"
 
+#include <ucs/sys/module.h>
 #include <cuda_runtime.h>
 #include <cuda.h>
+
 
 int uct_cuda_is_mem_type_owned(uct_md_h md, void *addr, size_t length)
 {
@@ -23,4 +25,11 @@ int uct_cuda_is_mem_type_owned(uct_md_h md, void *addr, size_t length)
 
     cu_err = cuPointerGetAttributes(2, attributes, attrdata, (CUdeviceptr)addr);
     return ((cu_err == CUDA_SUCCESS) && (!isManaged && (memType == CU_MEMORYTYPE_DEVICE)));
+}
+
+UCS_MODULE_INIT() {
+    /* TODO make gdrcopy independent of cuda */
+    UCS_MODULE_FRAMEWORK_DECLARE(uct_cuda);
+    UCS_MODULE_FRAMEWORK_LOAD(uct_cuda);
+    return UCS_OK;
 }
