c26614c2f4786cd9be7c8bbd9b2d664609b35c52
Alina Sklarevich <alinas@mellanox.com>
2019-04-04 17:57:22 +0300

    UCM: Make ucm_reloc_get_orig() a static function.
    Per Pr https://github.com/openucx/ucx/pull/3436

--- src/ucm/util/reloc.h
+++ src/ucm/util/reloc.h
@@ -10,6 +10,8 @@
 
 #include <ucs/datastruct/list.h>
 #include <ucs/type/status.h>
+#include <ucm/util/log.h>
+#include <dlfcn.h>
 
 
 /**
@@ -39,12 +41,35 @@ ucs_status_t ucm_reloc_modify(ucm_reloc_patch_t* patch);
 /**
  * Get the original implementation of 'symbol', which is not equal to 'replacement'.
  *
+ * This function is static to make sure the symbol search is done from the context
+ * of the shared object which defines the replacement function.
+ * If the replacement function is defined in a loadbale module, the symbols it
+ * imports from other libraries may not be available in global scope.
+ *
  * @param [in]  symbol       Symbol name,
  * @param [in]  replacement  Symbol replacement, which should be ignored.
  *
  * @return Original function pointer for 'symbol'.
  */
-void* ucm_reloc_get_orig(const char *symbol, void *replacement);
+static void* UCS_F_MAYBE_UNUSED
+ucm_reloc_get_orig(const char *symbol, void *replacement)
+{
+    const char *error;
+    void *func_ptr;
 
+    func_ptr = dlsym(RTLD_NEXT, symbol);
+    if (func_ptr == NULL) {
+        (void)dlerror();
+        func_ptr = dlsym(RTLD_DEFAULT, symbol);
+        if (func_ptr == replacement) {
+            error = dlerror();
+            ucm_fatal("could not find address of original %s(): %s", symbol,
+                      error ? error : "Unknown error");
+        }
+    }
+
+    ucm_debug("original %s() is at %p", symbol, func_ptr);
+    return func_ptr;
+}
 
 #endif
