530d9fa4bda53e04503f4873cdcc1f6f7195d4f2
Peter Rudenko <peterr@mellanox.com>
2019-04-24 15:47:09 +0300

    JUCX: unpack remote key buffer functionality.

--- bindings/java/src/main/java/org/ucx/jucx/ucp/UcpEndpoint.java
+++ bindings/java/src/main/java/org/ucx/jucx/ucp/UcpEndpoint.java
@@ -1,8 +1,13 @@
+/*
+ * Copyright (C) Mellanox Technologies Ltd. 2019. ALL RIGHTS RESERVED.
+ * See file LICENSE for terms.
+ */
 package org.ucx.jucx.ucp;
 
 import org.ucx.jucx.UcxNativeStruct;
 
 import java.io.Closeable;
+import java.nio.ByteBuffer;
 
 public class UcpEndpoint extends UcxNativeStruct implements Closeable {
 
@@ -16,7 +21,20 @@ public class UcpEndpoint extends UcxNativeStruct implements Closeable {
         setNativeId(null);
     }
 
+    /**
+     * This routine unpacks the remote key (RKEY) object into the local memory
+     * such that it can be accessed and used by UCP routines.
+     * @param rkeyBuffer - Packed remote key buffer
+     *                     (see {@link UcpMemory#getRemoteKeyBuffer()}).
+     */
+    public UcpRemoteKey unpackRemoteKey(ByteBuffer rkeyBuffer) {
+        return unpackRemoteKey(getNativeId(), rkeyBuffer);
+    }
+
     private static native long createEndpointNative(UcpEndpointParams params, long workerId);
 
     private static native void destroyEndpointNative(long epId);
+
+    private static native UcpRemoteKey unpackRemoteKey(long epId, ByteBuffer rkeyBuffer);
+
 }
