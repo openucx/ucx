dd01c8b9a153b5b73931c8a24647db1f4c4e7a20
Peter Rudenko <peterr@mellanox.com>
2019-04-24 13:45:37 +0300

    JUCX: Set enpoint error handler.

--- bindings/java/src/main/native/jucx_common_def.h
+++ bindings/java/src/main/native/jucx_common_def.h
@@ -13,7 +13,7 @@
 
 typedef uintptr_t native_ptr;
 
-static void log_error(const char* error);
+void log_error(const char* error);
 
 JNIEXPORT void JNICALL JNU_ThrowException(JNIEnv *, const char *);
 
