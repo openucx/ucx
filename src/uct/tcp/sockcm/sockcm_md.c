b257bda373c0e245d6e8aa47515ebfe3497f1e41
FNU Akshay Venkatesh <akvenkatesh@nvidia.com>
2019-05-08 11:14:55 -0700

    UCT/SOCKCM: sockcm implementation over skeleton

--- src/uct/tcp/sockcm/sockcm_md.c
+++ src/uct/tcp/sockcm/sockcm_md.c
@@ -43,10 +43,105 @@ ucs_status_t uct_sockcm_md_query(uct_md_h md, uct_md_attr_t *md_attr)
     return UCS_OK;
 }
 
+static int uct_sockcm_is_addr_route_resolved(int sock_id, struct sockaddr *addr,
+                                            int addrlen)
+{
+    char ip_port_str[UCS_SOCKADDR_STRING_LEN];
+    char host[UCS_SOCKADDR_STRING_LEN];
+    char serv[UCS_SOCKADDR_STRING_LEN];
+    int ret = -1;
+
+    ret = getnameinfo(addr, addrlen, host, UCS_SOCKADDR_STRING_LEN, serv,
+                      UCS_SOCKADDR_STRING_LEN, NI_NAMEREQD);
+    if (0 != ret) {
+        ucs_debug("getnameinfo error : %s\n", gai_strerror(ret));
+        return 0;
+    }
+
+    if (connect(sock_id, addr, addrlen)) {
+
+        if (errno == ECONNREFUSED) {
+            return 1;
+        }
+
+        ucs_debug("connect(addr = %s) failed: %m",
+                   ucs_sockaddr_str(addr, ip_port_str, UCS_SOCKADDR_STRING_LEN));
+        return 0;
+    }
+
+    return 1;
+}
+
+static int uct_sockcm_is_sockaddr_inaddr_any(struct sockaddr *addr)
+{
+    struct sockaddr_in6 *addr_in6;
+    struct sockaddr_in *addr_in;
+
+    switch (addr->sa_family) {
+    case AF_INET:
+        addr_in = (struct sockaddr_in *)addr;
+        return addr_in->sin_addr.s_addr == INADDR_ANY;
+    case AF_INET6:
+        addr_in6 = (struct sockaddr_in6 *)addr;
+        return !memcmp(&addr_in6->sin6_addr, &in6addr_any, sizeof(addr_in6->sin6_addr));
+    default:
+        ucs_debug("Invalid address family: %d", addr->sa_family);
+    }
+
+    return 0;
+}
+
 int uct_sockcm_is_sockaddr_accessible(uct_md_h md, const ucs_sock_addr_t *sockaddr,
                                       uct_sockaddr_accessibility_t mode)
 {
-    return 0;
+    uct_sockcm_md_t *sockcm_md = ucs_derived_of(md, uct_sockcm_md_t);
+    int is_accessible = 0;
+    int sock_id = -1;
+    char ip_port_str[UCS_SOCKADDR_STRING_LEN];
+    struct sockaddr *param_sockaddr = NULL;
+
+    param_sockaddr = (struct sockaddr *) sockaddr->addr;
+
+    if ((mode != UCT_SOCKADDR_ACC_LOCAL) && (mode != UCT_SOCKADDR_ACC_REMOTE)) {
+        ucs_error("Unknown sockaddr accessibility mode %d", mode);
+        return 0;
+    }
+
+    sock_id = socket(param_sockaddr->sa_family, SOCK_STREAM, 0);
+    if (-1 == sock_id) {
+        return 0;
+    }
+
+    if (mode == UCT_SOCKADDR_ACC_LOCAL) {
+
+        if (bind(sock_id, param_sockaddr, sockaddr->addrlen)) {
+            ucs_debug("bind(addr = %s) failed: %m",
+                      ucs_sockaddr_str((struct sockaddr *)sockaddr->addr,
+                                       ip_port_str, UCS_SOCKADDR_STRING_LEN));
+            goto out_destroy_id;
+        }
+
+        if (uct_sockcm_is_sockaddr_inaddr_any(param_sockaddr)) {
+            is_accessible = 1;
+            goto out_print;
+        }
+    }
+
+    is_accessible = uct_sockcm_is_addr_route_resolved(sock_id, param_sockaddr,
+                                                     sockaddr->addrlen);
+    if (!is_accessible) {
+        goto out_destroy_id;
+    }
+
+ out_print:
+    ucs_debug("address %s is accessible from sockcm_md %p with mode: %d",
+              ucs_sockaddr_str(param_sockaddr, ip_port_str,
+                               UCS_SOCKADDR_STRING_LEN), sockcm_md, mode);
+
+ out_destroy_id:
+    close(sock_id);
+
+    return is_accessible;
 }
 
 static ucs_status_t uct_sockcm_query_md_resources(uct_md_resource_desc_t **resources_p,
@@ -60,17 +155,22 @@ uct_sockcm_md_open(const char *md_name, const uct_md_config_t *uct_md_config,
                    uct_md_h *md_p)
 {
     uct_sockcm_md_t *md;
+    ucs_status_t status;
 
     md = ucs_malloc(sizeof(*md), "sockcm_md");
     if (md == NULL) {
-        return UCS_ERR_NO_MEMORY;
+        status = UCS_ERR_NO_MEMORY;
+        goto out;
     }
 
     md->super.ops            = &uct_sockcm_md_ops;
     md->super.component      = &uct_sockcm_mdc;
 
     *md_p = &md->super;
-    return UCS_OK;
+    status = UCS_OK;
+
+out:
+    return status;
 }
 
 UCT_MD_COMPONENT_DEFINE(uct_sockcm_mdc, UCT_SOCKCM_MD_PREFIX,
--- src/uct/tcp/tcp_iface.c
+++ src/uct/tcp/tcp_iface.c
@@ -96,7 +96,7 @@ static ucs_status_t uct_tcp_iface_query(uct_iface_h tl_iface, uct_iface_attr_t *
                              UCT_IFACE_FLAG_CB_SYNC          |
                              UCT_IFACE_FLAG_EVENT_SEND_COMP  |
                              UCT_IFACE_FLAG_EVENT_RECV;
-
+    attr->cap.flags       += UCT_IFACE_FLAG_ERRHANDLE_PEER_FAILURE;
     attr->cap.am.max_short = attr->cap.am.max_bcopy =
         iface->am_buf_size - sizeof(uct_tcp_am_hdr_t);
 
