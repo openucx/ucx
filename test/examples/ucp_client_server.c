133adf621aa43671cb44e62a052c8ceafd475919
FNU Akshay Venkatesh <akvenkatesh@nvidia.com>
2019-05-23 12:04:03 -0700

    TEST/EXAMPLES: use htons on server_port

--- test/examples/ucp_client_server.c
+++ test/examples/ucp_client_server.c
@@ -109,7 +109,7 @@ void set_listen_addr(struct sockaddr_in *listen_addr)
     memset(listen_addr, 0, sizeof(struct sockaddr_in));
     listen_addr->sin_family      = AF_INET;
     listen_addr->sin_addr.s_addr = INADDR_ANY;
-    listen_addr->sin_port        = server_port;
+    listen_addr->sin_port        = htons(server_port);
 }
 
 /**
@@ -120,7 +120,7 @@ void set_connect_addr(const char *address_str, struct sockaddr_in *connect_addr)
     memset(connect_addr, 0, sizeof(struct sockaddr_in));
     connect_addr->sin_family      = AF_INET;
     connect_addr->sin_addr.s_addr = inet_addr(address_str);
-    connect_addr->sin_port        = server_port;
+    connect_addr->sin_port        = htons(server_port);
 }
 
 /**
