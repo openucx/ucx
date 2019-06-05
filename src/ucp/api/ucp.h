ca20cae4f5080010cbdc1983adb85b5f18b5432a
Yossi Itigin <yosefe@mellanox.com>
2019-06-05 16:16:10 +0300

    UCP/API/DOC: Add clarification for ucp_ep_rkey_unpack()

--- src/ucp/api/ucp.h
+++ src/ucp/api/ucp.h
@@ -2062,6 +2062,8 @@ void ucp_rkey_buffer_release(void *rkey_buffer);
  * @note The application is responsible for releasing the RKEY object when
  *       it is no longer needed by calling the @ref ucp_rkey_destroy
  *       "ucp_rkey_destroy()" routine.
+ * @note The remote key object can be used for communications only on the
+ *       endpoint it was unpacked.
  *
  * @param [in]  ep            Endpoint to access using the remote key.
  * @param [in]  rkey_buffer   Packed rkey.
