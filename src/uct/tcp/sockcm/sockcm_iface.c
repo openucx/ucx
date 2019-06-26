bc24418ac1d820c6684d3e5877a313bd8d2067f8
FNU Akshay Venkatesh <akvenkatesh@nvidia.com>
2019-06-25 19:19:45 -0700

    UCT/SOCKCM: Add async handler for accept/reject handling at client

--- src/uct/tcp/sockcm/sockcm_iface.c
+++ src/uct/tcp/sockcm/sockcm_iface.c
@@ -67,7 +67,7 @@ static ucs_status_t uct_sockcm_iface_accept(uct_iface_h tl_iface,
 {
     ucs_status_t status   = UCS_OK;
     int          *fd      = conn_request;
-    int          accept   = 0;
+    char         accept   = 0;
     ssize_t      sent_len = -1;
 
     /* Notify client of accept and close associated fd */
@@ -75,7 +75,6 @@ static ucs_status_t uct_sockcm_iface_accept(uct_iface_h tl_iface,
     ucs_debug("sockcm_client: send_len = %d bytes %m", (int) sent_len);
 
     if (sent_len < 0) status = UCS_ERR_IO_ERROR;
-    close(*fd);
 
     return status;
 }
@@ -85,7 +84,7 @@ static ucs_status_t uct_sockcm_iface_reject(uct_iface_h tl_iface,
 {
     ucs_status_t status   = UCS_OK;
     int          *fd      = conn_request;
-    int          reject = 1;
+    char         reject = 1;
     ssize_t      sent_len = -1;
 
     /* Notify client of rejection and close associated fd */
@@ -93,7 +92,6 @@ static ucs_status_t uct_sockcm_iface_reject(uct_iface_h tl_iface,
     ucs_debug("sockcm_client: send_len = %d bytes %m", (int) sent_len);
 
     if (sent_len < 0) status = UCS_ERR_IO_ERROR;
-    close(*fd);
 
     return status;
 }
