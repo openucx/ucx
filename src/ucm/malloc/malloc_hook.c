a09b8eaf71be7cc696f32569351799415236e28d
Sergey Oblomov <sergeyo@mellanox.com>
2019-08-29 12:11:28 +0300

    UCS/MALLOC-HOOK: datatype convertion fix

--- src/ucm/malloc/malloc_hook.c
+++ src/ucm/malloc/malloc_hook.c
@@ -869,7 +869,7 @@ ucs_status_t ucm_malloc_install(int events)
             ucm_malloc_populate_glibc_cache();
             ucm_malloc_install_symbols(ucm_malloc_symbol_patches);
             ucm_malloc_hook_state.free           =
-                ucm_malloc_patchlist_prev_value(ucm_malloc_symbol_patches, "free");
+                (ucm_release_func_t)ucm_malloc_patchlist_prev_value(ucm_malloc_symbol_patches, "free");
             ucm_malloc_hook_state.install_state |= UCM_MALLOC_INSTALLED_MALL_SYMS;
         }
     } else {
