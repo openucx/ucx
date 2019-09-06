ea361a6903f7e7a0525f2dc2eed96b778116a984
Chris Ward <tjcw@uk.ibm.com>
2019-09-06 10:08:52 +0100

    Need to allocate 'unfinshed' before using it

--- src/ucp/core/ucp_am.c
+++ src/ucp/core/ucp_am.c
@@ -625,18 +625,18 @@ UCS_PROFILE_FUNC(ucs_status_ptr_t, ucp_am_rdma_send_nb,
         goto out ;
     }
 
-    ucp_am_send_req_init(req, ep, &(unfinished->rdma_header), UCP_DATATYPE_CONTIG, sizeof(ucp_am_rdma_header_t), flags, id);
-    status = ucp_ep_resolve_dest_ep_ptr(ep, ep->am_lane);
-    if (ucs_unlikely(status != UCS_OK)) {
-        ret = UCS_STATUS_PTR(status);
-        goto out;
-    }
-
     unfinished           = ucs_malloc(sizeof(ucp_am_rdma_client_unfinished_t),
                                          "unfinished UCP AM rdma client");
     if (ucs_unlikely(unfinished == NULL)) {
         ret = UCS_STATUS_PTR(UCS_ERR_NO_MEMORY);
         goto out ;
+    }
+
+    ucp_am_send_req_init(req, ep, &(unfinished->rdma_header), UCP_DATATYPE_CONTIG, sizeof(ucp_am_rdma_header_t), flags, id);
+    status = ucp_ep_resolve_dest_ep_ptr(ep, ep->am_lane);
+    if (ucs_unlikely(status != UCS_OK)) {
+        ret = UCS_STATUS_PTR(status);
+        goto out;
     }
 
     length = ucp_dt_iov_length(iovec, count);
