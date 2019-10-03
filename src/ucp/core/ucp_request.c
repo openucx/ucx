319c01793450cafe1be903bff7c7acb3f78cab2a
Chris Ward <tjcw@uk.ibm.com>
2019-10-03 09:25:24 +0100

    more rdma --> rendezvous

--- src/ucp/core/ucp_request.c
+++ src/ucp/core/ucp_request.c
@@ -388,14 +388,14 @@ ucp_request_send_start(ucp_request_t *req, ssize_t max_short,
     return UCS_ERR_NO_PROGRESS;
 }
 
-ucs_status_t ucp_request_rdma_send_start(ucp_request_t *req,
+ucs_status_t ucp_request_rendezvous_send_start(ucp_request_t *req,
                                          uct_pending_callback_t func)
 {
     req->send.uct.func = func;
     if (req->flags & UCP_REQUEST_FLAG_SEND_AM) {
         req->send.am.message_id = req->send.ep->worker->am_message_id++;
     }
-    UCS_PROFILE_REQUEST_EVENT(req, "start_rdma_contig_short", req->send.length);
+    UCS_PROFILE_REQUEST_EVENT(req, "request_rendezvous_send_start", req->send.length);
     return UCS_OK;
 }
 
