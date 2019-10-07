8e579ffe1f1e8b187bbf626810e2a9416e186e41
Chris Ward <tjcw@uk.ibm.com>
2019-10-07 16:52:21 +0100

    Revised in respect of comments from @brminich

--- src/ucp/api/ucpx.h
+++ src/ucp/api/ucpx.h
@@ -19,35 +19,146 @@
 
 BEGIN_C_DECLS
 
+/**
+ * @brief UCP active message rendezvous parameters field mask.
+ *
+ * The enumeration allows specifying which fields in @ref ucp_params_t are
+ * present. It is used to enable backward compatibility support.
+ */
 enum ucp_am_rendezvous_params_field {
-  UCP_AM_RENDEZVOUS_FIELD_IOVEC_SIZE = UCS_BIT(0)  /* Max size of the iovec returned by the initial AM */
+  /* Max size of the iovec returned by the initial AM */
+  UCP_AM_RENDEZVOUS_FIELD_IOVEC_SIZE = UCS_BIT(0)
 };
 
+/**
+ * @brief Tuning parameters for the UCP active message rendezvous.
+ *
+ * The structure defines the parameters that are used for the
+ * UCP active message rendezvous function
+ */
 typedef struct ucp_am_rendezvous_params {
   uint64_t field_mask ;
+  /* Number of elements that the ucp library should allocate in the 'iovec'
+   * for the rendezvous callback to fill in.
+   */
   size_t iovec_size ;
 } ucp_am_rendezvous_params_t ;
 
+/*
+ * The ucp library will drive a function on the arrival of each fragment
+ * of data from the client.
+ */
+typedef void (*ucp_am_data_function_t)(
+                                       void *target,
+                                       void *source,
+                                       size_t bytes,
+                                       void *cookie
+                                      ) ;
+/*
+ * The ucp library will drive a function when the transfer from the
+ * client is complete.
+ */
+typedef ucs_status_t (*ucp_am_local_function_t)(
+                                                void *arg,
+                                                void *cookie,
+                                                ucp_dt_iov_t *iovec,
+                                                size_t iovec_length
+                                               ) ;
 
-typedef void (*ucp_am_data_function_t)(void *target, void *source, size_t bytes, void *cookie) ;
-typedef ucs_status_t (*ucp_am_local_function_t)(void *arg, void *cookie, ucp_dt_iov_t *iovec, size_t iovec_length) ;
-
+/*
+ * Structure for communicatiob between the active message rendezvous
+ * callback and the ucp library. The ucp library sets up 'iovec_max_length'
+ * to indicate the length of the iovec in this structure; the remaining
+ * fields are set by the rendezvous callback to indicate what should happen
+ * with the remaining data from the client. In the initial implementation,
+ * iovec_max_length is always 1.
+ */
 typedef struct ucp_am_rendezvous_recv {
-    ucp_am_local_function_t local_fn ;
-    void * cookie ;
-    ucp_am_data_function_t data_fn ;
-    void *data_cookie ;
-    size_t iovec_max_length ;
-    size_t iovec_length ;
-    ucp_dt_iov_t iovec[1] ;
-  } ucp_am_rendezvous_recv_t ;
+    /* Function to be driven when data transfer is complete */
+    ucp_am_local_function_t local_fn;
+    /* Argument to be passed to local_fn */
+    void                   *cookie;
+    /* Function to be driven when each fragment of data transfer is complete
+     * In the initial implementation, all data is transferred in one fragment, so
+     * the data_fn si driven once just before the local_fn
+     */
+    ucp_am_data_function_t  data_fn;
+    /* Argument to be passed to data_fn */
+    void                   *data_cookie ;
+    /* Sise of iovec allocated by the ucp library */
+    size_t                  iovec_max_length;
+    /* Size of iovec filled in by the rendezvous callback */
+    size_t                  iovec_length;
+    /* iovec indicating where the data from the client is to be placed */
+    ucp_dt_iov_t            iovec[1];
+  } ucp_am_rendezvous_recv_t;
 
-typedef ucs_status_t (*ucp_am_rendezvous_callback_t)(void *arg, void *data, size_t length,
-                                          ucp_ep_h reply_ep, unsigned flags, size_t remaining_length, ucp_am_rendezvous_recv_t *recv);
+  /**
+   * @ingroup UCP_ENDPOINT
+   * @brief Callback to process incoming Active Message rendezvous.
+   *
+   * When the callback is called, @a flags indicates how @a data should be handled.
+   *
+   * @param [in]  arg      User-defined argument.
+   * @param [in]  data     Points to the received data header.
+   * @param [in]  length   Length of data header.
+   * @param [in]  reply_ep If the Active Message is sent with the
+   *                       UCP_AM_SEND_REPLY flag, the sending ep
+   *                       will be passed in. If not, NULL will be passed.
+   * @param [in]  flags    0.
+   * @parame[out] recv     Struture passed to the ucp library to control
+   *                       placement of the remaining data
+   *
+   * @return UCS_OK        @a data will not persist after the callback returns.
+   *
+   *
+   * @note This callback should be set and released
+   *       by @ref ucp_worker_set_am_rendezvous handler function.
+   *
+   */
+typedef ucs_status_t (*ucp_am_rendezvous_callback_t)(
+                                                 void *arg,
+                                                 void *data,
+                                                 size_t length,
+                                                 ucp_ep_h reply_ep,
+                                                 unsigned flags,
+                                                 size_t remaining_length,
+                                                 ucp_am_rendezvous_recv_t *recv
+                                                    );
 
-ucs_status_t ucp_worker_set_am_rendezvous_handler(ucp_worker_h worker, uint16_t id,
-                                       ucp_am_rendezvous_callback_t cb, void *arg,
-                                       uint32_t flags, const ucp_am_rendezvous_params_t *params );
+/**
+ * @ingroup UCP_WORKER
+ * @brief Add user defined callback for Active Message rendezvous.
+ *
+ * This routine installs a user defined callback to handle incoming Active
+ * Messages with a specific id. This callback is called whenever an Active
+ * Message that was sent from the remote peer by @ref ucp_am_rendezvous_send_nb
+ * is received on this worker, if the ucp library decides that rendezvous
+ * processing is appropriate for this active message; otherwise the callback
+ * registered with ucp_worker_set_am_handler will be driven.
+ *
+ * @param [in]  worker      UCP worker on which to set the Active Message
+ *                          handler.
+ * @param [in]  id          Active Message id.
+ * @param [in]  cb          Active Message callback. NULL to clear.
+ * @param [in]  arg         Active Message argument, which will be passed
+ *                          in to every invocation of the callback as the
+ *                          arg argument.
+ * @param [in]  flags       Dictates how an Active Message is handled on the
+ *                          remote endpoint. No flags currently defined.
+ * @param [in]  params      Tuning parameters for the active message
+ *
+ * @return error code if the worker does not support Active Messages or
+ *         requested callback flags.
+ */
+ucs_status_t ucp_worker_set_am_rendezvous_handler(
+                                       ucp_worker_h worker,
+                                       uint16_t id,
+                                       ucp_am_rendezvous_callback_t cb,
+                                       void *arg,
+                                       uint32_t flags,
+                                       const ucp_am_rendezvous_params_t *params
+                                       );
 
 /**
  * @ingroup UCP_COMM
