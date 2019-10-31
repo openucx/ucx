382775f387a0dcb7ae16414c15399081e6033d8e
dmitrygx <dmitrygla@mellanox.com>
2019-10-31 16:42:46 +0200

    UCT/IB, UCS/CONFIG: Make default PKEY - auto, improve PKEY search time

--- test/gtest/uct/ib/test_ib_pkey.cc
+++ test/gtest/uct/ib/test_ib_pkey.cc
@@ -8,6 +8,11 @@
 
 class test_uct_ib_pkey : public test_uct_ib_with_specific_port {
 public:
+    test_uct_ib_pkey() {
+        m_pkey_value = 0;
+        m_pkey_index = 0;
+    }
+
     void check_port_attr() {
         if (IBV_PORT_IS_LINK_LAYER_ETHERNET(&m_port_attr)) {
             /* no pkeys for Ethernet */
@@ -18,6 +23,9 @@ public:
     void send_recv_short() {
         create_connected_entities();
 
+        EXPECT_TRUE(check_pkey(m_e1->iface(), m_pkey_value, m_pkey_index));
+        EXPECT_TRUE(check_pkey(m_e2->iface(), m_pkey_value, m_pkey_index));
+
         test_uct_ib::send_recv_short();
 
         m_e1->destroy_eps();
@@ -26,52 +34,65 @@ public:
         m_entities.remove(m_e2);
     }
 
-    uint16_t query_pkey(uint16_t pkey_idx) {
+    uint16_t query_pkey(uint16_t pkey_idx) const {
         uint16_t pkey;
 
         if (ibv_query_pkey(m_ibctx, m_port, pkey_idx, &pkey)) {
             UCS_TEST_ABORT("Failed to query pkey on port " << m_port <<
                            " on device: " << m_dev_name);
         }
-        return ntohs(pkey) & UCT_IB_PKEY_PARTITION_MASK;
+        return ntohs(pkey);
     }
 
-    bool pkey_find() {
-        uct_ib_iface_config_t *ib_config =
-            ucs_derived_of(m_iface_config, uct_ib_iface_config_t);
+    bool check_pkey(const uct_iface_t *iface, uint16_t pkey_value,
+                    uint16_t pkey_index) const {
+        const uct_ib_iface_t *ib_iface = ucs_derived_of(iface, uct_ib_iface_t);
+        return ((pkey_value == ib_iface->pkey_value) &&
+                (pkey_index == ib_iface->pkey_index));
+    }
 
-        /* check if the configured pkey exists in the port's pkey table */
+    bool find_default_pkey(uint16_t &pkey_value, uint16_t &pkey_index) const {
         for (uint16_t table_idx = 0; table_idx < m_port_attr.pkey_tbl_len; table_idx++) {
             uint16_t pkey = query_pkey(table_idx);
-            if (pkey == ib_config->pkey_value) {
+            if (can_use_pkey(pkey)) {
+                /* found the first valid pkey with full membership */
+                pkey_value = pkey;
+                pkey_index = table_idx;
                 return true;
             }
         }
 
         return false;
     }
+
+    bool can_use_pkey(uint16_t pkey_value) const {
+        return (pkey_value && (pkey_value & UCT_IB_PKEY_MEMBERSHIP_MASK));
+    }
+
+public:
+    uint16_t m_pkey_value;
+    uint16_t m_pkey_index;
 };
 
-UCS_TEST_P(test_uct_ib_pkey, non_default_pkey) {
-    /* test with invalid pkey set (0 - trival case, start from 1) */
-    for (unsigned pkey = 1; pkey <= UCT_IB_PKEY_PARTITION_MASK; pkey++) {
-        modify_config("IB_PKEY", "0x" + ucs::to_hex_string(pkey));
-
-        if (!pkey_find()) {
-            send_recv_short();
-            break;
-        }
+UCS_TEST_P(test_uct_ib_pkey, default_pkey) {
+    if (!find_default_pkey(m_pkey_value, m_pkey_index)) {
+        UCS_TEST_SKIP_R("unable to find a valid pkey with full membership");
     }
+
+    send_recv_short();
 }
 
 UCS_TEST_P(test_uct_ib_pkey, all_avail_pkeys) {
     /* test all pkeys that are configured for the device */
     for (uint16_t table_idx = 0; table_idx < m_port_attr.pkey_tbl_len; table_idx++) {
-        uint16_t pkey = query_pkey(table_idx);
-        if (!(pkey & UCT_IB_PKEY_MEMBERSHIP_MASK)) {
+        m_pkey_value = query_pkey(table_idx);
+        if (!can_use_pkey(m_pkey_value)) {
             continue;
         }
-        modify_config("IB_PKEY", "0x" + ucs::to_hex_string(pkey));
+        modify_config("IB_PKEY", "0x" +
+                      ucs::to_hex_string(m_pkey_value &
+                                         UCT_IB_PKEY_PARTITION_MASK));
+        m_pkey_index = table_idx;
         send_recv_short();
     }
 }
