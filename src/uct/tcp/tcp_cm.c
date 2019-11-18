openeuler-mirror/kucx

UCT/TCP, UCS/SYS: Repeat connection establishment whn detected dropped conenctions

src/uct/tcp/tcp_cm.c
@@ -92,12 +92,8 @@ void uct_tcp_cm_change_conn_state(uct_tcp_ep_t *ep,
 
 static ucs_status_t uct_tcp_cm_io_err_handler_cb(void *arg, int io_errno)
 {
-    uct_tcp_ep_t *ep = (uct_tcp_ep_t*)arg;
-
-    uct_tcp_ep_dropped_connect_print_error(ep, io_errno);
-
-    /* always want to print the default error */
-    return UCS_ERR_NO_PROGRESS;
+    return uct_tcp_ep_handle_dropped_connect_error((uct_tcp_ep_t*)arg,
+                                                   io_errno);
 }
 
 /* `fmt_str` parameter has to contain "%s" to write event type */
@@ -544,6 +540,12 @@ ucs_status_t uct_tcp_cm_conn_start(uct_tcp_ep_t *ep)
     uct_tcp_iface_t *iface = ucs_derived_of(ep->super.super.iface,
                                             uct_tcp_iface_t);
     ucs_status_t status;
+
+    if (ep->conn_attempts++ > iface->config.max_conn_attempts) {
+        ucs_error("tcp_ep %p: reached maximum number of connections attempts "
+                  "(%u)", ep, iface->config.max_conn_attempts);
+        return UCS_ERR_TIMED_OUT;
+    }
 
     status = ucs_socket_connect(ep->fd, (const struct sockaddr*)&ep->peer_addr);
     if (UCS_STATUS_IS_ERR(status)) {

src/uct/tcp/tcp_iface.c
@@ -46,13 +46,18 @@ static ucs_config_field_t uct_tcp_iface_config_table[] = {
    ucs_offsetof(uct_tcp_iface_config_t, prefer_default), UCS_CONFIG_TYPE_BOOL},
 
   {"CONN_NB", "n",
-   "Enables non-blocking connection establishment. It may improve startup "
+   "Enable non-blocking connection establishment. It may improve startup "
    "time, but can lead to connection resets due to high load on TCP/IP stack",
    ucs_offsetof(uct_tcp_iface_config_t, conn_nb), UCS_CONFIG_TYPE_BOOL},
 
   {"MAX_POLL", UCS_PP_MAKE_STRING(UCT_TCP_MAX_EVENTS),
    "Number of times to poll on a ready socket. 0 - no polling, -1 - until drained",
    ucs_offsetof(uct_tcp_iface_config_t, max_poll), UCS_CONFIG_TYPE_UINT},
+
+  {UCT_TCP_CONFIG_MAX_CONN_ATTEMPTS, "5",
+   "How many connection establishment attmepts should be done if dropped "
+   "connection was detected due to lack of system resources",
+   ucs_offsetof(uct_tcp_iface_config_t, max_conn_attempts), UCS_CONFIG_TYPE_UINT},
 
   {"NODELAY", "y",
    "Set TCP_NODELAY socket option to disable Nagle algorithm. Setting this\n"
@@ -471,14 +476,15 @@ static UCS_CLASS_INIT_FUNC(uct_tcp_iface_t, uct_md_h md, uct_worker_h worker,
         return UCS_ERR_INVALID_PARAM;
     }
 
-    self->config.zcopy.max_hdr  = self->config.tx_seg_size -
-                                  self->config.zcopy.hdr_offset;
-    self->config.prefer_default = config->prefer_default;
-    self->config.conn_nb        = config->conn_nb;
-    self->config.max_poll       = config->max_poll;
-    self->sockopt.nodelay       = config->sockopt_nodelay;
-    self->sockopt.sndbuf        = config->sockopt_sndbuf;
-    self->sockopt.rcvbuf        = config->sockopt_rcvbuf;
+    self->config.zcopy.max_hdr     = self->config.tx_seg_size -
+                                     self->config.zcopy.hdr_offset;
+    self->config.prefer_default    = config->prefer_default;
+    self->config.conn_nb           = config->conn_nb;
+    self->config.max_poll          = config->max_poll;
+    self->config.max_conn_attempts = config->max_conn_attempts;
+    self->sockopt.nodelay          = config->sockopt_nodelay;
+    self->sockopt.sndbuf           = config->sockopt_sndbuf;
+    self->sockopt.rcvbuf           = config->sockopt_rcvbuf;
     ucs_list_head_init(&self->ep_list);
     kh_init_inplace(uct_tcp_cm_eps, &self->ep_cm_map);
 
@@ -683,4 +689,5 @@ out:
 }
 
 UCT_TL_DEFINE(&uct_tcp_component, tcp, uct_tcp_query_devices, uct_tcp_iface_t,
-              "TCP_", uct_tcp_iface_config_table, uct_tcp_iface_config_t);
+              UCT_TCP_CONFIG_PREFIX, uct_tcp_iface_config_table,
+              uct_tcp_iface_config_t);

openucx/ucx

src/uct/tcp/tcp_cm.c
@@ -92,12 +92,8 @@ void uct_tcp_cm_change_conn_state(uct_tcp_ep_t *ep,
 
 static ucs_status_t uct_tcp_cm_io_err_handler_cb(void *arg, int io_errno)
 {
-    uct_tcp_ep_t *ep = (uct_tcp_ep_t*)arg;
-
-    uct_tcp_ep_dropped_connect_print_error(ep, io_errno);
-
-    /* always want to print the default error */
-    return UCS_ERR_NO_PROGRESS;
+    return uct_tcp_ep_handle_dropped_connect((uct_tcp_ep_t*)arg,
+                                             io_errno);
 }
 
 /* `fmt_str` parameter has to contain "%s" to write event type */
@@ -544,6 +540,12 @@ ucs_status_t uct_tcp_cm_conn_start(uct_tcp_ep_t *ep)
     uct_tcp_iface_t *iface = ucs_derived_of(ep->super.super.iface,
                                             uct_tcp_iface_t);
     ucs_status_t status;
+
+    if (ep->conn_retries++ > iface->config.max_conn_retries) {
+        ucs_error("tcp_ep %p: reached maximum number of connection retries "
+                  "(%u)", ep, iface->config.max_conn_retries);
+        return UCS_ERR_TIMED_OUT;
+    }
 
     status = ucs_socket_connect(ep->fd, (const struct sockaddr*)&ep->peer_addr);
     if (UCS_STATUS_IS_ERR(status)) {

src/uct/tcp/tcp_iface.c
@@ -46,13 +46,18 @@ static ucs_config_field_t uct_tcp_iface_config_table[] = {
    ucs_offsetof(uct_tcp_iface_config_t, prefer_default), UCS_CONFIG_TYPE_BOOL},
 
   {"CONN_NB", "n",
-   "Enables non-blocking connection establishment. It may improve startup "
+   "Enable non-blocking connection establishment. It may improve startup "
    "time, but can lead to connection resets due to high load on TCP/IP stack",
    ucs_offsetof(uct_tcp_iface_config_t, conn_nb), UCS_CONFIG_TYPE_BOOL},
 
   {"MAX_POLL", UCS_PP_MAKE_STRING(UCT_TCP_MAX_EVENTS),
    "Number of times to poll on a ready socket. 0 - no polling, -1 - until drained",
    ucs_offsetof(uct_tcp_iface_config_t, max_poll), UCS_CONFIG_TYPE_UINT},
+
+  {UCT_TCP_CONFIG_MAX_CONN_RETRIES, "5",
+   "How many connection establishment attmepts should be done if dropped "
+   "connection was detected due to lack of system resources",
+   ucs_offsetof(uct_tcp_iface_config_t, max_conn_retries), UCS_CONFIG_TYPE_UINT},
 
   {"NODELAY", "y",
    "Set TCP_NODELAY socket option to disable Nagle algorithm. Setting this\n"
@@ -471,14 +476,15 @@ static UCS_CLASS_INIT_FUNC(uct_tcp_iface_t, uct_md_h md, uct_worker_h worker,
         return UCS_ERR_INVALID_PARAM;
     }
 
-    self->config.zcopy.max_hdr  = self->config.tx_seg_size -
-                                  self->config.zcopy.hdr_offset;
-    self->config.prefer_default = config->prefer_default;
-    self->config.conn_nb        = config->conn_nb;
-    self->config.max_poll       = config->max_poll;
-    self->sockopt.nodelay       = config->sockopt_nodelay;
-    self->sockopt.sndbuf        = config->sockopt_sndbuf;
-    self->sockopt.rcvbuf        = config->sockopt_rcvbuf;
+    self->config.zcopy.max_hdr     = self->config.tx_seg_size -
+                                     self->config.zcopy.hdr_offset;
+    self->config.prefer_default    = config->prefer_default;
+    self->config.conn_nb           = config->conn_nb;
+    self->config.max_poll          = config->max_poll;
+    self->config.max_conn_retries  = config->max_conn_retries;
+    self->sockopt.nodelay          = config->sockopt_nodelay;
+    self->sockopt.sndbuf           = config->sockopt_sndbuf;
+    self->sockopt.rcvbuf           = config->sockopt_rcvbuf;
     ucs_list_head_init(&self->ep_list);
     kh_init_inplace(uct_tcp_cm_eps, &self->ep_cm_map);
 
@@ -683,4 +689,5 @@ out:
 }
 
 UCT_TL_DEFINE(&uct_tcp_component, tcp, uct_tcp_query_devices, uct_tcp_iface_t,
-              "TCP_", uct_tcp_iface_config_table, uct_tcp_iface_config_t);
+              UCT_TCP_CONFIG_PREFIX, uct_tcp_iface_config_table,
+              uct_tcp_iface_config_t);