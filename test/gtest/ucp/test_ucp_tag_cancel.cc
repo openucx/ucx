5d83d7973ff22cede90093326d93c00997b35014
Mikhail Brinskii <mikhailb@mellanox.com>
2019-11-23 20:16:00 +0200

    UCP/TAG: Fix ucp_request_cancel for matched reqs

--- test/gtest/ucp/test_ucp_tag_cancel.cc
+++ test/gtest/ucp/test_ucp_tag_cancel.cc
@@ -1,5 +1,5 @@
 /**
-* Copyright (C) Mellanox Technologies Ltd. 2001-2014.  ALL RIGHTS RESERVED.
+* Copyright (C) Mellanox Technologies Ltd. 2001-2019.  ALL RIGHTS RESERVED.
 * Copyright (C) UT-Battelle, LLC. 2015. ALL RIGHTS RESERVED.
 *
 * See file LICENSE for terms.
@@ -9,8 +9,20 @@
 
 #include <common/test_helpers.h>
 
+extern "C" {
+#include <ucp/tag/tag_match.h>
+}
 
 class test_ucp_tag_cancel : public test_ucp_tag {
+public:
+    void release_send_req(request *sreq) {
+        if (sreq != NULL) {
+            wait(sreq);
+            EXPECT_TRUE(sreq->completed);
+            EXPECT_EQ(UCS_OK, sreq->status);
+            request_free(sreq);
+        }
+    }
 };
 
 UCS_TEST_P(test_ucp_tag_cancel, cancel_exp) {
@@ -31,5 +43,43 @@ UCS_TEST_P(test_ucp_tag_cancel, cancel_exp) {
     EXPECT_EQ(0ul, recv_data);
     request_release(req);
 }
+
+// Test that cancelling already matched (but not yet completed) request does
+// not produce any error. GH bug #4490.
+UCS_TEST_P(test_ucp_tag_cancel, cancel_matched, "RNDV_THRESH=32K") {
+    uint64_t small_data = 0;
+    ucp_tag_t tag       = 0xfafa;
+    size_t size         = 50000;
+
+    std::vector<char> sbuf(size, 0);
+    std::vector<char> rbuf(size, 0);
+
+    request *rreq1 = recv_nb(&rbuf[0], rbuf.size(), DATATYPE, tag,
+                             UCP_TAG_MASK_FULL);
+    request *rreq2 = recv_nb(&small_data, sizeof(small_data), DATATYPE, tag,
+                             UCP_TAG_MASK_FULL);
+
+    request *sreq1 = send_nb(&sbuf[0], sbuf.size(), DATATYPE, tag);
+    request *sreq2 = send_nb(&small_data, sizeof(small_data), DATATYPE, tag);
+
+    wait(rreq2);
+    EXPECT_EQ(UCS_OK, rreq2->status);
+
+    if (!rreq1->completed) {
+        ucp_request_cancel(receiver().worker(), rreq1);
+    } else {
+        UCS_TEST_MESSAGE << "nothing to cancel";
+    }
+
+    wait(rreq1);
+    EXPECT_EQ(UCS_OK, rreq1->status);
+    EXPECT_TRUE(rreq1->completed);
+
+    request_free(rreq1);
+    request_free(rreq2);
+    release_send_req(sreq1);
+    release_send_req(sreq2);
+}
+
 
 UCP_INSTANTIATE_TEST_CASE(test_ucp_tag_cancel)
