064ac4eec40e006477f661345b964bcd18c33b33
Evgeny Leksikov <evgenylek@mellanox.com>
2020-01-29 22:32:02 +0200

    UCP/WIREUP/CM: remove uct_cm_ep from arg to CM progress CBs

--- src/ucp/core/ucp_ep.c
+++ src/ucp/core/ucp_ep.c
@@ -823,9 +823,13 @@ static void ucp_ep_set_close_request(ucp_ep_h ep, ucp_request_t *request,
 
 static void ucp_ep_close_flushed_callback(ucp_request_t *req)
 {
-    ucp_ep_h ep = req->send.ep;
+    ucp_ep_h ep                = req->send.ep;
+    ucs_async_context_t *async = &ep->worker->async;
 
-    UCS_ASYNC_BLOCK(&ep->worker->async);
+    UCS_ASYNC_BLOCK(async);
+
+    ucs_debug("ep %p: flags 0x%x close flushed callback for request %p", ep,
+              ep->flags, req);
     if (ucp_ep_is_cm_local_connected(ep)) {
         /* Now, when close flush is completed and we are still locally connected,
          * we have to notify remote side */
@@ -834,11 +838,11 @@ static void ucp_ep_close_flushed_callback(ucp_request_t *req)
             /* Wait disconnect notification from remote side to complete this
              * request */
             ucp_ep_set_close_request(ep, req, "close flushed callback");
-            UCS_ASYNC_UNBLOCK(&ep->worker->async);
+            UCS_ASYNC_UNBLOCK(async);
             return;
         }
     }
-    UCS_ASYNC_UNBLOCK(&ep->worker->async);
+    UCS_ASYNC_UNBLOCK(async);
 
     /* If a flush is completed from a pending/completion callback, we need to
      * schedule slow-path callback to release the endpoint later, since a UCT
