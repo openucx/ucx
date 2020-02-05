66e703f7db9d4d33b13326f0ca6b4f96cbaa219e
Andrey Maslennikov <andreyma@mellanox.com>
2020-02-05 14:00:11 +0300

    AZP/CI: add comments

--- contrib/test_jenkins.sh
+++ contrib/test_jenkins.sh
@@ -45,7 +45,9 @@ fi
 
 
 #
-# Set affinity to 2 cores according to Jenkins executor number
+# Set affinity to 2 cores according to Jenkins executor number.
+# Affinity is inherited fomr agent in Azure CI.
+# TODO: remove or rename after CI migration.
 #
 if [ -n "$EXECUTOR_NUMBER" ] && [ -n "$JENKINS_RUN_TESTS" ]
 then
