c89305284ae479e0e85a52f839fdf4c44e770aae
Andrey Maslennikov <andreyma@mellanox.com>
2020-03-26 19:35:34 +0200

    CI: take into account number of pinned threads for parallel job

--- contrib/test_jenkins.sh
+++ contrib/test_jenkins.sh
@@ -64,6 +64,8 @@ num_cpus=$(lscpu -p | grep -v '^#' | wc -l)
 [ -z $num_cpus ] && num_cpus=1
 parallel_jobs=4
 [ $parallel_jobs -gt $num_cpus ] && parallel_jobs=$num_cpus
+num_pinned_threads=$(taskset -pc $$ | awk -F': ' '{print $2}' | perl -lne 's/-/../g; @a=(eval); print scalar(@a)')
+[ $parallel_jobs -gt $num_pinned_threads ] && parallel_jobs=$num_pinned_threads
 
 MAKE="make"
 MAKEP="make -j${parallel_jobs}"
