0de59a9d3f915499fc2cb3258edb0cf3b86ef90d
Yossi Itigin <yosefe@mellanox.com>
2020-04-17 20:50:29 +0300

    UCT/UGNI: Check return value from fscanf() to fix compile error

--- src/uct/ugni/base/ugni_device.c
+++ src/uct/ugni/base/ugni_device.c
@@ -420,7 +420,9 @@ ucs_status_t uct_ugni_create_cdm(uct_ugni_cdm_t *cdm, uct_ugni_device_t *device,
 
     fh = fopen ("/proc/sys/kernel/pid_max", "r");
     if (NULL != fh) {
-        if (fscanf (fh, "%d", &pid_max));
+        if (fscanf (fh, "%d", &pid_max) != 1) {
+            ucs_debug("cound not read pid_max, using default");
+        }
         fclose (fh);
     }
 
