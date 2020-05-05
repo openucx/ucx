68f55ca427e4895376854a8449902675ede2ec33
Akshay Venkatesh <akvenkatesh@nvidia.com>
2020-05-05 16:37:51 -0700

    UCS/TOPO: Change ucs_sys_device_t from a structure to unsigned int

--- test/gtest/ucs/test_topo.cc
+++ test/gtest/ucs/test_topo.cc
@@ -14,15 +14,25 @@ class test_topo : public ucs::test {
 
 UCS_TEST_F(test_topo, find_device_by_bus_id) {
     ucs_status_t status;
+    ucs_sys_device_t unknown_dev;
 
-    status = ucs_topo_find_device_by_bus_id(NULL, NULL);
+    status = ucs_topo_find_device_by_bus_id(&ucs_sys_bus_id_unknown, &unknown_dev);
     ASSERT_UCS_OK(status);
 }
 
 UCS_TEST_F(test_topo, get_distance) {
     ucs_status_t status;
+    ucs_sys_device_t unknown_dev1;
+    ucs_sys_device_t unknown_dev2;
+    ucs_sys_dev_distance_t distance;
 
-    status = ucs_topo_get_distance(NULL, NULL, NULL);
+    status = ucs_topo_find_device_by_bus_id(&ucs_sys_bus_id_unknown, &unknown_dev1);
+    ASSERT_UCS_OK(status);
+
+    status = ucs_topo_find_device_by_bus_id(&ucs_sys_bus_id_unknown, &unknown_dev2);
+    ASSERT_UCS_OK(status);
+
+    status = ucs_topo_get_distance(unknown_dev1, unknown_dev2, &distance);
     ASSERT_UCS_OK(status);
 }
 
