b2e66dac9ab1304389996dc00402b478f3e86391
Yossi Itigin <yosefe@mellanox.com>
2020-06-15 19:30:33 +0300

    UCP/PROTO: Introduce protocol definition template

---
+++ src/ucp/proto/proto.c
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) Mellanox Technologies Ltd. 2020.  ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#include "proto.h"
+
+
+const ucp_proto_t *ucp_protocols[UCP_PROTO_MAX_COUNT] = {};
+unsigned ucp_protocols_count = 0;
+
