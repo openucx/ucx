a8136140c306955d40904c45b2a7a3767a27b99d
Yossi Itigin <yosefe@mellanox.com>
2020-06-20 22:41:29 +0300

    TEST/UCP: Add placeholder test for new protocols

---
+++ test/gtest/ucp/test_ucp_proto.cc
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) Mellanox Technologies Ltd. 2020.  ALL RIGHTS RESERVED.
+ *
+ * See file LICENSE for terms.
+ */
+
+#include <common/test.h>
+
+extern "C" {
+#include <ucp/proto/proto.h>
+}
+
+class test_proto : public ucs::test {
+};
+
