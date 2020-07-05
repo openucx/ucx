ae33fd3413d75e945d6ac4829da778ad2e84b748
Yossi Itigin <yosefe@mellanox.com>
2020-07-06 02:00:24 +0300

    UCP/RNDV/TEST: Code review fixes

--- src/ucp/tag/rndv.c
+++ src/ucp/tag/rndv.c
@@ -512,6 +512,7 @@ UCS_PROFILE_FUNC(ucs_status_t, ucp_rndv_progress_rma_get_zcopy, (self),
         ucp_rkey_destroy(rndv_req->send.rndv_get.rkey);
         ucp_rndv_recv_data_init(rndv_req->send.rndv_get.rreq,
                                 rndv_req->send.length);
+        /* Update statistics counters from get_zcopy to rtr */
         UCP_WORKER_STAT_RNDV(ep->worker, GET_ZCOPY, -1);
         UCP_WORKER_STAT_RNDV(ep->worker, RTR,       +1);
         ucp_rndv_req_send_rtr(rndv_req, rndv_req->send.rndv_get.rreq,
@@ -1165,7 +1166,7 @@ UCS_PROFILE_FUNC_VOID(ucp_rndv_matched, (worker, rreq, rndv_rts_hdr),
      * configured to PUT, or GET rndv mode is unsupported - send an RTR and
      * the sender will send the data with active message or put_zcopy. */
     ucp_rndv_recv_data_init(rreq, rndv_rts_hdr->size);
-    UCP_WORKER_STAT_RNDV(ep->worker, RTR, +1);
+    UCP_WORKER_STAT_RNDV(ep->worker, RTR, 1);
     ucp_rndv_req_send_rtr(rndv_req, rreq, rndv_rts_hdr->sreq.reqptr,
                           rndv_rts_hdr->size, 0ul);
 
