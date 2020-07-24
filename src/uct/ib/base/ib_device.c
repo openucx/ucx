cb988a4ffaa21e5808603054a9ee07e6c3fdf6ad
Akshay Venkatesh <akvenkatesh@nvidia.com>
2020-07-24 07:46:08 -0700

    UCT/IB/BASE: Handle non-mlx devices

--- src/uct/ib/base/ib_device.c
+++ src/uct/ib/base/ib_device.c
@@ -782,9 +782,8 @@ ucs_status_t uct_ib_device_bus(uct_ib_device_t *dev, int port_num,
     /* realpath name is of form /sys/devices/.../0000:05:00.0/infiniband/mlx5_0
      * and bus_id is constructed from 0000:05:00.0 */
 
-    /* Make sure there is /infiniband/mlx5 substring in ib_realpath*/
-    /* doesn't handle non-mlx devices */
-    tmp = strstr(ib_realpath, "/infiniband/mlx5");
+    /* Make sure there is /infiniband substring in ib_realpath*/
+    tmp = strstr(ib_realpath, "/infiniband");
     if (NULL == tmp) {
         return UCS_ERR_NO_RESOURCE;
     }
