c2f03eb857d154141a6b5dfe522cf24554331fff
binl <bilei@nvidia.com>
2020-09-23 10:07:36 +0300

    TEST/APPS/IO_DEMO: Fix minor issue.

--- test/apps/iodemo/io_demo.cc
+++ test/apps/iodemo/io_demo.cc
@@ -717,6 +717,7 @@ public:
         for (size_t i = 0; i < _conn.size(); i++) {
             if (_conn[i].conn != NULL) {
                 delete _conn[i].conn;
+                _conn[i].conn = NULL;
             }
         }
         return (_status == OK) || (_status == RUNTIME_EXCEEDED);
