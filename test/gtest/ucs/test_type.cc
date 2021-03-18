9b9d2d36543aab3706f38e3fb88d4888160f5500
Leonid Genkin <lgenkin@nvidia.com>
2021-03-18 08:06:31 +0200

    UCS/TYPE: Support compression of double to char

--- test/gtest/ucs/test_type.cc
+++ test/gtest/ucs/test_type.cc
@@ -9,6 +9,7 @@ extern "C" {
 #include <ucs/type/cpu_set.h>
 #include <ucs/type/init_once.h>
 #include <ucs/type/status.h>
+#include <ucs/type/float8.h>
 }
 
 #include <time.h>
@@ -46,6 +47,42 @@ UCS_TEST_F(test_type, status) {
     EXPECT_TRUE(UCS_PTR_IS_PTR(ptr));
     EXPECT_FALSE(UCS_PTR_IS_PTR(NULL));
     EXPECT_NE(UCS_OK, UCS_PTR_STATUS(ptr));
+}
+
+/* Represents latency (in ns) */
+UCS_FP8_DECLARE_TYPE(LATENCY, UCS_BIT(7), UCS_BIT(20))
+
+UCS_TEST_F(test_type, pack_float) {
+    const std::size_t values_size    = 10;
+    double values_array[values_size] = {
+        130, 135.1234, 140, 200, 400, 1000, 10000, 100000, 1000000, 1000000
+    };
+    std::vector<double> values(values_array, values_array + values_size);
+    float unpacked;
+
+    /* 0 -> 0 */
+    unpacked = UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, 0));
+    EXPECT_EQ(unpacked, 0);
+
+    /* NaN -> NaN */
+    unpacked = UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, NAN));
+    EXPECT_TRUE(isnan(unpacked));
+
+    /* Below min -> min */
+    EXPECT_EQ(UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, UCS_BIT(7))),
+              UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, 15)));
+
+    /* Precision test throughout the whole range */
+    for (std::vector<double>::const_iterator it = values.begin();
+         it < values.end(); it++) {
+        unpacked = UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, *it));
+        ucs_assert((UCS_FP8_PRECISION < unpacked / *it) &&
+                   (unpacked / *it <= 1));
+    }
+
+    /* Above max -> max */
+    EXPECT_EQ(UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, UCS_BIT(20))),
+              UCS_FP8_UNPACK(LATENCY, UCS_FP8_PACK(LATENCY, 200000000)));
 }
 
 class test_init_once: public test_type {
