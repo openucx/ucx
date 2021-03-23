7ddd7bc354740d247703250a8a8277f172c314aa
dmitrygx <dmitrygla@mellanox.com>
2021-03-23 22:15:47 +0000

    UCP/CORE/RNDV: Check that request ID released when completing request + fix in RNDV and Eager/Sync

--- src/ucp/tag/eager_rcv.c
+++ src/ucp/tag/eager_rcv.c
@@ -259,6 +259,7 @@ UCS_PROFILE_FUNC(ucs_status_t, ucp_eager_offload_sync_ack_handler,
     ucs_queue_for_each_safe(sreq, iter, queue, send.tag_offload.queue) {
         if ((sreq->send.tag_offload.ssend_tag == rep_hdr->sender_tag) &&
             (ucp_ep_local_id(sreq->send.ep) == rep_hdr->ep_id)) {
+            ucp_request_id_release(sreq);
             ucp_tag_eager_sync_completion(sreq,
                                           UCP_REQUEST_FLAG_REMOTE_COMPLETED,
                                           UCS_OK);
