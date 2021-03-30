3476f17d3619ae6c130639fa08c69f2bdabae00f
dmitrygx <dmitrygla@mellanox.com>
2021-03-30 12:45:29 +0000

    UCT/RC/DC: Check that we don't have any pending operations after flush(CANCEL)

--- src/uct/ib/rc/base/rc_ep.c
+++ src/uct/ib/rc/base/rc_ep.c
@@ -364,12 +364,18 @@ ucs_arbiter_cb_result_t uct_rc_ep_process_pending(ucs_arbiter_t *arbiter,
     } else if (!uct_rc_iface_has_tx_resources(iface)) {
         /* No iface resources */
         return UCS_ARBITER_CB_RESULT_STOP;
-    } else {
-        /* No ep resources */
-        ucs_assertv(!uct_rc_ep_has_tx_resources(ep),
-                    "pending callback returned error but send resources are available");
-        return UCS_ARBITER_CB_RESULT_DESCHED_GROUP;
     }
+
+    
+    /* No any other pending operations (except no-op, flush(CANEL), and others
+     * which don't consume TX resources) operations are allowed to be still
+     * scheduled on an arbiter group for which flush(CANCEL) was done */
+    ucs_assert(!(ep->flags & UCT_RC_EP_FLAG_FLUSH_CANCEL));
+
+    /* No ep resources */
+    ucs_assertv(!uct_rc_ep_has_tx_resources(ep),
+                "pending callback returned error but send resources are available");
+    return UCS_ARBITER_CB_RESULT_DESCHED_GROUP;
 }
 
 ucs_arbiter_cb_result_t uct_rc_ep_arbiter_purge_cb(ucs_arbiter_t *arbiter,
