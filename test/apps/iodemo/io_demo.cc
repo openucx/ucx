9c2af23d37a752dd168c41559424cb2a378d0994
Changcheng Liu <jerrliu@nvidia.com>
2021-05-12 05:23:32 +0000

    TEST/IO_DEMO: extent iomsg_t field for tag iov operation
    1. data_type:
       default: ucp_dt_make_contig(1)
       other option: ucp_dt_make_iov()
    2. dt_iov_size: effective when data_type is ucp_dt_make_iov()
       the arrary size of dt_iov
    3. dt_iov: ucp_dt_iov_t::length
    4. get_iomsg_size: API to get iosmg effective data size
    
    Note: It need add struct tag here, or it'll build fail on Fedora34
     #error: add a tag name here [-Werror,-Wnon-c-typedef-for-linkage]
     #     typedef struct {
     #                   ^
     #                    iomsg_t
     #note: type is not C-compatible due to this member declaration
     #         size_t get_iomsg_size(void) const {
     #         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #     } iomsg_t;
     #       ^
    Signed-off-by: Changcheng Liu <jerrliu@nvidia.com>

--- test/apps/iodemo/io_demo.cc
+++ test/apps/iodemo/io_demo.cc
@@ -354,10 +354,17 @@ const unsigned IoDemoRandom::_M = 0x7fffffffU;
 class P2pDemoCommon : public UcxContext {
 public:
     /* IO header */
-    typedef struct {
+    typedef struct iomsg {
         uint32_t    sn;
         uint8_t     op;
         uint64_t    data_size;
+        ucp_datatype_t data_type;
+        uint8_t     dt_iov_size;
+        size_t      dt_iov[];
+        size_t get_iomsg_size(void) const {
+            return ucs_offsetof(iomsg, dt_iov) +
+                   dt_iov_size * sizeof(dt_iov[0]);
+        }
     } iomsg_t;
 
 protected:
@@ -715,8 +722,9 @@ protected:
 
     static void validate(const iomsg_t *msg, size_t iomsg_size) {
         unsigned seed   = msg->sn;
-        const void *buf = msg + 1;
-        size_t buf_size = iomsg_size - sizeof(*msg);
+        const void *buf = reinterpret_cast<const void*>(msg->dt_iov +
+                                                        msg->dt_iov_size);
+        size_t buf_size = iomsg_size - msg->get_iomsg_size();
 
         size_t err_pos = IoDemoRandom::validate(seed, buf, buf_size,
                                                 UCS_MEMORY_TYPE_HOST);
@@ -737,8 +745,9 @@ protected:
 
 private:
     bool send_io_message(UcxConnection *conn, IoMessage *msg) {
+        const iomsg_t *m = reinterpret_cast<iomsg_t *>(msg->buffer());
         VERBOSE_LOG << "sending IO " << io_op_names[msg->msg()->op] << ", sn "
-                    << msg->msg()->sn << " size " << sizeof(iomsg_t);
+                    << msg->msg()->sn << " size " << m->get_iomsg_size();
 
         /* send IO_READ_COMP as a data since the transaction must be matched
          * by sn on receiver side */
