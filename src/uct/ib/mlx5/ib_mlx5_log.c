a1f0aa9ec6902a5ce38d8f023a3e626d89b80aaa
Sergey Oblomov <sergeyo@nvidia.com>
2021-06-15 09:11:37 +0300

    COVERITY: suppressed false positives
    - there is set of coverity false positives
    - suppressed

--- src/uct/ib/mlx5/ib_mlx5_log.c
+++ src/uct/ib/mlx5/ib_mlx5_log.c
@@ -437,6 +437,7 @@ void uct_ib_mlx5_av_dump(char *buf, size_t max,
     p    = buf;
     endp = buf + max - 1;
 
+    /* cppcheck-suppress[uninitvar] */
     snprintf(p, endp - p, " [rqpn 0x%x",
              ntohl(base_av->dqp_dct & ~UCT_IB_MLX5_EXTENDED_UD_AV));
     p += strlen(p);
@@ -446,6 +447,7 @@ void uct_ib_mlx5_av_dump(char *buf, size_t max,
         p += strlen(p);
     }
 
+    /* cppcheck-suppress[uninitvar] */
     if (base_av->dqp_dct & UCT_IB_MLX5_EXTENDED_UD_AV) {
         if (is_eth || (grh_av->grh_gid_fl & UCT_IB_MLX5_AV_GRH_PRESENT)) {
             if (is_eth) {
