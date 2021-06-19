c57022bbb824def9e5bbf6414238dbdbd5e46b2f
Peter Rudenko <peterr@mellanox.com>
2021-06-19 12:25:53 +0300

    UCP/WIREUP_EP: initialize client_id.

--- src/ucp/wireup/wireup_cm.c
+++ src/ucp/wireup/wireup_cm.c
@@ -298,8 +298,8 @@ ucp_cm_ep_priv_data_pack(ucp_ep_h ep, const ucp_tl_bitmap_t *tl_bitmap,
     wireup_ep = ucp_ep_get_cm_wireup_ep(ep);
     user_data.client_id = wireup_ep->client_id;
     // Write user data to the end of sa_data and ucp_address block.
-    memcpy((char*)(sa_data + 1) + ucp_addr_size,
-           &user_data, sizeof(user_data));
+    memcpy(UCS_PTR_BYTE_OFFSET(sa_data + 1, ucp_addr_size),
+           &user_data, sizeof(ucp_wireup_user_data_t));
 
     *data_buf_p        = sa_data;
     *data_buf_length_p = ucp_cm_priv_data_length(ucp_addr_size);
