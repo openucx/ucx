b77e7722293332e2fbbc2cb1f0015f864dbfd349
Evgeny Leksikov <evgenylek@mellanox.com>
2021-08-20 15:30:06 +0300

    UCP/API: extend ucp_ep_query for debug info string

--- src/ucp/api/ucp.h
+++ src/ucp/api/ucp.h
@@ -4554,7 +4554,9 @@ ucs_status_ptr_t ucp_worker_flush_nbx(ucp_worker_h worker,
 enum ucp_ep_attr_field {
     UCP_EP_ATTR_FIELD_NAME            = UCS_BIT(0), /**< UCP endpoint name */
     UCP_EP_ATTR_FIELD_LOCAL_SOCKADDR  = UCS_BIT(1), /**< Sockaddr used by the endpoint */
-    UCP_EP_ATTR_FIELD_REMOTE_SOCKADDR = UCS_BIT(2)  /**< Sockaddr the endpoint is connected to */
+    UCP_EP_ATTR_FIELD_REMOTE_SOCKADDR = UCS_BIT(2), /**< Sockaddr the endpoint is connected to */
+    UCP_EP_ATTR_FIELD_DEBUG_STR       = UCS_BIT(3), /**< Available debug info in string format */
+    UCP_EP_ATTR_FIELD_DEBUG_STR_MAX   = UCS_BIT(4)  /**< Debug string buffer size */
 };
 
 
@@ -4595,6 +4597,18 @@ typedef struct ucp_ep_attr {
      * UCS_ERR_NOT_CONNECTED will be returned.
      */
     struct sockaddr_storage remote_sockaddr;
+
+     /**
+      * Available debug info in string format which fits into
+      * @ref ucp_ep_attr_t::debug_str_max.
+      */
+    char                    *debug_str;
+
+    /**
+     * Size of debug string buffer @ref ucp_ep_attr_t::debug_str including
+     * null-terminator.
+     */
+    size_t                  debug_str_max;
 } ucp_ep_attr_t;
 
 
