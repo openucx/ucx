0b7106a7cbaa0176ab1f74009a760cacda27ae52
Evgeny Leksikov <evgenylek@mellanox.com>
2021-08-20 15:31:50 +0300

    GTEST: extend testing of ucp_ep_query

--- test/gtest/ucp/test_ucp_sockaddr.cc
+++ test/gtest/ucp/test_ucp_sockaddr.cc
@@ -558,9 +558,14 @@ public:
                               m_test_addr.get_port());
         EXPECT_EQ(m_test_addr, attr.remote_sockaddr);
 
+        std::vector<char> debug_info(UCS_KBYTE);
         memset(&attr, 0, sizeof(attr));
-        attr.field_mask = UCP_EP_ATTR_FIELD_LOCAL_SOCKADDR |
-                          UCP_EP_ATTR_FIELD_REMOTE_SOCKADDR;
+        attr.field_mask    = UCP_EP_ATTR_FIELD_LOCAL_SOCKADDR  |
+                             UCP_EP_ATTR_FIELD_REMOTE_SOCKADDR |
+                             UCP_EP_ATTR_FIELD_DEBUG_STR       |
+                             UCP_EP_ATTR_FIELD_DEBUG_STR_MAX;
+        attr.debug_str     = &debug_info[0];
+        attr.debug_str_max = debug_info.size();
         status = ucp_ep_query(sender().ep(), &attr);
         ASSERT_UCS_OK(status);
 
@@ -570,8 +575,12 @@ public:
         ucs_sockaddr_set_port((struct sockaddr*)&attr.local_sockaddr,
                               m_test_addr.get_port());
         EXPECT_EQ(m_test_addr, attr.local_sockaddr);
+
+        std::string debug_str(attr.debug_str);
+        EXPECT_LT(debug_str.length(), attr.debug_str_max);
+        UCS_TEST_MESSAGE << "EP debug string: \"" << debug_str << "\"";
     }
-    
+
     void one_sided_disconnect(entity &e, enum ucp_ep_close_mode mode) {
         void *req           = e.disconnect_nb(0, 0, mode);
         ucs_time_t deadline = ucs::get_deadline();
