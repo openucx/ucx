a3c05968ba16cfb0ac38e7c94d5ae139b865571b
Changcheng Liu <jerrliu@nvidia.com>
2021-09-06 16:45:40 +0800

    UCT: ECE intersection
    Signed-off-by: Changcheng Liu <jerrliu@nvidia.com>

--- src/uct/ib/base/ece.h
+++ src/uct/ib/base/ece.h
@@ -7,7 +7,10 @@
 #ifndef MLX5_ECE_H_
 #define MLX5_ECE_H_
 
+#include <stdint.h>
+#include <infiniband/verbs.h>
 #include <ucs/config/types.h>
+#include <ucs/debug/assert.h>
 
 enum  {
     MLX5_ECE_DISABLED  = 0,
@@ -20,5 +23,48 @@ typedef struct mlx5_ece_cfg {
     ucs_on_off_auto_value_t ece_enable;
     ucs_on_off_auto_value_t ece_sr;
 } mlx5_ece_cfg_t;
+
+union ece_t {
+    struct {
+        uint32_t sr  : 1;
+        uint32_t rsv : 27;
+        uint32_t ver : 4;
+    } field;
+
+    uint32_t val;
+};
+
+typedef struct mlx5_ece {
+    uint32_t ece_enable;
+
+    union ece_t ece;
+} mlx5_ece_t;
+
+/**
+ * ece intersection
+ */
+static UCS_F_ALWAYS_INLINE
+uint32_t ece_int(uint32_t ece_val0, uint32_t ece_val1)
+{
+    union ece_t ece0, ece1, ece_rst;
+
+    ece0.val    = ece_val0;
+    ece1.val    = ece_val1;
+    ece_rst.val = 0;
+
+    ucs_assert(ece0.field.ver != 1 && ece1.field.ver != 1);
+
+    if (ece0.field.ver == 0 || ece1.field.ver == 0) {
+        return ece_rst.val;
+    }
+
+    // selective repeat
+    ece_rst.field.sr = ece0.field.sr & ece1.field.sr;
+
+    // ece version
+    ece_rst.field.ver = MLX5_ECE_VER_SR;
+
+    return ece_rst.val;
+}
 
 #endif
