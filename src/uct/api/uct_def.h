a249d8636d61e6ed4b0013bbdf5f11e6a75e5ad4
Changcheng Liu <jerrliu@nvidia.com>
2021-09-16 00:13:35 +0800

    UCT: add ece into uct_cm_remote_data for remote ece
    Signed-off-by: Changcheng Liu <jerrliu@nvidia.com>

--- src/uct/api/uct_def.h
+++ src/uct/api/uct_def.h
@@ -265,7 +265,10 @@ enum uct_cm_remote_data_field {
     UCT_CM_REMOTE_DATA_FIELD_CONN_PRIV_DATA        = UCS_BIT(2),
 
     /** Enables @ref uct_cm_remote_data::conn_priv_data_length */
-    UCT_CM_REMOTE_DATA_FIELD_CONN_PRIV_DATA_LENGTH = UCS_BIT(3)
+    UCT_CM_REMOTE_DATA_FIELD_CONN_PRIV_DATA_LENGTH = UCS_BIT(3),
+
+    /** Enables @ref uct_cm_remote_data::ece */
+    UCT_CM_REMOTE_DATA_FIELD_OOB_ECE               = UCS_BIT(4)
 };
 
 
@@ -304,6 +307,11 @@ typedef struct uct_cm_remote_data {
      * Length of the received data from the peer.
      */
     size_t                  conn_priv_data_length;
+
+    /**
+     * OOB ECE of the remote peer.
+     */
+    uint32_t                ece;
 } uct_cm_remote_data_t;
 
 
