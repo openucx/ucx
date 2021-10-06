159e639c5e044508233ab7e1bfe5c03b2afbe203
Akshay Venkatesh <akvenkatesh@nvidia.com>
2021-10-06 13:19:38 -0700

    UCP/PROTO: initialize rkey field to 0

--- src/ucp/proto/proto_common.c
+++ src/ucp/proto/proto_common.c
@@ -498,6 +498,7 @@ ucp_proto_common_buffer_copy_time(ucp_worker_h worker, const char *title,
                                    UCT_PERF_ATTR_FIELD_BANDWIDTH;
     perf_attr.local_memory_type  = local_mem_type;
     perf_attr.remote_memory_type = remote_mem_type;
+    perf_attr.rkey               = 0;
     perf_attr.operation          = memtype_op;
 
     switch (memtype_op) {
