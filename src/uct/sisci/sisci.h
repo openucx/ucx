65079b936f3e4fb31b6f80360a2fe32ba42e291a
sigurson <sigurson@ifi.uio.no>
2021-11-24 16:26:13 +0100

    mini 6

--- src/uct/sisci/sisci.h
+++ src/uct/sisci/sisci.h
@@ -32,10 +32,9 @@ typedef struct uct_sisci_iface {
 } uct_sisci_iface_t;
 
 ucs_status_t
-uct_sisci_base_query_tl_devices(uct_md_h md, uct_tl_device_resource_t **tl_devices_p,
+uct_sisci_query_tl_devices(uct_md_h md, uct_tl_device_resource_t **tl_devices_p,
                              unsigned *num_tl_devices_p);
 
-/*
 ucs_status_t uct_sisci_iface_get_device_address(uct_iface_t *tl_iface,
                                              uct_device_addr_t *addr);
 
@@ -48,6 +47,7 @@ size_t uct_sisci_iface_get_device_addr_len();
 
 ucs_status_t uct_sisci_ep_fence(uct_ep_t *tl_ep, unsigned flags);
 
+/*
 UCS_CLASS_DECLARE(uct_sisci_iface_t, uct_iface_ops_t*, uct_iface_internal_ops_t*,
                   uct_md_h, uct_worker_h, const uct_iface_params_t*,
                   const uct_iface_config_t*);
