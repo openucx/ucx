15978d2836fa6ee8e9dc89ef86df01bcff2e369d
sigurson <sigurson@ifi.uio.no>
2021-11-24 15:05:38 +0100

    mini 3

--- src/uct/sisci/sisci.h
+++ src/uct/sisci/sisci.h
@@ -14,10 +14,42 @@ typedef uint64_t uct_sisci_iface_addr_t;
 
 void sisci_testing();
 
+// iface file contents
+
+extern ucs_config_field_t uct_sisci_iface_config_table[];
+
 typedef struct uct_sisci_iface_config {
     uct_iface_config_t    super;
     size_t                seg_size;      /* Maximal send size */
 } uct_sisci_iface_config_t;
+
+
+typedef struct uct_sisci_iface {
+    uct_base_iface_t      super;
+    uct_sisci_iface_addr_t id;           /* Unique identifier for the instance */
+    size_t                send_size;    /* Maximum size for payload */
+    ucs_mpool_t           msg_mp;       /* Messages memory pool */
+} uct_sisci_iface_t;
+
+ucs_status_t
+uct_sisci_base_query_tl_devices(uct_md_h md, uct_tl_device_resource_t **tl_devices_p,
+                             unsigned *num_tl_devices_p);
+
+ucs_status_t uct_sisci_iface_get_device_address(uct_iface_t *tl_iface,
+                                             uct_device_addr_t *addr);
+
+int uct_sisci_iface_is_reachable(const uct_iface_h tl_iface, const uct_device_addr_t *dev_addr,
+                              const uct_iface_addr_t *iface_addr);
+
+ucs_status_t uct_sisci_iface_fence(uct_iface_t *tl_iface, unsigned flags);
+
+size_t uct_sisci_iface_get_device_addr_len();
+
+ucs_status_t uct_sisci_ep_fence(uct_ep_t *tl_ep, unsigned flags);
+
+UCS_CLASS_DECLARE(uct_sisci_iface_t, uct_iface_ops_t*, uct_iface_internal_ops_t*,
+                  uct_md_h, uct_worker_h, const uct_iface_params_t*,
+                  const uct_iface_config_t*);
 
 
 /**
@@ -48,12 +80,6 @@ typedef struct uct_sisci_md_config {
 } uct_sisci_md_config_t;
 
 
-typedef struct uct_sisci_iface {
-    uct_base_iface_t      super;
-    uct_sisci_iface_addr_t id;           /* Unique identifier for the instance */
-    size_t                send_size;    /* Maximum size for payload */
-    ucs_mpool_t           msg_mp;       /* Messages memory pool */
-} uct_sisci_iface_t;
 
 
 typedef struct uct_sisci_ep {
--- src/uct/sisci/sisci_iface.h
+++ src/uct/sisci/sisci_iface.h
@@ -28,23 +28,5 @@ extern ucs_config_field_t uct_sisci_iface_config_table[];
     } config;
 } uct_sisci_iface_t;*/
 
-ucs_status_t
-uct_sisci_base_query_tl_devices(uct_md_h md, uct_tl_device_resource_t **tl_devices_p,
-                             unsigned *num_tl_devices_p);
 
-ucs_status_t uct_sisci_iface_get_device_address(uct_iface_t *tl_iface,
-                                             uct_device_addr_t *addr);
-
-int uct_sisci_iface_is_reachable(const uct_iface_h tl_iface, const uct_device_addr_t *dev_addr,
-                              const uct_iface_addr_t *iface_addr);
-
-ucs_status_t uct_sisci_iface_fence(uct_iface_t *tl_iface, unsigned flags);
-
-size_t uct_sisci_iface_get_device_addr_len();
-
-ucs_status_t uct_sisci_ep_fence(uct_ep_t *tl_ep, unsigned flags);
-
-UCS_CLASS_DECLARE(uct_sisci_iface_t, uct_iface_ops_t*, uct_iface_internal_ops_t*,
-                  uct_md_h, uct_worker_h, const uct_iface_params_t*,
-                  const uct_iface_config_t*);
 
