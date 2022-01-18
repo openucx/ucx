2a8a36cc37f0c458c6c4bc865804f8e7f944ef44
Jakob Skrede <you@example.com>
2022-01-18 11:30:53 +0100

    quick fix

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#a small script that sets all the systemvariabls that we are interested in
+export LD_LIBRARY_PATH=$PWD/install/lib
+export UCX_LOG_LEVEL=info
+export UCX_TLS=sisci
+export UCX_LOG_FILE=./logfiletest
