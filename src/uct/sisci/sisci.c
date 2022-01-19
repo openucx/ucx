4537f1f45c8c72e0d7c70ca84dccfa9a7452044b
Jakob Skrede <you@example.com>
2022-01-19 11:47:58 +0100

    quick fix

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -539,8 +539,8 @@ static ucs_status_t uct_sisci_iface_query(uct_iface_h tl_iface, uct_iface_attr_t
     bandwidth.dedicated = 1;
     bandwidth.shared = 1;
     attr->bandwidth = bandwidth; 
-    //attr->cap.flags = UCT_IFACE_FLAG_CONNECT_TO_IFACE | UCT_IFACE_FLAG_CONNECT_TO_EP |
-     //UCT_IFACE_FLAG_AM_BCOPY | UCT_IFACE_FLAG_AM_SHORT | UCT_IFACE_FLAG_AM_ZCOPY;
+    attr->cap.flags = UCT_IFACE_FLAG_CONNECT_TO_IFACE | UCT_IFACE_FLAG_CONNECT_TO_EP |
+     UCT_IFACE_FLAG_AM_BCOPY | UCT_IFACE_FLAG_AM_SHORT | UCT_IFACE_FLAG_AM_ZCOPY;
 
     attr->device_addr_len  = sizeof(uct_sisci_device_addr_t);
     attr->ep_addr_len      = sizeof(uct_sicsci_ep_addr_t);
