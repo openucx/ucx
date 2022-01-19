85e28d077f63201a6838fdfcb35d15fddf8485f7
Jakob Skrede <you@example.com>
2022-01-19 14:05:40 +0100

    quick fix

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -586,7 +586,6 @@ static ucs_status_t uct_sisci_iface_query(uct_iface_h tl_iface, uct_iface_attr_t
     attr->cap.am.min_zcopy = 64;
     attr->cap.am.max_zcopy = 1024;
 
-    attr->
 
     attr->latency                 = ucs_linear_func_make(0, 0);
     attr->bandwidth.dedicated     = 6911.0 * UCS_MBYTE;
