d78e242ea329dcfda08286379563d1659cf95f12
Jakob Skrede <you@example.com>
2022-01-28 18:21:12 +0100

    quick fix

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -659,7 +659,7 @@ static void uct_sci_process_recv(uct_iface_h tl_iface) {
     ucs_status_t status;
     status = uct_iface_invoke_am(&iface->super, packet->am_id, iface->recv_buffer + sizeof(sisci_packet_t), packet->length,0);
     
-    printf("length: %ld what we recieved %s\n", packet->length, (char *) iface->recv_buffer + sizeof(sisci_packet_t));
+    printf("length: %d what we recieved %s\n", packet->length, (char *) iface->recv_buffer + sizeof(sisci_packet_t));
     printf("sizeof struct %zd sizeof struct members: %zd\n", sizeof(sisci_packet_t), sizeof(unsigned) + sizeof(uint8_t)*2);
 
     if(status == UCS_INPROGRESS) {
