ae748eff869b526ca044e6f7e471f50b29a047f4
Jakob Skrede <you@example.com>
2022-02-02 14:00:00 +0100

    Testing debug still

--- src/uct/sisci/sisci.h
+++ src/uct/sisci/sisci.h
@@ -20,7 +20,7 @@
 #define DEBUG 3
 
 #if defined(DEBUG) && DEBUG > 0
- #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+ #define DEBUG_PRINT(fmt, args...) fprintf(stdout, "DEBUG: %s:%d:%s(): " fmt, \
     __FILE__, __LINE__, __func__, ##args)
 #else
  #define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
