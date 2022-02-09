12acce5b9ac4e2e71c366cecebdc399c8324e1e8
Jakob Skrede <you@example.com>
2022-02-09 15:18:20 +0100

    regualr dma 2

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -177,7 +177,7 @@ static UCS_CLASS_INIT_FUNC(uct_sci_iface_t, uct_md_h md, uct_worker_h worker,
     } 
 
     dma_seg_id = ucs_generate_uuid(trash);
-    SCICreateSegment(md->sci_virtial_device, &self->dma_segment, dma_seg_id, self->send_size, NULL, NULL, SCI_NO_FLAGS, &sci_error);
+    SCICreateSegment(md->sci_virtual_device, &self->dma_segment, dma_seg_id, self->send_size, NULL, NULL, SCI_NO_FLAGS, &sci_error);
 
     if(sci_error != SCI_ERR_OK) {
         printf("DMA create segment: %s \n", SCIGetErrorString(sci_error));
@@ -191,7 +191,7 @@ static UCS_CLASS_INIT_FUNC(uct_sci_iface_t, uct_md_h md, uct_worker_h worker,
         return UCS_ERR_NO_RESOURCE;
     } 
 
-    self->tx_map = SCIMapLocalSegment(self->dma_segment, &self->tx_map, 0, self->send_size, NULL, SCI_NO_FLAGS, &sci_error);
+    self->tx_map = SCIMapLocalSegment(self->dma_segment, self->tx_map, 0, self->send_size, NULL, SCI_NO_FLAGS, &sci_error);
 
     if(sci_error != SCI_ERR_OK) {
         printf("DMA map segment: %s \n", SCIGetErrorString(sci_error));
