ef4d45b04aa18b1e645cfd37ff7d48d7c89c7b34
Jakob Skrede <you@example.com>
2022-02-09 14:26:48 +0100

    trying again

--- src/uct/sisci/sisci_ep.c
+++ src/uct/sisci/sisci_ep.c
@@ -252,14 +252,23 @@ ucs_status_t uct_sci_ep_am_zcopy(uct_ep_h uct_ep, uint8_t id, const void *header
     }
     
 
-
-    SCIStartDmaTransferMem(iface->dma_queue, tx, iov_total_len + total_header_len + SCI_PACKET_SIZE, 0, NULL, NULL, SCI_NO_FLAGS, &sci_error);
+    /*SCIStartDmaTransferMem 	(sci_dma_queue_t dq, void *	localAddress,
+		sci_remote_segment_t  	remoteSegment,
+		size_t  	size,
+		size_t  	remoteOffset,
+		sci_cb_dma_t  	callback,
+		void *  	callbackArg,
+		unsigned int  	flags,
+		sci_error_t *  	error 
+	) 	*/
+    
+    SCIStartDmaTransferMem(iface->dma_queue, tx, iov_total_len + total_header_len + SCI_PACKET_SIZE, 0, 0, NULL, SCI_NO_FLAGS, &sci_error);
 
     if(sci_error != SCI_ERR_OK) {
         printf("DMA Transfer Error: %s\n", SCIGetErrorString(sci_error));
     }
 
-    SCIWaitForDMAQueue(self->dma_queue, SCI_INFINITE_TIMEOUT, SCI_NO_FLAGS, &sci_error);
+    SCIWaitForDMAQueue(iface->dma_queue, SCI_INFINITE_TIMEOUT, SCI_NO_FLAGS, &sci_error);
 
     //memcpy(ep->buf, tx, tx_pack->length + sizeof(sisci_packet_t));
     //SCIFlush(NULL, SCI_NO_FLAGS);
