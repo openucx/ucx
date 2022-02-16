2eb4221e549cb012868feb103d114bb693b42b33
Jakob Skrede <you@example.com>
2022-02-16 14:21:19 +0100

    hm

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -317,8 +317,7 @@ static ucs_status_t uct_sci_query_devices(uct_md_h md,
 static ucs_status_t uct_sci_md_query(uct_md_h md, uct_md_attr_t *attr)
 {
     /* Dummy memory registration provided. No real memory handling exists */
-    attr->cap.flags               = UCT_MD_FLAG_REG |
-                                    UCT_MD_FLAG_NEED_RKEY; /* TODO ignore rkey in rma/amo ops */
+    attr->cap.flags               = UCT_MD_FLAG_NEED_RKEY; /* TODO ignore rkey in rma/amo ops */
     attr->cap.max_alloc           = 0;
     attr->cap.reg_mem_types       = UCS_BIT(UCS_MEMORY_TYPE_HOST);
     attr->cap.alloc_mem_types     = 0;
