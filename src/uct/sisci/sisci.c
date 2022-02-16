7b4ccd13b7e51666333ba1f562403e2475d992d5
Jakob Skrede <you@example.com>
2022-02-16 14:29:22 +0100

    checking a quick race condition

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -472,6 +472,8 @@ static void uct_sci_process_recv(uct_iface_h tl_iface) {
     uct_sci_iface_t* iface = ucs_derived_of(tl_iface, uct_sci_iface_t);
     sisci_packet_t* packet = (sisci_packet_t*) iface->recv_buffer;
     ucs_status_t status;
+
+    packet->status = 2;
     status = uct_iface_invoke_am(&iface->super, packet->am_id, iface->recv_buffer + sizeof(sisci_packet_t), packet->length,0);
     
 
