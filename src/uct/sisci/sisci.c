c3b194d094b4b1431d68e4f65166b0b48d73ef34
Jakob Skrede <you@example.com>
2022-03-02 17:15:34 +0100

    ah

--- src/uct/sisci/sisci.c
+++ src/uct/sisci/sisci.c
@@ -80,7 +80,7 @@ sci_callback_action_t conn_handler(void* arg, sci_local_data_interrupt_t interru
     answer.node_id = iface->device_addr;
     answer.segment_id = iface->sci_fds[i].segment_id;
 
-    print("sending %d %d \n", iface->device_addr, iface->sci_fds[i].segment_id);
+    printf("sending %d %d \n", iface->device_addr, iface->sci_fds[i].segment_id);
 
     SCITriggerDataInterrupt(ans_interrupt, (void *) &answer, sizeof(answer), SCI_NO_FLAGS, &sci_error);
 
