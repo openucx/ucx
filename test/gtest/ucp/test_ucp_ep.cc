5943ca41fb7b433bb16207ff5f6a6554f6dfe7ec
David Wootton <dwootton@us.ibm.com>
2022-04-13 11:19:32 -0400

    UCP/ENDPOINT: Resolve second review comments
    Signed-off-by: David Wootton <dwootton@us.ibm.com>
    
    UCP/ENDPOINT: Add gtest unit test for basic validation of API change
    
    Signed-off-by: David Wootton <dwootton@us.ibm.com>
    
    UCP/ENDPOINT: Resolve testcase review comments
    
    Signed-off-by: David Wootton <dwootton@us.ibm.com>

--- test/gtest/ucp/test_ucp_ep.cc
+++ test/gtest/ucp/test_ucp_ep.cc
@@ -7,7 +7,6 @@
 #include "ucp_test.h"
 #include <ucp/core/ucp_context.h>
 
-
 class test_ucp_ep : public ucp_test {
 public:
     static void get_test_variants(std::vector<ucp_test_variant> &variants)
@@ -60,6 +59,37 @@ UCS_TEST_P(test_ucp_ep, ucp_query_ep)
        size (which represents current calculation model) */
     EXPECT_FLOAT_EQ(attr.estimated_time - estimated_time_1000,
                     estimated_time_1000 - estimated_time_0);
+}
+
+UCS_TEST_P(test_ucp_ep, ucp_query_transport)
+{
+    ucs_status_t status;
+    int i;
+    ucp_ep_attr_t ep_attrs;
+    std::vector<ucp_transport_entry_t> transport_entries(100);
+
+    ep_attrs.field_mask             = UCP_EP_ATTR_FIELD_TRANSPORTS;
+    ep_attrs.transports.entries     = &transport_entries[0];
+    ep_attrs.transports.num_entries = transport_entries.size();
+    ep_attrs.transports.entry_size  = sizeof(ucp_transport_entry_t);
+    status                          = ucp_ep_query(sender().ep(), &ep_attrs);
+
+    // Verify ucp_ep_query completed successfully, that the number of
+    // transports is updated, since no system should reasonably have
+    // 100 transport/device name pairs, and verify returned strings are
+    // not empty.
+    ASSERT_UCS_OK(status);
+    EXPECT_LT(ep_attrs.transports.num_entries, 100);
+    UCS_TEST_MESSAGE << "Number of transport/device name pairs: "
+                     << ep_attrs.transports.num_entries;
+    for (i = 0; i < ep_attrs.transports.num_entries; i++) {
+        UCS_TEST_MESSAGE << "Transport[" << i << "] transport="
+                         << ep_attrs.transports.entries[i].transport_name
+                         << " device="
+                         << ep_attrs.transports.entries[i].device_name;
+        EXPECT_STRNE(ep_attrs.transports.entries[i].transport_name, "");
+        EXPECT_STRNE(ep_attrs.transports.entries[i].device_name, "");
+    }
 }
 
 UCP_INSTANTIATE_TEST_CASE(test_ucp_ep);
