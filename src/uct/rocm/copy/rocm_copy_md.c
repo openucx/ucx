openucx/ucx

commit 604c3b05879576a936932dc31bfd696c9dbe148c

UCT/API: Introduce uct_mem_reg_v2

src/uct/rocm/copy/rocm_copy_md.c
@@ -117,10 +117,7 @@ static ucs_status_t uct_rocm_copy_mem_reg_internal(
     ucs_status_t err;
     ucs_memory_type_t mem_type;
 
-    if(address == NULL) {
-        memset(mem_hndl, 0, sizeof(*mem_hndl));
-        return UCS_OK;
-    }
+    ucs_assert((address != NULL) && (length != 0));
 
     err = uct_rocm_base_detect_memory_type(uct_md, address, length, &mem_type);
     if (err != UCS_OK) {
@@ -150,8 +147,9 @@ static ucs_status_t uct_rocm_copy_mem_reg_internal(
     return UCS_OK;
 }
 
-static ucs_status_t uct_rocm_copy_mem_reg(uct_md_h md, void *address, size_t length,
-                                          unsigned flags, uct_mem_h *memh_p)
+static ucs_status_t
+uct_rocm_copy_mem_reg(uct_md_h md, void *address, size_t length,
+                      const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
     uct_rocm_copy_mem_t *mem_hndl = NULL;
     ucs_status_t status;
@@ -238,9 +236,12 @@ uct_rocm_copy_rache_region_from_memh(uct_mem_h memh)
 }
 
 static ucs_status_t
-uct_rocm_copy_mem_rcache_reg(uct_md_h uct_md, void *address, size_t length,
-                             unsigned flags, uct_mem_h *memh_p)
+uct_rocm_copy_mem_rcache_reg(uct_md_h md, void *address, size_t length,
+                             const uct_md_mem_reg_params_t *params,
+                             uct_mem_h *memh_p)
 {
+    uint64_t flags         = UCT_MD_MEM_REG_FIELD_VALUE(params, flags,
+                                                        FIELD_FLAGS, 0);
     uct_rocm_copy_md_t *md = ucs_derived_of(uct_md, uct_rocm_copy_md_t);
     ucs_rcache_region_t *rregion;
     ucs_status_t status;

src/uct/api/v2/uct_v2.h
@@ -192,6 +192,15 @@ typedef struct {
 
 /**
  * @ingroup UCT_MD
+ * @brief MD memory registration operation flags.
+ */
+typedef enum {
+    UCT_MD_MEM_REG_FIELD_FLAGS = UCS_BIT(0)
+} uct_md_mem_reg_field_mask_t;
+
+
+/**
+ * @ingroup UCT_MD
  * @brief MD memory de-registration operation flags.
  */
 typedef enum {
@@ -310,6 +319,26 @@ typedef void (*uct_md_mem_invalidate_cb_t)(void *arg);
 
 /**
  * @ingroup UCT_MD
+ * @brief Operation parameters passed to @ref uct_md_mem_reg_v2.
+ */
+typedef struct uct_md_mem_reg_params {
+    /**
+     * Mask of valid fields in this structure and operation flags, using
+     * bits from @ref uct_md_mem_reg_field_mask_t. Fields not specified
+     * in this mask will be ignored. Provides ABI compatibility with respect
+     * to adding new fields.
+     */
+    uint64_t                     field_mask;
+
+    /**
+     * Operation specific flags, using bits from @ref uct_md_mem_flags_t.
+     */
+    uint64_t                     flags;
+} uct_md_mem_reg_params_t;
+
+
+/**
+ * @ingroup UCT_MD
  * @brief Operation parameters passed to @ref uct_md_mem_dereg_v2.
  */
 typedef struct uct_md_mem_dereg_params {
@@ -419,16 +448,39 @@ uct_iface_estimate_perf(uct_iface_h tl_iface, uct_perf_attr_t *perf_attr);
 
 /**
  * @ingroup UCT_MD
- * @brief Undo the operation of @ref uct_md_mem_reg() and invalidate memory
- *        region.
+ * @brief Register memory for zero-copy sends and remote access.
+ *
+ * Register memory on the memory domain. In order to use this function, @a md
+ * must support the @ref UCT_MD_FLAG_REG flag.
+ *
+ * @param [in]  md          Memory domain that was used to register the memory.
+ * @param [in]  address     Memory to register.
+ * @param [in]  length      Size of memory to register. Must be > 0.
+ * @param [in]  params      Operation parameters, see @ref
+ *                          uct_md_mem_reg_params_t.
+ * @param [out] memh_p      Filled with handle for allocated region.
+ *
+ * @return Error code.
+ */
+ucs_status_t uct_md_mem_reg_v2(uct_md_h md, void *address, size_t length,
+                               const uct_md_mem_reg_params_t *params,
+                               uct_mem_h *memh_p);
+
+
+/**
+ * @ingroup UCT_MD
+ * @brief Undo the operation of @ref uct_md_mem_reg() or
+ *        @ref uct_md_mem_reg_v2() and invalidate memory region.
  *
  * This routine deregisters the memory region registered by @ref uct_md_mem_reg
- * and allow the memory region to be invalidated with callback called when the
+ * and allows the memory region to be invalidated with callback called when the
  * memory region is unregistered.
  *
  * @param [in]  md          Memory domain that was used to register the memory.
  * @param [in]  params      Operation parameters, see @ref
  *                          uct_md_mem_dereg_params_t.
+ *
+ * @return Error code.
  */
 ucs_status_t uct_md_mem_dereg_v2(uct_md_h md,
                                  const uct_md_mem_dereg_params_t *params);

src/uct/base/uct_md.c
@@ -465,6 +465,19 @@ uct_md_mem_advise(uct_md_h md, uct_mem_h memh, void *addr, size_t length,
 ucs_status_t uct_md_mem_reg(uct_md_h md, void *address, size_t length,
                             unsigned flags, uct_mem_h *memh_p)
 {
+    uct_md_mem_reg_params_t params = {
+        .field_mask = UCT_MD_MEM_REG_FIELD_FLAGS,
+        .flags      = flags
+    };
+
+    return uct_md_mem_reg_v2(md, address, length, &params, memh_p);
+}
+
+ucs_status_t uct_md_mem_reg_v2(uct_md_h md, void *address, size_t length,
+                               const uct_md_mem_reg_params_t *params,
+                               uct_mem_h *memh_p)
+{
+    uint64_t flags = UCT_MD_MEM_REG_FIELD_VALUE(params, flags, FIELD_FLAGS, 0);
     ucs_status_t status;
 
     if ((length == 0) || (address == NULL)) {
@@ -477,12 +490,12 @@ ucs_status_t uct_md_mem_reg(uct_md_h md, void *address, size_t length,
     status = uct_mem_check_flags(flags);
     if (status != UCS_OK) {
         uct_md_log_mem_reg_error(flags,
-                                 "uct_md_mem_reg(flags=0x%x): invalid flags",
-                                 flags);
+                                 "uct_md_mem_reg_v2(flags=0x%lx): invalid"
+                                 " flags", flags);
         return status;
     }
 
-    return md->ops->mem_reg(md, address, length, flags, memh_p);
+    return md->ops->mem_reg(md, address, length, params, memh_p);
 }
 
 ucs_status_t uct_md_mem_dereg(uct_md_h md, uct_mem_h memh)
@@ -517,6 +530,25 @@ ucs_status_t uct_md_detect_memory_type(uct_md_h md, const void *addr, size_t len
                                        ucs_memory_type_t *mem_type_p)
 {
     return md->ops->detect_memory_type(md, addr, length, mem_type_p);
+}
+
+ucs_status_t uct_md_dummy_mem_reg(uct_md_h md, void *address, size_t length,
+                                  const uct_md_mem_reg_params_t *params,
+                                  uct_mem_h *memh_p)
+{
+    /* We have to emulate memory registration. Return dummy pointer */
+    *memh_p = (void*)0xdeadbeef;
+    return UCS_OK;
+}
+
+ucs_status_t uct_md_dummy_mem_dereg(uct_md_h uct_md,
+                                    const uct_md_mem_dereg_params_t *params)
+{
+    UCT_MD_MEM_DEREG_CHECK_PARAMS(params, 0);
+
+    ucs_assert(params->memh == (void*)0xdeadbeef);
+
+    return UCS_OK;
 }
 
 void uct_md_set_rcache_params(ucs_rcache_params_t *rcache_params,

src/uct/base/uct_md.h
@@ -25,6 +25,10 @@
     ucs_log(uct_md_reg_log_lvl(_flags), _fmt, ## __VA_ARGS__)
 
 
+#define UCT_MD_MEM_REG_FIELD_VALUE(_params, _name, _flag, _default) \
+    UCS_PARAM_VALUE(UCT_MD_MEM_REG, _params, _name, _flag, _default)
+
+
 #define UCT_MD_MEM_DEREG_FIELD_VALUE(_params, _name, _flag, _default) \
     UCS_PARAM_VALUE(UCT_MD_MEM_DEREG, _params, _name, _flag, _default)
 
@@ -92,10 +96,10 @@ typedef ucs_status_t (*uct_md_mem_advise_func_t)(uct_md_h md,
                                                  size_t length,
                                                  unsigned advice);
 
-typedef ucs_status_t (*uct_md_mem_reg_func_t)(uct_md_h md, void *address,
-                                              size_t length,
-                                              unsigned flags,
-                                              uct_mem_h *memh_p);
+typedef ucs_status_t
+(*uct_md_mem_reg_func_t)(uct_md_h md, void *address, size_t length,
+                         const uct_md_mem_reg_params_t *params,
+                         uct_mem_h *memh_p);
 
 typedef ucs_status_t
 (*uct_md_mem_dereg_func_t)(uct_md_h md,
@@ -220,6 +224,12 @@ ucs_status_t uct_mem_alloc_check_params(size_t length,
                                         unsigned num_methods,
                                         const uct_mem_alloc_params_t *params);
 
+ucs_status_t uct_md_dummy_mem_reg(uct_md_h md, void *address, size_t length,
+                                  const uct_md_mem_reg_params_t *params,
+                                  uct_mem_h *memh_p);
+
+ucs_status_t uct_md_dummy_mem_dereg(uct_md_h uct_md,
+                                    const uct_md_mem_dereg_params_t *params);
 
 void uct_md_set_rcache_params(ucs_rcache_params_t *rcache_params,
                               const uct_md_rcache_config_t *rcache_config);

src/uct/cuda/cuda_copy/cuda_copy_md.c
@@ -88,19 +88,15 @@ static ucs_status_t uct_cuda_copy_rkey_release(uct_component_t *component,
 }
 
 UCS_PROFILE_FUNC(ucs_status_t, uct_cuda_copy_mem_reg,
-                 (md, address, length, flags, memh_p),
+                 (md, address, length, params, memh_p),
                  uct_md_h md, void *address, size_t length,
-                 unsigned flags, uct_mem_h *memh_p)
+                 const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
+    uint64_t flags = UCT_MD_MEM_REG_FIELD_VALUE(params, flags, FIELD_FLAGS, 0);
     ucs_log_level_t log_level;
     CUmemorytype memType;
     CUresult result;
     ucs_status_t status;
-
-    if (address == NULL) {
-        *memh_p = address;
-        return UCS_OK;
-    }
 
     result = cuPointerGetAttribute(&memType, CU_POINTER_ATTRIBUTE_MEMORY_TYPE,
                                    (CUdeviceptr)(address));

src/uct/cuda/cuda_ipc/cuda_ipc_md.c
@@ -226,9 +226,7 @@ uct_cuda_ipc_mem_reg_internal(uct_md_h uct_md, void *addr, size_t length,
     CUdevice cu_device;
     ucs_status_t status;
 
-    if (!length) {
-        return UCS_OK;
-    }
+    ucs_assert((addr != NULL) && (length != 0));
 
     log_level = (flags & UCT_MD_MEM_FLAG_HIDE_ERRORS) ? UCS_LOG_LEVEL_DEBUG :
                 UCS_LOG_LEVEL_ERROR;
@@ -252,8 +250,9 @@ uct_cuda_ipc_mem_reg_internal(uct_md_h uct_md, void *addr, size_t length,
     return UCS_OK;
 }
 
-static ucs_status_t uct_cuda_ipc_mem_reg(uct_md_h md, void *address, size_t length,
-                                         unsigned flags, uct_mem_h *memh_p)
+static ucs_status_t
+uct_cuda_ipc_mem_reg(uct_md_h md, void *address, size_t length,
+                     const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
     uct_cuda_ipc_key_t *key;
     ucs_status_t status;

src/uct/cuda/gdr_copy/gdr_copy_md.c
@@ -115,10 +115,7 @@ UCS_PROFILE_FUNC(ucs_status_t, uct_gdr_copy_mem_reg_internal,
     ucs_log_level_t log_level;
     int ret;
 
-    if (!length) {
-        memset(mem_hndl, 0, sizeof(*mem_hndl));
-        return UCS_OK;
-    }
+    ucs_assert((address != NULL) && (length != 0));
 
     log_level = (flags & UCT_MD_MEM_FLAG_HIDE_ERRORS) ? UCS_LOG_LEVEL_DEBUG :
                 UCS_LOG_LEVEL_ERROR;
@@ -188,8 +185,9 @@ UCS_PROFILE_FUNC(ucs_status_t, uct_gdr_copy_mem_dereg_internal,
     return UCS_OK;
 }
 
-static ucs_status_t uct_gdr_copy_mem_reg(uct_md_h uct_md, void *address, size_t length,
-                                         unsigned flags, uct_mem_h *memh_p)
+static ucs_status_t
+uct_gdr_copy_mem_reg(uct_md_h uct_md, void *address, size_t length,
+                     const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
     uct_gdr_copy_mem_t *mem_hndl = NULL;
     void *start, *end;
@@ -289,8 +287,11 @@ uct_gdr_copy_rache_region_from_memh(uct_mem_h memh)
 
 static ucs_status_t
 uct_gdr_copy_mem_rcache_reg(uct_md_h uct_md, void *address, size_t length,
-                            unsigned flags, uct_mem_h *memh_p)
+                            const uct_md_mem_reg_params_t *params,
+                            uct_mem_h *memh_p)
 {
+    uint64_t flags        = UCT_MD_MEM_REG_FIELD_VALUE(params, flags,
+                                                       FIELD_FLAGS, 0);
     uct_gdr_copy_md_t *md = ucs_derived_of(uct_md, uct_gdr_copy_md_t);
     ucs_rcache_region_t *rregion;
     ucs_status_t status;

src/uct/ib/base/ib_md.c
@@ -752,9 +752,12 @@ err:
     return status;
 }
 
-static ucs_status_t uct_ib_mem_reg(uct_md_h uct_md, void *address, size_t length,
-                                   unsigned flags, uct_mem_h *memh_p)
+static ucs_status_t
+uct_ib_mem_reg(uct_md_h uct_md, void *address, size_t length,
+               const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
+    uint64_t flags  = UCT_MD_MEM_REG_FIELD_VALUE(params, flags, FIELD_FLAGS,
+                                                 0);
     uct_ib_md_t *md = ucs_derived_of(uct_md, uct_ib_md_t);
     ucs_status_t status;
     uct_ib_mem_t *memh;
@@ -958,10 +961,12 @@ static inline uct_ib_rcache_region_t* uct_ib_rcache_region_from_memh(uct_mem_h m
     return ucs_container_of(memh, uct_ib_rcache_region_t, memh);
 }
 
-static ucs_status_t uct_ib_mem_rcache_reg(uct_md_h uct_md, void *address,
-                                          size_t length, unsigned flags,
-                                          uct_mem_h *memh_p)
+static ucs_status_t
+uct_ib_mem_rcache_reg(uct_md_h uct_md, void *address, size_t length,
+                      const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
+    uint64_t flags  = UCT_MD_MEM_REG_FIELD_VALUE(params, flags, FIELD_FLAGS,
+                                                 0);
     uct_ib_md_t *md = ucs_derived_of(uct_md, uct_ib_md_t);
     ucs_rcache_region_t *rregion;
     ucs_status_t status;
@@ -1083,16 +1088,19 @@ static ucs_status_t uct_ib_md_odp_query(uct_md_h uct_md, uct_md_attr_t *md_attr)
     return UCS_OK;
 }
 
-static ucs_status_t uct_ib_mem_global_odp_reg(uct_md_h uct_md, void *address,
-                                              size_t length, unsigned flags,
-                                              uct_mem_h *memh_p)
+static ucs_status_t
+uct_ib_mem_global_odp_reg(uct_md_h uct_md, void *address, size_t length,
+                          const uct_md_mem_reg_params_t *params,
+                          uct_mem_h *memh_p)
 {
-    uct_ib_md_t *md = ucs_derived_of(uct_md, uct_ib_md_t);
+    uint64_t flags     = UCT_MD_MEM_REG_FIELD_VALUE(params, flags, FIELD_FLAGS,
+                                                    0);
+    uct_ib_md_t *md    = ucs_derived_of(uct_md, uct_ib_md_t);
     uct_ib_mem_t *memh = md->global_odp;
 
     ucs_assert(md->global_odp != NULL);
     if (flags & UCT_MD_MEM_FLAG_LOCK) {
-        return uct_ib_mem_reg(uct_md, address, length, flags, memh_p);
+        return uct_ib_mem_reg(uct_md, address, length, params, memh_p);
     }
 
     if (md->config.odp.prefetch) {

commit a19d73a7aa63ac7fd6bd6edc6df1f2d454a3e0a5

UCT/ROCM/COPY: fix compilation problem
commit 604c3b05879576a936932dc31bfd696c9dbe148c (Introduce
uct_mem_reg_v2) accidentally renamed a parameter in
uct_rocm_copy_mem_rcache_reg(), breaking the compilation of the
rocm/copy component. This commit fixes it.

src/uct/rocm/copy/rocm_copy_md.c
@@ -236,7 +236,7 @@ uct_rocm_copy_rache_region_from_memh(uct_mem_h memh)
 }
 
 static ucs_status_t
-uct_rocm_copy_mem_rcache_reg(uct_md_h md, void *address, size_t length,
+uct_rocm_copy_mem_rcache_reg(uct_md_h uct_md, void *address, size_t length,
                              const uct_md_mem_reg_params_t *params,
                              uct_mem_h *memh_p)
 {