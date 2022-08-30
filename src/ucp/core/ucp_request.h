a1e6ef86f2dfd93563a80682f00500546744176d
Sergey Oblomov <sergeyo@nvidia.com>
2022-08-30 13:47:29 +0300

    UCP/WORKER_FENCE: added worker_fence over EPs
    - added implementation of worker_fence via EPs flush_remote calls
    - added test

--- src/ucp/core/ucp_request.h
+++ src/ucp/core/ucp_request.h
@@ -444,8 +444,9 @@ struct ucp_request {
             ucp_worker_h            worker;       /* Worker to flush */
             ucp_send_nbx_callback_t cb;           /* Completion callback */
             uct_worker_cb_id_t      prog_id;      /* Progress callback ID */
-            int                     comp_count;   /* Countdown to request completion */
             ucp_ep_ext_t            *next_ep_ext; /* Extension of the next endpoint to flush */
+            int                     comp_count;   /* Countdown to request completion */
+            unsigned                uct_flags;    /* Flags to pass to @ref uct_ep_flush */
         } flush_worker;
     };
 };
