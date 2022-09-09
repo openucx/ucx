068400a0530107745fd2f870b346928a97226cea
Boris Karasev <karasev.b@gmail.com>
2022-09-09 07:28:18 +0300

    UCT/CUDA_COPY: fix trace format string

--- src/uct/cuda/cuda_copy/cuda_copy_ep.c
+++ src/uct/cuda/cuda_copy/cuda_copy_ep.c
@@ -157,7 +157,7 @@ uct_cuda_copy_post_cuda_async_copy(uct_ep_h tl_ep, void *dst, void *src,
     ucs_queue_push(event_q, &cuda_event->queue);
     cuda_event->comp = comp;
 
-    ucs_trace("cuda async issued :%p dst:%p[%s], src:%p[%s] len:%ld",
+    ucs_trace("cuda async issued: %p dst:%p[%s], src:%p[%s] len:%ld",
               cuda_event, dst, ucs_memory_type_names[dst_type], src,
               ucs_memory_type_names[src_type], length);
     return UCS_INPROGRESS;
