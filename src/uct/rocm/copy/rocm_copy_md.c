26c12dfe0145387e1dbf0f252a518eb55a800d0e
Edgar Gabriel <Edgar.Gabriel@amd.com>
2022-09-12 22:38:13 +0000

    UCT/ROCM: make rocm components compile again
    the md_attr_v2 commit broke the rocm compilation. This commit fixes it
    again.

--- src/uct/rocm/copy/rocm_copy_md.c
+++ src/uct/rocm/copy/rocm_copy_md.c
@@ -35,7 +35,7 @@ static ucs_config_field_t uct_rocm_copy_md_config_table[] = {
 };
 
 static ucs_status_t
-uct_rocm_copy_md_query(uct_md_h md, uct_md_attr_v2_t *md_attr_v2)
+uct_rocm_copy_md_query(uct_md_h md, uct_md_attr_v2_t *md_attr)
 {
     md_attr->flags            = UCT_MD_FLAG_REG | UCT_MD_FLAG_NEED_RKEY;
     md_attr->reg_mem_types    = UCS_BIT(UCS_MEMORY_TYPE_HOST) |
