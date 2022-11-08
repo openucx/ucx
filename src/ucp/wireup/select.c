42d40c92585d79d3cedf2c2d4c97e377a6e325f2
Mikhail Brinskii <mikhailb@nvidia.com>
2022-11-08 18:11:00 +0200

    UCP: Remove lane selection restrictions

--- src/ucp/wireup/select.c
+++ src/ucp/wireup/select.c
@@ -1009,17 +1009,6 @@ static void ucp_wireup_fill_peer_err_criteria(ucp_wireup_criteria_t *criteria,
     }
 }
 
-static void
-ucp_wireup_fill_exported_memh_criteria(ucp_context_h context,
-                                       unsigned ep_init_flags,
-                                       ucp_wireup_criteria_t *criteria)
-{
-    if ((context->config.features & UCP_FEATURE_EXPORTED_MEMH) &&
-        !(ep_init_flags & UCP_EP_INIT_FLAG_MEM_TYPE)) {
-        criteria->local_md_flags |= UCT_MD_FLAG_EXPORTED_MKEY;
-    }
-}
-
 static double ucp_wireup_aux_score_func(const ucp_worker_iface_t *wiface,
                                         const uct_md_attr_v2_t *md_attr,
                                         const ucp_address_entry_t *remote_addr,
@@ -1119,8 +1108,6 @@ static ucs_status_t
 ucp_wireup_add_rma_lanes(const ucp_wireup_select_params_t *select_params,
                          ucp_wireup_select_context_t *select_ctx)
 {
-    ucp_ep_h ep                    = select_params->ep;
-    ucp_context_h context          = ep->worker->context;
     unsigned ep_init_flags         = ucp_wireup_ep_init_flags(select_params,
                                                               select_ctx);
     ucp_wireup_criteria_t criteria = {};
@@ -1154,7 +1141,6 @@ ucp_wireup_add_rma_lanes(const ucp_wireup_select_params_t *select_params,
     }
     criteria.calc_score             = ucp_wireup_rma_score_func;
     ucp_wireup_fill_peer_err_criteria(&criteria, ep_init_flags);
-    ucp_wireup_fill_exported_memh_criteria(context, ep_init_flags, &criteria);
 
     tl_bitmap = ucp_tl_bitmap_max;
     for (mem_type = 0; mem_type < UCS_MEMORY_TYPE_LAST; ++mem_type) {
@@ -1670,8 +1656,6 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
     ucp_wireup_init_select_flags(&bw_info.criteria.local_iface_flags,
                                  UCT_IFACE_FLAG_PENDING, 0);
     ucp_wireup_fill_peer_err_criteria(&bw_info.criteria, ep_init_flags);
-    ucp_wireup_fill_exported_memh_criteria(context, ep_init_flags,
-                                           &bw_info.criteria);
 
     if (ucs_test_all_flags(ucp_ep_get_context_features(ep),
                            UCP_FEATURE_TAG | UCP_FEATURE_WAKEUP)) {
@@ -1707,8 +1691,6 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
     bw_info.criteria.lane_type        = UCP_LANE_TYPE_RMA_BW;
     bw_info.max_lanes                 = context->config.ext.max_rndv_lanes;
     bw_info.criteria.local_cmpt_flags = 0;
-    ucp_wireup_fill_exported_memh_criteria(context, ep_init_flags,
-                                           &bw_info.criteria);
 
     /* If error handling is requested we require memory invalidation
      * support to provide correct data integrity in case of error */
