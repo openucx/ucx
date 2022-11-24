a75f7e1320df3bbb38ce9efd10d1efcf3dea9ca8
Brian Sheng <bins@nvidia.com>
2022-11-24 09:19:44 +0200

    UCT/IB/SHM/CUDA/TCP/UGNI/ROCM: Add test scenario for null pointer in v2 param

--- test/gtest/uct/ib/test_ib_pkey.cc
+++ test/gtest/uct/ib/test_ib_pkey.cc
@@ -228,8 +228,26 @@ UCS_TEST_P(test_uct_ib_pkey, test_pkey_pairs) {
             .info_string_length = 4096
         };
 
+        const uct_iface_is_reachable_params_t params_null1 = {
+            .device_addr        = (uct_device_addr_t*)ib_addr1,
+            .iface_addr         = NULL,
+            .info_string        = NULL,
+            .info_string_length = 0
+        };
+        
+        const uct_iface_is_reachable_params_t params_null2 = {
+            .device_addr        = (uct_device_addr_t*)ib_addr2,
+            .iface_addr         = NULL,
+            .info_string        = NULL,
+            .info_string_length = 4096
+        };
+
         EXPECT_EQ(res, uct_ib_iface_is_reachable_v2(m_e1->iface(), &params1));
         EXPECT_EQ(res, uct_ib_iface_is_reachable_v2(m_e2->iface(), &params2));
+        
+        /* Test null buffer pointer */
+        EXPECT_EQ(res, uct_ib_iface_is_reachable_v2(m_e1->iface(), &params_null1));
+        EXPECT_EQ(res, uct_ib_iface_is_reachable_v2(m_e2->iface(), &params_null2));
 
         ucs_free(params1.info_string);
         ucs_free(params2.info_string);
