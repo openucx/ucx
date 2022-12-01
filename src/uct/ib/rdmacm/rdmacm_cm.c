6cefebf1ca0f7eedbc8dc72827ad10f9e54c6720
Brian Sheng <bins@nvidia.com>
2022-12-01 10:01:13 +0200

    UCT/IB/SHM/CUDA/TCP/UGNI/ROCM: Use ucs_empty_function_return_zero to align v2 with v1

--- src/uct/ib/rdmacm/rdmacm_cm.c
+++ src/uct/ib/rdmacm/rdmacm_cm.c
@@ -878,7 +878,7 @@ static uct_iface_internal_ops_t uct_rdmacm_cm_iface_internal_ops = {
     .ep_query              = uct_rdmacm_ep_query,
     .ep_invalidate         = (uct_ep_invalidate_func_t)ucs_empty_function_return_unsupported,
     .ep_connect_to_ep_v2   = ucs_empty_function_return_unsupported,
-    .iface_is_reachable_v2 = ucs_empty_function_return_unsupported
+    .iface_is_reachable_v2 = (uct_iface_is_reachable_v2_func_t)ucs_empty_function_return_zero
 };
 
 static ucs_status_t
