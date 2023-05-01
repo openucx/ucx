ce35a9878dca7cf0ab61a019f9a66e1c07e9259a
Shachar Hasson <shasson5@gmail.com>
2023-05-01 10:07:51 +0300

    UCT/IB: Added IB iface is_reachable_v2

--- src/uct/ib/rc/verbs/rc_verbs_iface.c
+++ src/uct/ib/rc/verbs/rc_verbs_iface.c
@@ -508,7 +508,7 @@ static uct_rc_iface_ops_t uct_rc_verbs_iface_ops = {
             .ep_query              = (uct_ep_query_func_t)ucs_empty_function_return_unsupported,
             .ep_invalidate         = (uct_ep_invalidate_func_t)ucs_empty_function_return_unsupported,
             .ep_connect_to_ep_v2   = uct_rc_verbs_ep_connect_to_ep_v2,
-            .iface_is_reachable_v2 = uct_base_iface_is_reachable_v2
+            .iface_is_reachable_v2 = uct_ib_iface_is_reachable_v2
         },
         .create_cq      = uct_ib_verbs_create_cq,
         .destroy_cq     = uct_ib_verbs_destroy_cq,
