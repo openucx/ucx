11435f6c63031f9c5e46f2667917295acc200682
Thomas Vegas <tvegas@nvidia.com>
2023-05-05 16:20:56 +0300

    UCP/WIREUP: Init lanes: Also update selected when detected as same
    When score is very similar, it is more deterministic to select the last
    identical.

--- src/ucp/wireup/select.c
+++ src/ucp/wireup/select.c
@@ -405,6 +405,7 @@ static UCS_F_NOINLINE ucs_status_t ucp_wireup_select_transport(
     double score;
     uint8_t priority;
     ucp_md_index_t md_index;
+    bool selected;
 
     p            = tls_info;
     endp         = tls_info + sizeof(tls_info) - 1;
@@ -594,17 +595,19 @@ static UCS_F_NOINLINE ucs_status_t ucp_wireup_select_transport(
             priority     = iface_attr->priority + ae->iface_attr.priority;
             is_reachable = 1;
 
-            ucs_trace(UCT_TL_RESOURCE_DESC_FMT
-                      "->addr[%u] : %s score %.2f priority %d",
-                      UCT_TL_RESOURCE_DESC_ARG(resource),
-                      addr_index, criteria->title, score, priority);
 
-            if (!found || (ucp_score_prio_cmp(score, priority, sinfo.score,
-                                              sinfo.priority) > 0)) {
+            selected = ucp_score_prio_cmp(score, priority, sinfo.score,
+                                     sinfo.priority) >= 0;
+            if (!found || (selected != 0)) {
                 ucp_wireup_init_select_info(score, addr_index, rsc_index,
                                             priority, &sinfo);
                 found = 1;
             }
+            ucs_trace(UCT_TL_RESOURCE_DESC_FMT
+                      "->addr[%u] : %s score %.2f priority %d %s",
+                      UCT_TL_RESOURCE_DESC_ARG(resource),
+                      addr_index, criteria->title, score, priority,
+                      selected? "selected" : "");
         }
 
         /* If a local resource cannot reach any of the remote addresses,
