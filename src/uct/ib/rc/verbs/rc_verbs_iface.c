57fd6ee91b3fde16cdc857ef63a4953c70cc4c53
Mikhail Brinskii <mikhailb@nvidia.com>
2023-07-20 19:01:31 +0300

    UCT/RC/VERBS: Fix ep address management
    Low byte of flush mkey (aka atomic mr_id) can be 0.
    When unified mode is used, UCP uses local iface query to know
    the length of ep address. If one of the peers atomic mr_id is 0
    while another is not, UCP uses worng ep address length and
    incorrectly unpacks UCP address.

--- src/uct/ib/rc/verbs/rc_verbs_iface.c
+++ src/uct/ib/rc/verbs/rc_verbs_iface.c
@@ -231,8 +231,9 @@ static ucs_status_t uct_rc_verbs_iface_query(uct_iface_h tl_iface, uct_iface_att
     iface_attr->latency.m += 1e-9;  /* 1 ns per each extra QP */
     iface_attr->overhead   = 75e-9; /* Software overhead */
 
+    status                  = uct_ib_md_ops(md)->get_atomic_mr_id(md, &mr_id);
     iface_attr->ep_addr_len = (uct_rc_iface_flush_rkey_enabled(&iface->super) ||
-                               (uct_ib_md_get_atomic_mr_id(md) != 0)) ?
+                               (status == UCS_OK)) ?
                                       sizeof(uct_rc_verbs_ep_flush_addr_t) :
                                       sizeof(uct_rc_verbs_ep_addr_t);
 
