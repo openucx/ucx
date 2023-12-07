506f072ce0de90f397a1b8726c7cf8cabeccd41d
Raul Akhmetshin <rakhmetshin@nvidia.com>
2023-12-07 14:18:44 +0200

    UCT/IB/UD: Moved flag setting to complete method. Reverted.
    This reverts commit d4bec926a377fa7afa2589ed613b6f92cfe531eb.

--- src/uct/ib/ud/accel/ud_mlx5.c
+++ src/uct/ib/ud/accel/ud_mlx5.c
@@ -271,6 +271,8 @@ static UCS_F_ALWAYS_INLINE ucs_status_t uct_ud_mlx5_ep_inline_iov_post(
 
     uct_ud_enter(&iface->super);
 
+    uct_ud_ep_set_am_flag(&ep->super);
+
     skb = uct_ud_ep_get_tx_skb(&iface->super, &ep->super);
     if (!skb) {
         status = UCS_ERR_NO_RESOURCE;
