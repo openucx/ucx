bdb03bb4436beef17fa2c47bb1456eaa528017d6
Raul Akhmetshin <rakhmetshin@nvidia.com>
2024-01-31 20:54:01 +0200

    UCP/WIREUP: Activate wiface if lane is wireup message lane.

--- src/ucp/wireup/wireup.c
+++ src/ucp/wireup/wireup.c
@@ -1032,6 +1032,7 @@ static int ucp_wireup_should_activate_wiface(ucp_worker_iface_t *wiface,
     return !context->config.ext.proto_enable ||
            (context->config.features & UCP_FEATURE_STREAM) ||
            (ucp_ep_config(ep)->key.keepalive_lane == lane) ||
+           (ucp_ep_config(ep)->key.wireup_msg_lane == lane) ||
            (ep->flags & UCP_EP_FLAG_INTERNAL) ||
            (wiface->flags & UCP_WORKER_IFACE_FLAG_KEEP_ACTIVE);
 }
