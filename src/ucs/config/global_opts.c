1e51f89e87947abaf3bf802e330b7e6d023e8f2c
rakhmets <rakhmetshin@nvidia.com>
2024-06-11 16:38:19 +0300

    UCS/CONFIG: Consolidated variables.

--- src/ucs/config/global_opts.c
+++ src/ucs/config/global_opts.c
@@ -191,38 +191,40 @@ static ucs_config_field_t ucs_global_opts_table[] = {
   "The list order decides the priority of the providers.",
   ucs_offsetof(ucs_global_opts_t, topo_prio), UCS_CONFIG_TYPE_STRING_ARRAY},
 
- {"PCI_ROOT_LAT", "300ns",
-  "Estimated latency between devices with the same PCI domain and bus number",
-   ucs_offsetof(ucs_global_opts_t, pci_root_lat), UCS_CONFIG_TYPE_TIME},
+ {"DISTANCE_LAT", "pci:300ns,numa:300ns,sys:500ns",
+  "Estimated latency between system devices", 0,
+  UCS_CONFIG_TYPE_KEY_VALUE(UCS_CONFIG_TYPE_TIME,
+      {"pci", "latency between devices with the same PCI domain and bus number",
+       ucs_offsetof(ucs_global_opts_t, pci_root_lat)},
+      {"numa",
+       "latency between devices with different PCI domains and/or bus numbers "
+       "residing on the same NUMA node",
+       ucs_offsetof(ucs_global_opts_t, common_numa.latency)},
+      {"sys", "latency between devices residing on different NUMA nodes",
+       ucs_offsetof(ucs_global_opts_t, sys_root.latency)},
+      {NULL}
+ )},
 
- {"PCI_ROOT_BW_K", "19200MBs",
-  "Coefficient of proportionality between the estimated bandwidth and the\n"
-  "distance between devices with the same PCI domain and bus number. The\n"
-  "bandwidth is inversely proportional to the distance.",
-  ucs_offsetof(ucs_global_opts_t, pci_root_bw_k), UCS_CONFIG_TYPE_BW},
-
- {"PCI_ROOT_BW_MAX", "3500MBs",
-  "Maximum estimated bandwidth between devices with the same PCI domain and\n"
-  "bus number",
-  ucs_offsetof(ucs_global_opts_t, pci_root_bw_max), UCS_CONFIG_TYPE_BW},
-
- {"COMMON_NUMA_LAT", "300ns",
-  "Estimated latency between devices with different PCI domains and/or bus\n"
-  "numbers residing on the same NUMA node.",
-  ucs_offsetof(ucs_global_opts_t, common_numa.latency), UCS_CONFIG_TYPE_TIME},
-
- {"COMMON_NUMA_BW", "17000MBs",
-  "Estimated bandwidth between devices with different PCI domains and/or bus\n"
-  "numbers residing on the same NUMA node.",
-  ucs_offsetof(ucs_global_opts_t, common_numa.bandwidth), UCS_CONFIG_TYPE_BW},
-
- {"SYS_ROOT_LAT", "500ns",
-  "Estimated latency between devices residing on different NUMA nodes",
-  ucs_offsetof(ucs_global_opts_t, sys_root.latency), UCS_CONFIG_TYPE_TIME},
-
- {"SYS_ROOT_BW", "220MBs",
-  "Estimated bandwidth between devices residing on different NUMA nodes",
-  ucs_offsetof(ucs_global_opts_t, sys_root.bandwidth), UCS_CONFIG_TYPE_BW},
+ {"DISTANCE_BW", "pci_k:19200MBs,pci_max:3500MBs,numa:17000MBs,sys:220MBs",
+  "Estimated bandwidth between system devices", 0,
+  UCS_CONFIG_TYPE_KEY_VALUE(UCS_CONFIG_TYPE_BW,
+      {"pci_k",
+       "coefficient of proportionality between the estimated bandwidth and the "
+       "distance between devices with the same PCI domain and bus number. The "
+       "bandwidth is inversely proportional to the distance.",
+       ucs_offsetof(ucs_global_opts_t, pci_root_bw_k)},
+      {"pci_max",
+       "maximum estimated bandwidth between devices with the same PCI domain "
+       "and bus number",
+       ucs_offsetof(ucs_global_opts_t, pci_root_bw_max)},
+      {"numa",
+       "bandwidth between devices with different PCI domains and/or bus "
+       "numbers residing on the same NUMA node",
+       ucs_offsetof(ucs_global_opts_t, common_numa.bandwidth)},
+      {"sys", "bandwidth between devices residing on different NUMA nodes",
+       ucs_offsetof(ucs_global_opts_t, sys_root.bandwidth)},
+      {NULL}
+ )},
 
  {NULL}
 };
