dc0bf9b3a838b6705274a4119bf84201a5d82a93
rakhmets <rakhmetshin@nvidia.com>
2024-10-08 18:41:16 +0300

    GTEST/COMMON: Fixed compilation with cuda11.1.

--- test/gtest/common/mem_buffer.cc
+++ test/gtest/common/mem_buffer.cc
@@ -238,12 +238,16 @@ void *mem_buffer::allocate(size_t size, ucs_memory_type_t mem_type, bool async)
         return ptr;
 #if HAVE_CUDA
     case UCS_MEMORY_TYPE_CUDA:
+#if CUDA_VERSION >= 11020
         if (async) {
             CUDA_CALL(cudaMallocAsync(&ptr, size, 0), ": size=" << size);
             cudaStreamSynchronize(0);
         } else {
+#endif
             CUDA_CALL(cudaMalloc(&ptr, size), ": size=" << size);
+#if CUDA_VERSION >= 11020
         }
+#endif
         return ptr;
     case UCS_MEMORY_TYPE_CUDA_MANAGED:
         CUDA_CALL(cudaMallocManaged(&ptr, size), ": size=" << size);
@@ -272,12 +276,16 @@ void mem_buffer::release(void *ptr, ucs_memory_type_t mem_type, bool async)
             break;
 #if HAVE_CUDA
         case UCS_MEMORY_TYPE_CUDA:
+#if CUDA_VERSION >= 11020
             if (async) {
                 cudaStreamSynchronize(0);
                 CUDA_CALL(cudaFreeAsync(ptr, 0), ": ptr=" << ptr);
             } else {
+#endif
                 CUDA_CALL(cudaFree(ptr), ": ptr=" << ptr);
+#if CUDA_VERSION >= 11020
             }
+#endif
             break;
         case UCS_MEMORY_TYPE_CUDA_MANAGED:
             CUDA_CALL(cudaFree(ptr), ": ptr=" << ptr);
