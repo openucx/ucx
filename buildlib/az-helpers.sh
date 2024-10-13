361017721166f5aa305d7a238356f830da30509c
Michal Shalev <mshalev@nvidia.com>
2024-10-13 12:45:19 +0000

    CI: cpu utilization

--- buildlib/az-helpers.sh
+++ buildlib/az-helpers.sh
@@ -452,6 +452,21 @@ function log_cpu_affinity() {
     taskset -p $$ | sed 's/^/    /'
 }
 
+function log_cpu_utilization() {
+    echo -e "\n${COLORS[CYAN]}CPU Utilization${COLORS[NC]}"
+    echo -e "${COLORS[BLUE]}----------------------------${COLORS[NC]}"
+    echo "##[group]    CPU Utilization Info"
+    if command -v vmstat &> /dev/null; then
+        echo "##[group]    mpstat Output"
+        vmstat
+        echo "##[endgroup]"
+    else
+        echo -e "${COLORS[RED]}    mpstat command not found.${COLORS[NC]}"
+    fi
+
+    echo "##[endgroup]"
+}
+
 function log_gpu_info() {
     echo -e "\n${COLORS[CYAN]}GPU Information${COLORS[NC]}"
     echo -e "${COLORS[BLUE]}----------------------------${COLORS[NC]}"
@@ -519,6 +534,7 @@ function log_info_on_exit() {
     log_loaded_modules
     log_cpu_info
     log_cpu_affinity
+    log_cpu_utilization
     log_gpu_info
     log_ibv_info
     log_lshca_info
