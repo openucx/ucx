07507062b8df2e536aaa99d33249db1d1b25971e
Shachar Hasson <shasson5@gmail.com>
2024-11-26 10:34:21 +0000

    UCP/TEST: Enhance reconfig test

--- test/gtest/ucp/test_ucp_ep_reconfig.cc
+++ test/gtest/ucp/test_ucp_ep_reconfig.cc
@@ -15,8 +15,10 @@ extern "C" {
 
 class test_ucp_ep_reconfig : public ucp_test {
 protected:
-    using address_t = std::pair<void*, ucp_unpacked_address_t>;
-    using address_p = std::unique_ptr<address_t, void (*)(address_t*)>;
+    using address_t        = std::pair<void*, ucp_unpacked_address_t>;
+    using address_p        = std::unique_ptr<address_t, void (*)(address_t*)>;
+    using mem_buffer_p     = std::unique_ptr<mem_buffer>;
+    using mem_buffer_vec_t = std::vector<mem_buffer_p>;
 
     class entity : public ucp_test_base::entity {
     public:
@@ -52,7 +54,15 @@ protected:
         bool                   m_exclude_ifaces;
     };
 
-    bool is_single_transport()
+    void init_buffers(mem_buffer_vec_t &sbufs, mem_buffer_vec_t &rbufs,
+                      mem_buffer_vec_t &o_sbufs, mem_buffer_vec_t &o_rbufs,
+                      size_t size);
+
+    void pattern_check(const mem_buffer_vec_t &rbufs,
+                       const mem_buffer_vec_t &o_rbufs,
+                       bool bidirectional) const;
+
+    bool is_single_transport() const
     {
         return GetParam().transports.size() == 1;
     }
@@ -103,29 +113,28 @@ public:
     void skip_non_p2p();
 
     void send_message(const ucp_test_base::entity &e1,
-                      const ucp_test_base::entity &e2, const mem_buffer &sbuf,
-                      const mem_buffer &rbuf, size_t msg_size,
-                      std::vector<void*> &reqs)
+                      const ucp_test_base::entity &e2, const mem_buffer *sbuf,
+                      const mem_buffer *rbuf, std::vector<void*> &reqs)
     {
         const ucp_request_param_t param = {
             .op_attr_mask = UCP_OP_ATTR_FLAG_NO_IMM_CMPL
         };
 
-        void *sreq = ucp_tag_send_nbx(e1.ep(), sbuf.ptr(), msg_size, 0, &param);
-        void *sreq_sync = ucp_tag_send_sync_nbx(e1.ep(), sbuf.ptr(), msg_size,
-                                                0, &param);
+        void *sreq = ucp_tag_send_nbx(e1.ep(), sbuf->ptr(), sbuf->size(), 0,
+                                      &param);
+        void *sreq_sync = ucp_tag_send_sync_nbx(e1.ep(), sbuf->ptr(),
+                                                sbuf->size(), 0, &param);
         reqs.insert(reqs.end(), {sreq, sreq_sync});
 
         for (unsigned iter = 0; iter < 2; iter++) {
-            void *rreq = ucp_tag_recv_nbx(e2.worker(), rbuf.ptr(), msg_size, 0,
-                                          0, &param);
+            void *rreq = ucp_tag_recv_nbx(e2.worker(), rbuf->ptr(),
+                                          rbuf->size(), 0, 0, &param);
             reqs.push_back(rreq);
         }
     }
 
     void send_recv(bool bidirectional)
     {
-        static const unsigned num_iterations = 1000;
 /* TODO: remove this when 100MB asan bug is solved */
 #ifdef __SANITIZE_ADDRESS__
         static const size_t msg_sizes[] = {8, 1024, 16384, 65536};
@@ -134,29 +143,27 @@ public:
 #endif
 
         for (auto msg_size : msg_sizes) {
+            mem_buffer_vec_t sbufs, rbufs, o_sbufs, o_rbufs;
+            std::vector<void*> reqs;
+
+            init_buffers(sbufs, rbufs, o_sbufs, o_rbufs, msg_size);
+
             for (unsigned i = 0; i < num_iterations; ++i) {
-                mem_buffer sbuf(msg_size, UCS_MEMORY_TYPE_HOST, i);
-                mem_buffer rbuf(msg_size, UCS_MEMORY_TYPE_HOST, ucs::rand());
-                mem_buffer o_sbuf(msg_size, UCS_MEMORY_TYPE_HOST, i);
-                mem_buffer o_rbuf(msg_size, UCS_MEMORY_TYPE_HOST, ucs::rand());
-
-                std::vector<void*> reqs;
-                send_message(sender(), receiver(), sbuf, rbuf, msg_size, reqs);
+                send_message(sender(), receiver(), sbufs[i].get(),
+                             rbufs[i].get(), reqs);
 
                 if (bidirectional) {
-                    send_message(receiver(), sender(), o_sbuf, o_rbuf, msg_size,
-                                 reqs);
-                }
-
-                requests_wait(reqs);
-                rbuf.pattern_check(i);
-
-                if (bidirectional) {
-                    o_rbuf.pattern_check(i);
+                    send_message(receiver(), sender(), o_sbufs[i].get(),
+                                 o_rbufs[i].get(), reqs);
                 }
             }
+
+            requests_wait(reqs);
+            pattern_check(rbufs, o_rbufs, bidirectional);
         }
     }
+
+    static constexpr unsigned num_iterations = 1000;
 };
 
 void test_ucp_ep_reconfig::entity::store_config()
@@ -288,6 +295,36 @@ test_ucp_ep_reconfig::entity::get_address(const ucp_tl_bitmap_t &tl_bitmap) cons
     return address;
 }
 
+void test_ucp_ep_reconfig::init_buffers(mem_buffer_vec_t &sbufs,
+                                        mem_buffer_vec_t &rbufs,
+                                        mem_buffer_vec_t &o_sbufs,
+                                        mem_buffer_vec_t &o_rbufs, size_t size)
+{
+    for (unsigned i = 0; i < num_iterations; ++i) {
+        sbufs.push_back(
+                mem_buffer_p(new mem_buffer(size, UCS_MEMORY_TYPE_HOST, i)));
+        rbufs.push_back(mem_buffer_p(
+                new mem_buffer(size, UCS_MEMORY_TYPE_HOST, ucs::rand())));
+        o_sbufs.push_back(
+                mem_buffer_p(new mem_buffer(size, UCS_MEMORY_TYPE_HOST, i)));
+        o_rbufs.push_back(mem_buffer_p(
+                new mem_buffer(size, UCS_MEMORY_TYPE_HOST, ucs::rand())));
+    }
+}
+
+void test_ucp_ep_reconfig::pattern_check(const mem_buffer_vec_t &rbufs,
+                                         const mem_buffer_vec_t &o_rbufs,
+                                         bool bidirectional) const
+{
+    for (unsigned i = 0; i < num_iterations; ++i) {
+        rbufs[i]->pattern_check(i);
+
+        if (bidirectional) {
+            o_rbufs[i]->pattern_check(i);
+        }
+    }
+}
+
 void test_ucp_ep_reconfig::run(bool bidirectional)
 {
     create_entities_and_connect();
@@ -309,8 +346,7 @@ void test_ucp_ep_reconfig::skip_non_p2p()
 }
 
 /* TODO: Remove skip condition after next PRs are merged. */
-UCS_TEST_SKIP_COND_P(test_ucp_ep_reconfig, basic,
-                     !has_transport("rc_x") || !has_transport("rc_v"))
+UCS_TEST_SKIP_COND_P(test_ucp_ep_reconfig, basic, !has_transport("rc"))
 {
     run();
 }
