c64164fb2d94b04eedc8896094e33d88afaa9eca
Arun Chandran <Arun.Chandran@amd.com>
2024-12-05 16:05:24 +0530

    RFC: UCS/ARCH: Avoid direct usage of REP MOVSB in favor of memcpy
    Glibc has two tunable parameters that control the use of 'REP MOVSB':
    
    a) __x86_rep_movsb_threshold
    b) __x86_rep_movsb_stop_threshold
    
    Ref: https://elixir.bootlin.com/glibc/glibc-2.34/source/sysdeps/x86_64/multiarch/memmove-vec-unaligned-erms.S
    
    Users can set the desired value of __x86_rep_movsb_threshold through
    the tunable glibc.cpu.x86_rep_movsb_threshold and can decide
    which length range uses ERMS and which range uses
    vectorized routines.
    
    Ref: https://www.gnu.org/software/libc/manual/html_node/Tunables.html
    
    These tunable parameters are set based on the x86 CPU type and
    available hardware features.  Instead of replicating this infrastructure
    in UCX, we can utilize the 'REP MOVSB' mechanism provided by memcpy().

--- src/ucs/arch/x86_64/global_opts.h
+++ src/ucs/arch/x86_64/global_opts.h
@@ -15,16 +15,12 @@
 BEGIN_C_DECLS
 
 #define UCS_ARCH_GLOBAL_OPTS_INITALIZER { \
-    .builtin_memcpy_min     = UCS_MEMUNITS_AUTO, \
-    .builtin_memcpy_max     = UCS_MEMUNITS_AUTO, \
     .nt_buffer_transfer_min = UCS_MEMUNITS_AUTO, \
     .nt_dest_threshold      = UCS_MEMUNITS_AUTO  \
 }
 
-/* built-in memcpy & nt-buffer-transfer config */
+/*  nt-buffer-transfer config */
 typedef struct ucs_arch_global_opts {
-    size_t builtin_memcpy_min;
-    size_t builtin_memcpy_max;
     size_t nt_buffer_transfer_min;
     size_t nt_dest_threshold;
 } ucs_arch_global_opts_t;
