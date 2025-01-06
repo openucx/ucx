efcbcd56c88bf5e3c0b19b5ac43ee6f00b89ceea
Akshay Venkatesh <akvenkatesh@nvidia.com>
2025-01-06 22:16:10 +0000

    UCT/CUDA_IPC: build fix for non-fabric cases

--- src/uct/cuda/cuda_ipc/cuda_ipc_cache.c
+++ src/uct/cuda/cuda_ipc/cuda_ipc_cache.c
@@ -480,9 +480,8 @@ UCS_PROFILE_FUNC(ucs_status_t, uct_cuda_ipc_map_memhandle, (key, mapped_addr),
     ucs_pgt_region_t *pgt_region;
     uct_cuda_ipc_cache_region_t *region;
     int ret;
+    const void *arg1, *arg2;
     size_t cmp_size;
-
-    cmp_size = sizeof(key->ph.buffer_id);
 
     status = uct_cuda_ipc_get_remote_cache(key->pid, &cache);
     if (status != UCS_OK) {
@@ -494,8 +493,16 @@ UCS_PROFILE_FUNC(ucs_status_t, uct_cuda_ipc_map_memhandle, (key, mapped_addr),
                                   &cache->pgtable, key->d_bptr);
     if (ucs_likely(pgt_region != NULL)) {
         region = ucs_derived_of(pgt_region, uct_cuda_ipc_cache_region_t);
-        if (memcmp((const void *)&key->ph.buffer_id,
-                   (const void *)&region->key.ph.buffer_id, cmp_size) == 0) {
+#if HAVE_CUDA_FABRIC
+    cmp_size = sizeof(key->ph.buffer_id);
+    arg1     = (const void *)&key->ph.buffer_id;
+    arg2     = (const void *)&region->key.ph.buffer_id;
+#else
+    cmp_size = sizeof(key->ph);
+    arg1     = (const void *)&key->ph;
+    arg2     = (const void *)&region->key.ph;
+#endif
+        if (memcmp(arg1, arg2, cmp_size) == 0) {
             /*cache hit */
             ucs_trace("%s: cuda_ipc cache hit addr:%p size:%lu region:"
                       UCS_PGT_REGION_FMT, cache->name, (void *)key->d_bptr,
