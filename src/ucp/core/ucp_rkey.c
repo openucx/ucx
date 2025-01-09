68c0538086ad0874d34bc70f0968d952b462b147
Ilia Yastrebov <iyastrebov@nvidia.com>
2025-01-09 20:25:31 +0000

    UCP: Invalidation for pipeline rtr

--- src/ucp/core/ucp_rkey.c
+++ src/ucp/core/ucp_rkey.c
@@ -156,6 +156,8 @@ UCS_PROFILE_FUNC(ssize_t, ucp_rkey_pack_memh,
     *ucs_serialize_next(&p, ucp_md_map_t) = md_map;
     *ucs_serialize_next(&p, uint8_t)      = mem_info->type;
 
+    memh = ucp_memh_get_pack_memh(memh, md_map, uct_flags, 1);
+
     params.field_mask = UCT_MD_MKEY_PACK_FIELD_FLAGS;
     /* Write both size and rkey_buffer for each UCT rkey */
     ucs_for_each_bit (md_index, md_map) {
