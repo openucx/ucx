5fb7b89005855357a1f32c99ec4b87d0d5c3736e
Yossi Itigin <yosefe@nvidia.com>
2025-02-26 11:56:58 +0200

    TEST/UCT/IFACE: Fix CI failures

--- test/gtest/uct/test_uct_iface.cc
+++ test/gtest/uct/test_uct_iface.cc
@@ -31,44 +31,56 @@ protected:
     }
 
 private:
-    entity *m_entity;
+    entity *m_entity{nullptr};
 };
 
 void test_uct_iface::test_is_reachable()
 {
     const auto &iface_attr = get_entity().iface_attr();
     auto iface             = get_entity().iface();
-
-    auto dev_addr   = (uct_device_addr_t*)malloc(iface_attr.device_addr_len);
-    auto iface_addr = (uct_iface_addr_t*)malloc(iface_attr.iface_addr_len);
+    uct_iface_is_reachable_params_t params;
     ucs_status_t status;
 
-    status = uct_iface_get_device_address(iface, dev_addr);
-    ASSERT_UCS_OK(status);
-
-    status = uct_iface_get_address(iface, iface_addr);
-    ASSERT_UCS_OK(status);
-
     char info_str[256];
-    uct_iface_is_reachable_params_t params;
     params.field_mask         = UCT_IFACE_IS_REACHABLE_FIELD_DEVICE_ADDR |
                                 UCT_IFACE_IS_REACHABLE_FIELD_IFACE_ADDR |
                                 UCT_IFACE_IS_REACHABLE_FIELD_INFO_STRING |
                                 UCT_IFACE_IS_REACHABLE_FIELD_INFO_STRING_LENGTH |
                                 UCT_IFACE_IS_REACHABLE_FIELD_DEVICE_ADDR_LENGTH |
                                 UCT_IFACE_IS_REACHABLE_FIELD_IFACE_ADDR_LENGTH;
-    params.device_addr        = dev_addr;
-    params.iface_addr         = iface_addr;
     params.info_string        = info_str;
     params.info_string_length = sizeof(info_str);
     params.device_addr_length = iface_attr.device_addr_len;
     params.iface_addr_length  = iface_attr.iface_addr_len;
 
+    auto dev_addr = (uct_device_addr_t*)malloc(iface_attr.device_addr_len);
+    status        = uct_iface_get_device_address(iface, dev_addr);
+    ASSERT_UCS_OK(status);
+    params.device_addr = dev_addr;
+
+    auto iface_addr = (uct_iface_addr_t*)malloc(iface_attr.iface_addr_len);
+    status          = uct_iface_get_address(iface, iface_addr);
+    ASSERT_UCS_OK(status);
+    params.iface_addr = iface_addr;
+
     bool is_reachable = uct_iface_is_reachable_v2(iface, &params);
     EXPECT_EQ(is_self_reachable(), is_reachable);
 
-    memset(dev_addr, 0xff, iface_attr.device_addr_len);
-    memset(iface_addr, 0xff, iface_attr.iface_addr_len);
+    free(iface_addr);
+    free(dev_addr);
+
+    // Allocate corrupted address buffers, make it larger than the correct
+    // buffer size in case the corrupted data indicates a larger address length
+
+    size_t invalid_dev_addr_len = ucs_min(4096, iface_attr.device_addr_len);
+    dev_addr = (uct_device_addr_t*)malloc(invalid_dev_addr_len);
+    ucs::fill_random((uint8_t*)dev_addr, invalid_dev_addr_len);
+    params.device_addr = dev_addr;
+
+    size_t invalid_iface_addr_len = ucs_min(4096, iface_attr.iface_addr_len);
+    iface_addr = (uct_iface_addr_t*)malloc(invalid_iface_addr_len);
+    ucs::fill_random((uint8_t*)iface_addr, invalid_iface_addr_len);
+    params.iface_addr = iface_addr;
 
     // Corrupted device and iface address should not be reachable, and should
     // provide the reason in the info string
