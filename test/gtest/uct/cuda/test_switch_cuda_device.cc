95e7493cf46a091b7b800d60ff84144e020ebfbf
rakhmets <rakhmetshin@nvidia.com>
2025-02-26 18:41:37 +0200

    GTEST/UCT/CUDA: Updated test.

--- test/gtest/uct/cuda/test_switch_cuda_device.cc
+++ test/gtest/uct/cuda/test_switch_cuda_device.cc
@@ -25,26 +25,15 @@ void test_p2p_switch_cuda_device::test_xfer(send_func_t send, size_t length,
         UCS_TEST_SKIP_R("less than two cuda devices available");
     }
 
-    mapped_buffer sendbuf(length, SEED1, sender(), 0, UCS_MEMORY_TYPE_HOST);
+    uct_p2p_rma_test::test_xfer(send, length, flags, mem_type);
 
     int current_device;
     ASSERT_EQ(cudaGetDevice(&current_device), cudaSuccess);
     ASSERT_EQ(cudaSetDevice((current_device + 1) % num_devices), cudaSuccess);
 
-    mapped_buffer recvbuf(length, SEED2, receiver(), 0, mem_type);
+    uct_p2p_rma_test::test_xfer(send, length, flags, mem_type);
 
     EXPECT_EQ(cudaSetDevice(current_device), cudaSuccess);
-
-    blocking_send(send, sender_ep(), sendbuf, recvbuf, true);
-    if (flags & TEST_UCT_FLAG_SEND_ZCOPY) {
-        sendbuf.memset(0);
-        wait_for_remote();
-        recvbuf.pattern_check(SEED1);
-    } else if (flags & TEST_UCT_FLAG_RECV_ZCOPY) {
-        recvbuf.memset(0);
-        sendbuf.pattern_check(SEED2);
-        wait_for_remote();
-    }
 }
 
 UCS_TEST_P(test_p2p_switch_cuda_device, put_short)
