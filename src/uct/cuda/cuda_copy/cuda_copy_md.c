5b29e69114970825cea8dbc4456b67b2b2b9a6a5
rakhmets <rakhmetshin@nvidia.com>
2025-03-01 16:20:12 +0200

    UCT/CUDA/CUDA_COPY: Resolved merge conflict in cuda_copy_md.c.

--- src/uct/cuda/cuda_copy/cuda_copy_md.c
+++ src/uct/cuda/cuda_copy/cuda_copy_md.c
@@ -681,14 +681,8 @@ uct_cuda_copy_md_query_attributes(uct_cuda_copy_md_t *md, const void *address,
         goto out_default_range;
     }
 
-    /* GetAddressRange requires context to be set. On DGXA100 it takes 0.03 us
-     * to push and pop the context associated with address (which should be
-     * non-NULL if we are at this point)*/
-    cuCtxPushCurrent(cuda_mem_ctx);
-
     cu_err = cuMemGetAddressRange(&base_address, &alloc_length,
                                   (CUdeviceptr)address);
-    cuCtxPopCurrent(&cuda_mem_ctx);
     if (cu_err != CUDA_SUCCESS) {
         ucs_error("cuMemGetAddressRange(%p) error: %s", address,
                   uct_cuda_base_cu_get_error_string(cu_err));
