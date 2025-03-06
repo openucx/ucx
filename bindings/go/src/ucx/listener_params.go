ba5cc7e061c03116201eaeb0f29cfabd26b775cb
Ofir Farjon <ofarjon@nvidia.com>
2025-03-06 13:05:51 +0000

    BINDINGS/GO: Bug fix - make gobindings thread safe.

--- bindings/go/src/ucx/listener_params.go
+++ bindings/go/src/ucx/listener_params.go
@@ -16,18 +16,17 @@ import (
 
 // Tuning parameters for the UCP listener.
 type UcpListenerParams struct {
-	params        C.ucp_listener_params_t
-	connHandlerId uint64
+	params   C.ucp_listener_params_t
+	callback UcpListenerConnectionHandler
 }
 
 //export ucxgo_completeConnHandler
-func ucxgo_completeConnHandler(connRequest C.ucp_conn_request_h, cbId unsafe.Pointer) {
-	id := uint64(uintptr((cbId)))
-	if callback, found := getCallback(id); found {
-		listener := connHandles2Listener[id]
-		callback.(UcpListenerConnectionHandler)(&UcpConnectionRequest{
+func ucxgo_completeConnHandler(connRequest C.ucp_conn_request_h, packedCb unsafe.Pointer) {
+	if callback := PackedCallback(packedCb).unpack(); callback != nil {
+		listener := callback.(*UcpListener)
+		listener.callback(&UcpConnectionRequest{
 			connRequest: connRequest,
-			listener:    listener,
+			listener:    listener.listener,
 		})
 	}
 }
@@ -49,10 +48,8 @@ func (p *UcpListenerParams) SetSocketAddress(a *net.TCPAddr) (*UcpListenerParams
 // Handler of an incoming connection request in a client-server connection flow.
 func (p *UcpListenerParams) SetConnectionHandler(connHandler UcpListenerConnectionHandler) *UcpListenerParams {
 	var ucpConnHndl C.ucp_listener_conn_handler_t
-	cbId := register(connHandler)
 
-	p.connHandlerId = cbId
-	ucpConnHndl.arg = unsafe.Pointer(uintptr(cbId))
+	p.callback = connHandler
 	ucpConnHndl.cb = (C.ucp_listener_conn_callback_t)(C.ucxgo_completeConnHandler)
 	p.params.field_mask |= C.UCP_LISTENER_PARAM_FIELD_CONN_HANDLER
 	p.params.conn_handler = ucpConnHndl
