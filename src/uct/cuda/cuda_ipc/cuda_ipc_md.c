6ee2eb158fbce076438b8bf5c79326792185406e
Evgeny Leksikov <evgenylek@nvidia.com>
2025-05-16 17:54:19 +0300

    UCT/CUDA: fix merge issue

--- src/uct/cuda/cuda_ipc/cuda_ipc_md.c
+++ src/uct/cuda/cuda_ipc/cuda_ipc_md.c
@@ -495,11 +495,8 @@ uct_cuda_ipc_mem_reg(uct_md_h md, void *address, size_t length,
                      const uct_md_mem_reg_params_t *params, uct_mem_h *memh_p)
 {
     uct_cuda_ipc_memh_t *memh;
-    CUdevice cu_device;
 
     UCT_CHECK_NOT_DERIVED_MEMH(params, "cuda_ipc_md");
-
-    UCT_CUDA_IPC_GET_DEVICE(cu_device);
 
     memh = ucs_malloc(sizeof(*memh), "uct_cuda_ipc_memh_t");
     if (NULL == memh) {
