6ccee8960dea288ae73d7a52cfc25d89ce0b3fdd
Thomas Vegas <tvegas@nvidia.com>
2025-08-21 18:02:29 +0000

    GTEST/UCP: Add CUDA file to build system

--- test/gtest/ucp/cuda/test_cuda_device.cu
+++ test/gtest/ucp/cuda/test_cuda_device.cu
@@ -4,14 +4,13 @@
  * See file LICENSE for terms.
  */
 
-#include <cuda.h>
+#include <cuda_runtime.h>
 #include <ucp/ucp_test.h>
 
 static __global__ void memcmp_kernel(const void* a, const void* b,
                                      int* result, size_t size) {
     unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    result[0] = 0;
     for (size_t i = idx; i < size; i += blockDim.x * gridDim.x) {
         if (reinterpret_cast<const uint8_t*>(a)[i]
             != reinterpret_cast<const uint8_t*>(b)[i]) {
@@ -31,7 +30,7 @@ protected:
     // Compare generic CUDA buffers without copying them
     static int cuda_memcmp(const void *a, const void *b, size_t size)
     {
-        int h_result = 1;
+        int h_result = 0;
         int* d_result;
 
         if (cudaMalloc(&d_result, sizeof(*d_result)) != cudaSuccess) {
@@ -40,6 +39,7 @@ protected:
 
         if (cudaMemcpy(d_result, &h_result, sizeof(h_result), cudaMemcpyHostToDevice)
             != cudaSuccess) {
+            h_result = -1;
             goto out;
         }
 
@@ -47,6 +47,7 @@ protected:
         cudaDeviceSynchronize();
         if (cudaMemcpy(&h_result, d_result, sizeof(h_result), cudaMemcpyDeviceToHost)
             != cudaSuccess) {
+            h_result = -1;
             goto out;
         }
 
@@ -59,19 +60,19 @@ out:
 UCS_TEST_P(test_ucp_cuda_device, cuda_kernel_memcmp)
 {
     size_t size = 100 * UCS_MBYTE;
-    uint8_t *data, *src, *dst;
+    uint8_t *data;
 
     ASSERT_EQ(cudaSuccess, cudaMalloc(&data, 2 * size));
-    src = data;
-    dst = static_cast<uint8_t*>(UCS_PTR_BYTE_OFFSET(data, size));
+    uint8_t *src = data;
+    uint8_t *dst = static_cast<uint8_t*>(UCS_PTR_BYTE_OFFSET(data, size));
 
-    ASSERT_EQ(cudaSuccess, cudaMemset(src, 0x11, size));
-    ASSERT_EQ(cudaSuccess, cudaMemset(dst, 0xde, size));
+    EXPECT_EQ(cudaSuccess, cudaMemset(src, 0x11, size));
+    EXPECT_EQ(cudaSuccess, cudaMemset(dst, 0xde, size));
 
     EXPECT_EQ(1, cuda_memcmp(src, dst, size));
-    ASSERT_EQ(cudaSuccess, cudaMemset(dst, 0x11, size));
+    EXPECT_EQ(cudaSuccess, cudaMemset(dst, 0x11, size));
     EXPECT_EQ(0, cuda_memcmp(src, dst, size));
-    ASSERT_EQ(cudaSuccess, cudaMemset(dst + size/10, 0xfa, 10));
+    EXPECT_EQ(cudaSuccess, cudaMemset(dst + size/10, 0xfa, 10));
     EXPECT_EQ(1, cuda_memcmp(src, dst, size));
 
     EXPECT_EQ(cudaSuccess, cudaFree(data));
