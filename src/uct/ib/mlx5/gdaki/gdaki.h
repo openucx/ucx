34317a76c8b30575ffa5b69befa4252a09ba01aa
Artemy Kovalyov <artemyko@nvidia.com>
2025-09-02 10:17:52 +0300

    UCT/GDAKI: Export EP to GPU (#10833)

--- src/uct/ib/mlx5/gdaki/gdaki.h
+++ src/uct/ib/mlx5/gdaki/gdaki.h
@@ -3,8 +3,8 @@
  * See file LICENSE for terms.
  */
 
-#ifndef UCT_GDAKI_IFACE_H
-#define UCT_GDAKI_IFACE_H
+#ifndef UCT_GDAKI_H
+#define UCT_GDAKI_H
 
 #include <uct/ib/mlx5/rc/rc_mlx5_common.h>
 #include <uct/base/uct_iface.h>
@@ -12,6 +12,7 @@
 #include <cuda.h>
 #include <doca_gpunetio.h>
 
+#include "gdaki_dev.h"
 
 #define UCT_DEV_CUDA_NAME "cuda"
 #define UCT_DEV_CUDA_NAME_LEN 4
@@ -25,9 +26,13 @@ typedef struct uct_rc_gdaki_iface {
 
 
 typedef struct uct_rc_gdaki_ep {
-    uct_base_ep_t      super;
-    uct_ib_mlx5_cq_t   cq;
-    uct_ib_mlx5_txwq_t qp;
+    uct_base_ep_t                super;
+    uct_ib_mlx5_cq_t             cq;
+    uct_ib_mlx5_txwq_t           qp;
+    struct mlx5dv_devx_umem      *umem;
+    struct doca_gpu_verbs_qp     *qp_cpu;
+    struct doca_gpu_dev_verbs_qp *qp_gpu;
+    uct_rc_gdaki_dev_ep_t        *ep_gpu;
 } uct_rc_gdaki_ep_t;
 
 #endif /* UCT_GDAKI_IFACE_H */
