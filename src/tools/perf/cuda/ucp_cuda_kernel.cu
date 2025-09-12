135b242d2738caedfadb7e65198758d5e1a90d2c
Yossi Itigin <yosefe@nvidia.com>
2025-09-12 12:47:32 +0300

    TOOLS/PERF: Perftest cuda kernel fixes

--- src/tools/perf/cuda/ucp_cuda_kernel.cu
+++ src/tools/perf/cuda/ucp_cuda_kernel.cu
@@ -23,7 +23,7 @@ ucp_perf_cuda_put_multi_bw_kernel(ucx_perf_cuda_context &ctx)
 
     for (ucx_perf_counter_t idx = 0; idx < max_iters; idx++) {
         // TODO: replace with actual put multi call
-        __nanosleep(1000000); // 1ms
+        __nanosleep(100000); // 100us
 
         ucx_perf_cuda_update_report(ctx, idx + 1, max_iters, last_report_time);
         __syncthreads();
@@ -40,23 +40,19 @@ ucp_perf_cuda_put_multi_latency_kernel(ucx_perf_cuda_context &ctx, bool is_sende
     for (ucx_perf_counter_t idx = 0; idx < max_iters; idx++) {
         // TODO: replace with actual put multi call
         // TODO: wait for completion
-        __nanosleep(1000000); // 1ms
+        __nanosleep(100000); // 100us
 
         ucx_perf_cuda_update_report(ctx, idx + 1, max_iters, last_report_time);
         __syncthreads();
     }
 }
 
-class ucp_perf_cuda_test_runner:
-    public ucx_perf_cuda_test_runner<ucp_perf_test_runner_base<uint64_t>> {
+class ucp_perf_cuda_test_runner : public ucx_perf_cuda_test_runner {
 public:
-    using psn_t = uint64_t;
-
     ucp_perf_cuda_test_runner(ucx_perf_context_t &perf) :
-        ucx_perf_cuda_test_runner<ucp_perf_test_runner_base<uint64_t>>(perf)
+        ucx_perf_cuda_test_runner(perf)
     {
         size_t length = ucx_perf_get_message_size(&m_perf.params);
-        ucs_assert(length >= sizeof(psn_t));
 
         m_perf.send_allocator->memset(m_perf.send_buffer, 0, length);
         m_perf.recv_allocator->memset(m_perf.recv_buffer, 0, length);
@@ -74,8 +70,9 @@ public:
         ucp_perf_cuda_put_multi_latency_kernel
             <UCP_DEVICE_LEVEL_BLOCK><<<1, thread_count>>>(gpu_ctx(), my_index);
         CUDA_CALL(UCS_ERR_NO_DEVICE, cudaGetLastError);
-
         wait_for_kernel(length);
+
+        CUDA_CALL(UCS_ERR_IO_ERROR, cudaDeviceSynchronize);
         ucx_perf_get_time(&m_perf);
         ucp_perf_barrier(&m_perf);
         return UCS_OK;
@@ -94,35 +91,14 @@ public:
             ucp_perf_cuda_put_multi_bw_kernel<UCP_DEVICE_LEVEL_BLOCK>
                                              <<<1, thread_count>>>(gpu_ctx());
             CUDA_CALL(UCS_ERR_NO_DEVICE, cudaGetLastError);
-
             wait_for_kernel(length);
-
-            // TODO: remove once real GDAKI is used
-            send_signal(length);
-        } else if (my_index == 0) {
-            wait_for_sn(length);
         }
+        // TODO run receiver kernel
 
+        CUDA_CALL(UCS_ERR_IO_ERROR, cudaDeviceSynchronize);
         ucx_perf_get_time(&m_perf);
         ucp_perf_barrier(&m_perf);
         return UCS_OK;
-    }
-
-private:
-    // TODO: remove once real GDAKI is used
-    void send_signal(size_t length)
-    {
-        ucs_memory_type_t mem_type = m_perf.params.send_mem_type;
-        write_sn(m_perf.send_buffer, mem_type, length, m_perf.params.max_iter,
-                 m_perf.ucp.self_send_rkey);
-
-        ucs_status_ptr_t request;
-        ucp_request_param_t param = {0};
-        request = ucp_put_nbx(m_perf.ucp.ep, m_perf.send_buffer, length,
-                              m_perf.ucp.remote_addr, m_perf.ucp.rkey, &param);
-        request_wait(request, mem_type, "write_sn");
-        request = ucp_ep_flush_nbx(m_perf.ucp.self_ep, &param);
-        request_wait(request, mem_type, "flush write_sn");
     }
 };
 
