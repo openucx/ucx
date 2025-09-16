87d18197115548020b18d61a8dc02b5eb79aaaa1
Alexey Rivkin <arivkin@nvidia.com>
2025-09-16 21:50:51 +0300

    AZP/RELEASE: Add CUDA 13 support - v1.19.x
    (ported from PR #10788)
    
    Signed-off-by: Alexey Rivkin <arivkin@nvidia.com>

--- buildlib/az-helpers.sh
+++ buildlib/az-helpers.sh
@@ -188,14 +188,20 @@ try_load_cuda_env() {
     num_gpus=$(nvidia-smi -L | grep GPU | wc -l)
     [ "${num_gpus}" -gt 0 ] || return 0
 
-    # Check cuda env module
-    az_module_load dev/cuda12.8 || return 0
-    have_cuda=yes
+    # Prefer local CUDA if available
+    cuda_local_dir="/usr/local/cuda"
+    if find ${cuda_local_dir}/ -name 'libcudart.so.1[2-9]*' | grep -q .; then
+        have_cuda="${cuda_local_dir}"
+    else
+        # Fallback to env module
+        az_module_load dev/cuda13.0.0 || return 0
+        have_cuda=yes
+    fi
 
     # Check gdrcopy
     if [ -w "/dev/gdrdrv" ]
     then
-        az_module_load dev/gdrcopy2.4.4_cuda12.8.0 && have_gdrcopy=yes
+        az_module_load dev/gdrcopy2.5.1_cuda13.0.0 && have_gdrcopy=yes
     fi
 }
 
