184799844a67c2cc5873796f8f4102e6bd160e83
Michal Shalev <mshalev@nvidia.com>
2025-09-22 12:52:35 +0300

    UCP/DEVICE: Add local and remote address fields to memory list element

--- src/ucp/api/device/ucp_host.h
+++ src/ucp/api/device/ucp_host.h
@@ -34,8 +34,10 @@ BEGIN_C_DECLS
  * It is used to enable backward compatibility support.
  */
 enum ucp_device_mem_list_elem_field {
-    UCP_DEVICE_MEM_LIST_ELEM_FIELD_MEMH = UCS_BIT(0), /**< Source memory handle */
-    UCP_DEVICE_MEM_LIST_ELEM_FIELD_RKEY = UCS_BIT(1)  /**< Unpacked remote memory key */
+    UCP_DEVICE_MEM_LIST_ELEM_FIELD_MEMH   = UCS_BIT(0), /**< Source memory handle */
+    UCP_DEVICE_MEM_LIST_ELEM_FIELD_RKEY   = UCS_BIT(1), /**< Unpacked remote memory key */
+    UCP_DEVICE_MEM_LIST_ELEM_FIELD_L_ADDR = UCS_BIT(2), /**< Local address */
+    UCP_DEVICE_MEM_LIST_ELEM_FIELD_R_ADDR = UCS_BIT(3)  /**< Remote address */
 };
 
 
@@ -59,6 +61,16 @@ typedef struct ucp_device_mem_list_elem {
      * Local memory registration handle.
      */
     ucp_mem_h  memh;
+
+    /**
+     * Local address of the memory to be transferred.
+     */
+    void *     l_addr;
+
+    /**
+     * Remote address of the memory to be transferred to.
+     */
+    uint64_t   r_addr;
 
     /**
      * Unpacked memory key for the remote memory endpoint.
