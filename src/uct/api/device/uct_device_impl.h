5fe5dafc9397446be65ae54332b4f12802f82ce9
Michal Shalev <mshalev@nvidia.com>
2025-10-14 12:38:07 +0300

    DEVICE: Document return status for device APIs (#10940)

--- src/uct/api/device/uct_device_impl.h
+++ src/uct/api/device/uct_device_impl.h
@@ -42,6 +42,11 @@ union uct_device_completion {
  * @param [in]  flags           Flags to modify the function behavior.
  * @param [in]  comp            Completion object to track the progress of operation.
  *
+ * @return UCS_INPROGRESS     - Operation successfully posted, use @ref
+ *                              uct_device_ep_progress and @ref
+ *                              uct_device_ep_check_completion to
+ *                              check for completion.
+ * @return UCS_OK             - Operation completed successfully.
  * @return Error code as defined by @ref ucs_status_t
  */
 template<ucs_device_level_t level>
@@ -83,6 +88,11 @@ UCS_F_DEVICE ucs_status_t uct_device_ep_put_single(
  * @param [in]  flags           Flags to modify the function behavior.
  * @param [in]  comp            Completion object to track the progress of operation.
  *
+ * @return UCS_INPROGRESS      - Operation successfully posted, use @ref
+ *                               uct_device_ep_progress and @ref
+ *                               uct_device_ep_check_completion to check
+ *                               for completion.
+ * @return UCS_OK              - Operation completed successfully.
  * @return Error code as defined by @ref ucs_status_t
  */
 template<ucs_device_level_t level>
@@ -139,6 +149,11 @@ UCS_F_DEVICE ucs_status_t uct_device_ep_atomic_add(
  * @param [in]  flags                  Flags to modify the function behavior.
  * @param [out] req                    Request populated by the call.
  *
+ * @return UCS_INPROGRESS            - Operation successfully posted, use @ref
+ *                                     uct_device_ep_progress and @ref
+ *                                     uct_device_ep_check_completion to check
+ *                                     for completion.
+ * @return UCS_OK                    - Operation completed successfully.
  * @return Error code as defined by @ref ucs_status_t
  */
 template<ucs_device_level_t level>
@@ -215,6 +230,11 @@ UCS_F_DEVICE ucs_status_t uct_device_ep_put_multi(
  * @param [in]  flags                  Flags to modify the function behavior.
  * @param [in]  comp                   Completion object to track progress.
  *
+ * @return UCS_INPROGRESS            - Operation successfully posted, use @ref
+ *                                     uct_device_ep_progress and @ref
+ *                                     uct_device_ep_check_completion to check
+ *                                     for completion.
+ * @return UCS_OK                    - Operation completed successfully.
  * @return Error code as defined by @ref ucs_status_t
  */
 template<ucs_device_level_t level>
