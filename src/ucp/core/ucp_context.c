e6cdd13c79a981ecf393bf14ca3abbb6015f004a
Shachar Hasson <shasson5@gmail.com>
2025-12-17 16:36:47 +0200

    UCP/PROTO: minor fix

--- src/ucp/core/ucp_context.c
+++ src/ucp/core/ucp_context.c
@@ -57,7 +57,7 @@
     _macro(UCP_AM_ID_AM_FIRST) \
     _macro(UCP_AM_ID_AM_MIDDLE) \
     _macro(UCP_AM_ID_AM_SINGLE_REPLY) \
-    _macro(UCP_AM_ID_AM_SINGLE_PSN) \
+    _macro(UCP_AM_ID_AM_SINGLE_PSN)
 
 #define UCP_AM_HANDLER_DECL(_id) extern ucp_am_handler_t ucp_am_handler_##_id;
 
