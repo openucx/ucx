0cb56929f4ef4b1f8e410d517d81cce61c1c6feb
Thomas Vegas <tvegas@nvidia.com>
2026-01-08 18:42:51 +0200

    UCT/IB: Use global traffic with auto configuration

--- src/uct/ib/base/ib_device.c
+++ src/uct/ib/base/ib_device.c
@@ -1005,6 +1005,23 @@ int uct_ib_device_test_roce_gid_index(uct_ib_device_t *dev, uint8_t port_num,
     return 1;
 }
 
+unsigned long
+uct_ib_device_query_roce_tclass(const char *dev_name, uint8_t port_num)
+{
+    char buf[128];
+    unsigned long tclass;
+    int ret;
+
+    ret = ucs_read_file(buf, sizeof(buf) - 1, 1, UCT_IB_DEVICE_SYSFS_ROCE_TC,
+                        dev_name, port_num);
+    if ((ret < 0) || (sscanf(buf, "Global tclass=%lu", &tclass) != 1)) {
+        tclass = UCT_IB_DEFAULT_ROCEV2_DSCP;
+    }
+
+    ucs_debug("'%s:%d' global_traffic_class=%lu", dev_name, port_num, tclass);
+    return tclass;
+}
+
 ucs_status_t
 uct_ib_device_roce_gid_to_sockaddr(sa_family_t af, const void *gid,
                                    struct sockaddr_storage *sock_storage)
