5e8524e02a39b6626172e042c6aa31a28fa13f6d
Ilia Yastrebov <iyastrebov@nvidia.com>
2026-01-12 15:47:22 +0200

    UCP: Separate field for port_speed

--- src/ucp/core/ucp_ep.h
+++ src/ucp/core/ucp_ep.h
@@ -201,9 +201,9 @@ typedef struct ucp_ep_config_key_lane {
     ucp_md_index_t       dst_md_index; /* Destination memory domain index */
     ucs_sys_device_t     dst_sys_dev; /* Destination system device */
     uint8_t              path_index; /* Device path index */
-    ucp_lane_type_mask_t lane_types: 28; /* Which types of operations this lane
-                                            was selected for */
-    uint32_t             port_speed: 4; /* Quantified port speed */
+    ucp_lane_type_mask_t lane_types; /* Which types of operations this lane
+                                        was selected for */
+    uint8_t              port_speed; /* Quantified port speed */
     size_t               seg_size; /* Maximal fragment size which can be
                                       received by the peer */
 } ucp_ep_config_key_lane_t;
