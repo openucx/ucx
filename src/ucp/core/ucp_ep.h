9cf8a2924285c9688785095573076d8c8f8beb25
Evgeny Leksikov <evgenylek@nvidia.com>
2026-01-29 10:22:32 +0100

    UCP/FT: extend error flows to handle set of lanes (#11151)
    * UCP/FT: extend err flows to handle set of lanes
    
    * UCP/FT: extend err flows to handle set of lanes, fix CR1
    
    * UCP/FT: extend err flows to handle set of lanes, fix CR2

--- src/ucp/core/ucp_ep.h
+++ src/ucp/core/ucp_ep.h
@@ -760,10 +760,11 @@ void ucp_ep_disconnected(ucp_ep_h ep, int force);
 void ucp_ep_destroy_internal(ucp_ep_h ep);
 
 ucs_status_t
-ucp_ep_set_failed(ucp_ep_h ucp_ep, ucp_lane_index_t lane, ucs_status_t status);
+ucp_ep_set_lanes_failed(ucp_ep_h ucp_ep, ucp_lane_map_t lanes,
+                        ucs_status_t status);
 
-void ucp_ep_set_failed_schedule(ucp_ep_h ucp_ep, ucp_lane_index_t lane,
-                                ucs_status_t status);
+void ucp_ep_set_lanes_failed_schedule(ucp_ep_h ucp_ep, ucp_lane_map_t lanes,
+                                      ucs_status_t status);
 
 void ucp_ep_unprogress_uct_ep(ucp_ep_h ep, uct_ep_h uct_ep,
                               ucp_rsc_index_t rsc_index);
