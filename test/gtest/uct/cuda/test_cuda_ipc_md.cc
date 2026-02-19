5d6554ff24a99af6167c65aee5aec3ee7dbf040f
Tomer Gilad <tgilad@funk03.mec01.labs.mlnx>
2026-02-19 16:10:27 +0000

    UCT/CUDA_IPC: fixes

--- test/gtest/uct/cuda/test_cuda_ipc_md.cc
+++ test/gtest/uct/cuda/test_cuda_ipc_md.cc
@@ -415,15 +415,6 @@ UCS_TEST_P(test_cuda_ipc_md, posix_fd_same_node_ipc)
             ASSERT_EQ(CUDA_SUCCESS, cuCtxCreate(&ctx, 0, dev));
 #endif
 
-            uct_rkey_unpack_params_t unpack_params = {};
-            uct_rkey_bundle_t rkey_bundle           = {};
-            ucs_status_t unpack_status = uct_rkey_unpack_v2(
-                    component, &rkey, &unpack_params, &rkey_bundle);
-            ASSERT_UCS_OK(unpack_status);
-
-            uct_cuda_ipc_unpacked_rkey_t *unpacked =
-                (uct_cuda_ipc_unpacked_rkey_t *)rkey_bundle.rkey;
-            void *mapped_addr;
             bool pidfd_supported = false;
             int probe_pidfd = syscall(SYS_pidfd_open, getpid(), 0);
             if (probe_pidfd >= 0) {
@@ -436,23 +427,33 @@ UCS_TEST_P(test_cuda_ipc_md, posix_fd_same_node_ipc)
                 close(probe_pidfd);
             }
 
-            ucs_log_level_t log_level = pidfd_supported ?
-                    UCS_LOG_LEVEL_ERROR : UCS_LOG_LEVEL_DIAG;
+            uct_rkey_unpack_params_t unpack_params = {};
+            uct_rkey_bundle_t rkey_bundle           = {};
+            ucs_status_t unpack_status = uct_rkey_unpack_v2(
+                    component, &rkey, &unpack_params, &rkey_bundle);
+
+            if (!pidfd_supported) {
+                ASSERT_EQ(UCS_ERR_UNREACHABLE, unpack_status);
+                cuCtxDestroy(ctx);
+                return;
+            }
+
+            ASSERT_UCS_OK(unpack_status);
+
+            uct_cuda_ipc_unpacked_rkey_t *unpacked =
+                (uct_cuda_ipc_unpacked_rkey_t *)rkey_bundle.rkey;
+            void *mapped_addr;
             ucs_status_t map_status = uct_cuda_ipc_map_memhandle(
-                    &unpacked->super, dev, &mapped_addr, log_level);
+                    &unpacked->super, dev, &mapped_addr,
+                    UCS_LOG_LEVEL_ERROR);
+            ASSERT_UCS_OK(map_status);
 
-            if (pidfd_supported) {
-                ASSERT_UCS_OK(map_status);
-
-                std::vector<uint8_t> host_buf(size);
-                ASSERT_EQ(CUDA_SUCCESS, cuMemcpyDtoH(
-                        host_buf.data(), (CUdeviceptr)mapped_addr, size));
-                for (size_t i = 0; i < size; i++) {
-                    ASSERT_EQ(0xAB, host_buf[i])
-                        << "Data mismatch at byte " << i;
-                }
-            } else {
-                EXPECT_EQ(UCS_ERR_IO_ERROR, map_status);
+            std::vector<uint8_t> host_buf(size);
+            ASSERT_EQ(CUDA_SUCCESS, cuMemcpyDtoH(
+                    host_buf.data(), (CUdeviceptr)mapped_addr, size));
+            for (size_t i = 0; i < size; i++) {
+                ASSERT_EQ(0xAB, host_buf[i])
+                    << "Data mismatch at byte " << i;
             }
 
             uct_rkey_release(component, &rkey_bundle);
