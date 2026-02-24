9117b1bc7867d299db5ced29dca5d1a1d9038609
shasson5 <103439971+shasson5@users.noreply.github.com>
2026-02-24 14:11:26 +0200

    UCT/CUDA_IPC: Enforce host memory support for mem_type EP (#10933)

--- src/ucp/core/ucp_context.c
+++ src/ucp/core/ucp_context.c
@@ -2781,7 +2781,7 @@ void ucp_memory_detect_slowpath(ucp_context_h context, const void *address,
 }
 
 void ucp_context_memaccess_tl_bitmap(ucp_context_h context,
-                                     ucs_memory_type_t mem_type,
+                                     uint64_t mem_type_bitmap,
                                      uint64_t md_reg_flags,
                                      ucp_tl_bitmap_t *tl_bitmap)
 {
@@ -2799,7 +2799,7 @@ void ucp_context_memaccess_tl_bitmap(ucp_context_h context,
         } else {
             mem_types = md_attr->access_mem_types;
         }
-        if (mem_types & UCS_BIT(mem_type)) {
+        if (ucs_test_all_flags(mem_types, mem_type_bitmap)) {
             UCS_STATIC_BITMAP_SET(tl_bitmap, rsc_index);
         }
     }
