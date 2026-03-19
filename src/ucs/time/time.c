ff232a1cfba23412294c04e188e63d005c23b20e
Colin Hirsch <chirsch@nvidia.com>
2026-03-19 10:23:34 +0200

    UCS/TIME: Fix initialization.

--- src/ucs/time/time.c
+++ src/ucs/time/time.c
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) NVIDIA CORPORATION & AFFILIATES, 2001-2014. ALL RIGHTS RESERVED.
+* Copyright (c) NVIDIA CORPORATION & AFFILIATES, 2001-2026. ALL RIGHTS RESERVED.
 *
 * See file LICENSE for terms.
 */
@@ -8,19 +8,20 @@
 #  include "config.h"
 #endif
 
+#include <ucs/sys/compiler_def.h>
 #include <ucs/time/time.h>
 #include <ucs/debug/log.h>
 
 
 double ucs_get_cpu_clocks_per_sec()
 {
-    static double clocks_per_sec = 0.0;
-    static int initialized = 0;
+    static double clocks_per_sec = -42.0;
 
-    if (!initialized) {
+    ucs_compiler_fence();
+
+    if (ucs_unlikely(clocks_per_sec <= 0.0)) {
         clocks_per_sec = ucs_arch_get_clocks_per_sec();
         ucs_debug("arch clock frequency: %.2f Hz", clocks_per_sec);
-        initialized = 1;
     }
     return clocks_per_sec;
 }
