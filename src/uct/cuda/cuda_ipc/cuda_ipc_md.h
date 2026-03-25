235b7b2b2407d61432b21f3b855ac6e186a8abfd
Ilia Yastrebov <iyastrebov@nvidia.com>
2026-03-25 14:59:17 +0200

    CUDA_IPC: Formatting

--- src/uct/cuda/cuda_ipc/cuda_ipc_md.h
+++ src/uct/cuda/cuda_ipc/cuda_ipc_md.h
@@ -49,9 +49,9 @@ typedef struct uct_cuda_ipc_md {
 
 
 typedef struct {
-    uint8_t type; // uct_cuda_ipc_key_handle_t
-    uint8_t is_local;
-    CUuuid  uuid;
+    uint8_t type;     /**< uct_cuda_ipc_key_handle_t */
+    uint8_t is_local; /**< 1 if the key is local (PID+PID_NS), 0 otherwise */
+    CUuuid  uuid;     /**< GPU Device UUID */
 } uct_cuda_ipc_uuid_hash_key_t;
 
 
@@ -79,7 +79,8 @@ static UCS_F_ALWAYS_INLINE khint32_t
 uct_cuda_ipc_uuid_hash_func(uct_cuda_ipc_uuid_hash_key_t key)
 {
     int64_t *i64 = (int64_t *)key.uuid.bytes;
-    return kh_int64_hash_func(i64[0] ^ i64[1] ^ ((key.type << 1) | key.is_local));
+    return kh_int64_hash_func(i64[0] ^ i64[1] ^
+                              ((key.type << 1) | key.is_local));
 }
 
 
