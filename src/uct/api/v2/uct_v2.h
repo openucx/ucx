0c816b09d2ef49527339d238da4c33533afe511c
Michal Shalev <mshalev@nvidia.com>
2026-03-30 05:13:26 -0700

    UCT: Merge fixes

--- src/uct/api/v2/uct_v2.h
+++ src/uct/api/v2/uct_v2.h
@@ -1300,6 +1300,9 @@ ucs_status_t uct_rkey_unpack_v2(uct_component_h component,
 ucs_status_t uct_md_mem_elem_pack(uct_md_h md, uct_mem_h memh, uct_rkey_t rkey,
                                   uct_device_mem_element_t *mem_elem);
 
+typedef ucs_status_t (*uct_iface_query_v2_func_t)(
+        uct_iface_h iface, uct_iface_attr_v2_t *iface_attr);
+
 typedef ucs_status_t (*uct_iface_estimate_perf_func_t)(
         uct_iface_h iface, uct_perf_attr_t *perf_attr);
 
@@ -1333,6 +1336,7 @@ typedef ucs_status_t (*uct_ep_get_device_ep_func_t)(
  * @brief Internal v2 operations table.
  */
 typedef struct uct_iface_internal_ops {
+    uct_iface_query_v2_func_t        iface_query_v2;
     uct_iface_estimate_perf_func_t   iface_estimate_perf;
     uct_iface_vfs_refresh_func_t     iface_vfs_refresh;
     uct_ep_query_func_t              ep_query;
