d3219a19cf78aff35c253fa722957c67a676144d
Roie Danino <rdanino@nvidia.com>
2026-04-05 15:16:08 +0300

    UCT/IB/PLUGIN: added runtime function resolve in stubs
    Signed-off-by: Roie Danino <rdanino@nvidia.com>

--- src/uct/ib/plugin/uct_ib_plugin_stubs.c
+++ src/uct/ib/plugin/uct_ib_plugin_stubs.c
@@ -7,16 +7,27 @@
 #include "config.h"
 #endif
 
+#include <dlfcn.h>
+
+#include <ucs/debug/log.h>
 #include "uct_ib_plugin.h"
 
-uint64_t __attribute__((weak)) uct_ib_plugin_iface_flags(void)
-{
-    return 0;
-}
 
-ucs_status_t __attribute__((weak))
-uct_ib_plugin_qp_query(const uct_ib_plugin_qp_query_params_t *params,
-                       uct_ib_plugin_qp_query_attr_t *attr)
-{
-    return UCS_ERR_UNSUPPORTED;
-}
+#define UCT_IB_PLUGIN_STUB(_ret_type, _name, _default_ret, _params, _args) \
+    typedef _ret_type (*_name##_fn_t) _params;                              \
+    _ret_type _name _params                                                 \
+    {                                                                       \
+        _name##_fn_t real = (_name##_fn_t)dlsym(RTLD_DEFAULT, #_name);      \
+        if (real == _name) {                                                \
+            return _default_ret;                                            \
+        }                                                                   \
+        return real _args;                                                  \
+    }
+
+UCT_IB_PLUGIN_STUB(uint64_t, uct_ib_plugin_iface_flags, 0,
+                   (void), ())
+
+UCT_IB_PLUGIN_STUB(ucs_status_t, uct_ib_plugin_qp_query, UCS_ERR_UNSUPPORTED,
+                   (const uct_ib_plugin_qp_query_params_t *params,
+                    uct_ib_plugin_qp_query_attr_t *attr),
+                   (params, attr))
