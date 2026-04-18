5cdc7f67f0c6fc9f1b8336227916d44e7900dacd
GenericRikka <rikka.goering@outlook.de>
2026-04-18 23:28:37 +0200

    UCS: portability fixes for non-glibc and clang environments

--- src/ucs/sys/sys.h
+++ src/ucs/sys/sys.h
@@ -50,6 +50,8 @@
 #include <net/if.h>
 #include <netdb.h>
 #include <dirent.h>
+#include <limits.h>
+#include <unistd.h>
 
 
 #include <sys/types.h>
@@ -61,6 +63,14 @@ typedef cpu_set_t ucs_sys_cpuset_t;
 typedef cpuset_t ucs_sys_cpuset_t;
 #else
 #error "Port me"
+#endif
+
+#ifndef UCS_HOST_NAME_MAX
+#  ifdef _POSIX_HOST_NAME_MAX
+#    define UCS_HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+#  else
+#    define UCS_HOST_NAME_MAX 256
+#  endif
 #endif
 
 #define UCS_SYS_FS_SYSTEM_PATH "/sys/devices/system"
