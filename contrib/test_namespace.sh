59c62b8ed1bf6baa009acf13a3bd2d5cd5386635
Shachar Hasson <shasson5@gmail.com>
2026-05-03 17:22:12 +0300

    CONTRIB/CI: Add cuda_ipc to PID namespace test

--- contrib/test_namespace.sh
+++ contrib/test_namespace.sh
@@ -11,17 +11,39 @@
 #  - WORKSPACE           : path to work dir
 #
 
+source $(dirname $0)/../buildlib/az-helpers.sh
 source $(dirname $0)/../buildlib/tools/common.sh
 
 ucx_inst=${WORKSPACE}/install
 
 echo "==== Running namespace tests on $(hostname) ===="
 
+test_namespace_pid() {
+	local tl=$1
+	local mem_type=$2
+	local test_type=$3
+
+	echo "==== Running perftest different PID namespace test for $tl ====" >&2
+
+	cmd="$base_perftest -t $test_type -m $mem_type -p $server_port"
+	config="UCX_PROTO_INFO=y UCX_TLS=$tl,sysv"
+
+    # TODO: remove this once we have a way to test with multiple GPUs
+    config="$config CUDA_VISIBLE_DEVICES=0"
+	
+	unshare_cmd="sudo unshare --mount-proc --pid --fork sudo -u $USER $config $cmd"
+	
+	step_server_port
+	$unshare_cmd &
+	sleep 3
+	$unshare_cmd localhost
+}
+
 test_namespace() {
 	# Make sure to try to use CMA when possible
 	# Expect fallback on SYSV
-	perftest="$ucx_inst/bin/ucx_perftest -t ucp_get -s 9999999 -n 5"
-
+	base_perftest="$ucx_inst/bin/ucx_perftest -s 9999999 -n 5"
+	perftest="$base_perftest -t ucp_get"
 	echo "==== Running perftest namespace positive tests ===="
 
 	for tls in posix cma,sysv
@@ -33,16 +55,17 @@ test_namespace() {
 		unshare --user bash -c "{ $cmd & sleep 3; $cmd localhost; }"
 	done
 
+	test_namespace_pid posix host ucp_get
+	test_namespace_pid cma host ucp_get 
+
+	if [ "X$have_cuda" != "Xno" ] 
+	then
+		output=$(test_namespace_pid cuda_ipc,cuda_copy cuda ucp_put_bw)
+		echo "$output" | grep -q "cuda_ipc"
+	fi
+
 	for tl in posix cma
 	do
-		echo "==== Running perftest different PID namespace test for $tl ===="
-
-		cmd="$perftest -p $server_port"
-		step_server_port
-		sudo unshare --pid --fork sudo -u $USER UCX_TLS=$tl,sysv $cmd &
-		sleep 3
-		sudo unshare --pid --fork sudo -u $USER UCX_TLS=$tl,sysv $cmd localhost
-
 		echo "==== Running perftest different USER namespace test for $tl ===="
 		cmd="$perftest -p $server_port"
 		step_server_port
@@ -60,5 +83,6 @@ test_namespace() {
 }
 
 prepare
+try_load_cuda_env
 build release
 test_namespace
