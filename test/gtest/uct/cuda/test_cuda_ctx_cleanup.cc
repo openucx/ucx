7bcf518ab030df85997f621f3e0b40eb757b4df1
Roie Danino <rdanino@nvidia.com>
2026-05-12 17:37:04 +0300

    TEST/GTEST/UCT/CUDA: simplified tests - rely on tests not crashing rather than complex hook installations affecting the entire gtest binary
    Signed-off-by: Roie Danino <rdanino@nvidia.com>

--- test/gtest/uct/cuda/test_cuda_ctx_cleanup.cc
+++ test/gtest/uct/cuda/test_cuda_ctx_cleanup.cc
@@ -13,160 +13,6 @@ extern "C" {
 
 #include <cuda.h>
 
-#include <atomic>
-#include <cstdlib>
-#include <dlfcn.h>
-
-
-static std::atomic<bool>     g_destroy_audit(false);
-static std::atomic<bool>     g_primary_ctx_audit(false);
-static std::atomic<unsigned> g_event_destroy_count(0);
-static std::atomic<unsigned> g_stream_destroy_count(0);
-static std::atomic<unsigned> g_ctx_push_count(0);
-static std::atomic<unsigned> g_primary_ctx_retain_count(0);
-static std::atomic<unsigned> g_primary_ctx_release_count(0);
-
-template<typename func_t>
-static func_t cuda_real_func(const char *symbol)
-{
-    static void *handle = dlopen("libcuda.so.1", RTLD_NOW | RTLD_LOCAL);
-    void *func;
-
-    if (handle == NULL) {
-        abort();
-    }
-
-    func = dlsym(handle, symbol);
-    if (func == NULL) {
-        abort();
-    }
-
-    return reinterpret_cast<func_t>(func);
-}
-
-extern "C" CUresult cuEventDestroy_v2(CUevent event)
-{
-    typedef CUresult (*func_t)(CUevent);
-
-    if (g_destroy_audit.load(std::memory_order_relaxed)) {
-        ++g_event_destroy_count;
-        return CUDA_SUCCESS;
-    }
-
-    return cuda_real_func<func_t>("cuEventDestroy_v2")(event);
-}
-
-extern "C" CUresult cuStreamDestroy_v2(CUstream stream)
-{
-    typedef CUresult (*func_t)(CUstream);
-
-    if (g_destroy_audit.load(std::memory_order_relaxed)) {
-        ++g_stream_destroy_count;
-        return CUDA_SUCCESS;
-    }
-
-    return cuda_real_func<func_t>("cuStreamDestroy_v2")(stream);
-}
-
-extern "C" CUresult cuCtxPushCurrent(CUcontext ctx)
-{
-    typedef CUresult (*func_t)(CUcontext);
-    CUresult result;
-
-    result = cuda_real_func<func_t>("cuCtxPushCurrent_v2")(ctx);
-    if (g_destroy_audit.load(std::memory_order_relaxed) &&
-        (result == CUDA_SUCCESS)) {
-        ++g_ctx_push_count;
-    }
-
-    return result;
-}
-
-extern "C" CUresult cuDevicePrimaryCtxRetain(CUcontext *ctx, CUdevice dev)
-{
-    typedef CUresult (*func_t)(CUcontext*, CUdevice);
-    CUresult result;
-
-    result = cuda_real_func<func_t>("cuDevicePrimaryCtxRetain")(ctx, dev);
-    if (g_primary_ctx_audit.load(std::memory_order_relaxed) &&
-        (result == CUDA_SUCCESS)) {
-        ++g_primary_ctx_retain_count;
-    }
-
-    return result;
-}
-
-extern "C" CUresult cuDevicePrimaryCtxRelease(CUdevice dev)
-{
-    typedef CUresult (*func_t)(CUdevice);
-    CUresult result;
-
-    result = cuda_real_func<func_t>("cuDevicePrimaryCtxRelease")(dev);
-    if (g_primary_ctx_audit.load(std::memory_order_relaxed) &&
-        (result == CUDA_SUCCESS)) {
-        ++g_primary_ctx_release_count;
-    }
-
-    return result;
-}
-
-
-class scoped_cuda_destroy_audit {
-public:
-    scoped_cuda_destroy_audit()
-    {
-        g_event_destroy_count.store(0, std::memory_order_relaxed);
-        g_stream_destroy_count.store(0, std::memory_order_relaxed);
-        g_ctx_push_count.store(0, std::memory_order_relaxed);
-        g_destroy_audit.store(true, std::memory_order_relaxed);
-    }
-
-    ~scoped_cuda_destroy_audit()
-    {
-        g_destroy_audit.store(false, std::memory_order_relaxed);
-    }
-
-    unsigned event_destroy_count() const
-    {
-        return g_event_destroy_count.load(std::memory_order_relaxed);
-    }
-
-    unsigned stream_destroy_count() const
-    {
-        return g_stream_destroy_count.load(std::memory_order_relaxed);
-    }
-
-    unsigned ctx_push_count() const
-    {
-        return g_ctx_push_count.load(std::memory_order_relaxed);
-    }
-};
-
-class scoped_cuda_primary_ctx_audit {
-public:
-    scoped_cuda_primary_ctx_audit()
-    {
-        g_primary_ctx_retain_count.store(0, std::memory_order_relaxed);
-        g_primary_ctx_release_count.store(0, std::memory_order_relaxed);
-        g_primary_ctx_audit.store(true, std::memory_order_relaxed);
-    }
-
-    ~scoped_cuda_primary_ctx_audit()
-    {
-        g_primary_ctx_audit.store(false, std::memory_order_relaxed);
-    }
-
-    unsigned retain_count() const
-    {
-        return g_primary_ctx_retain_count.load(std::memory_order_relaxed);
-    }
-
-    unsigned release_count() const
-    {
-        return g_primary_ctx_release_count.load(std::memory_order_relaxed);
-    }
-};
-
 
 class test_cuda_ctx_cleanup : public ucs::test {
 protected:
@@ -358,15 +204,11 @@ private:
 
 UCS_TEST_F(test_cuda_ctx_cleanup, retain_primary_ctx_until_rsc_cleanup)
 {
-    scoped_cuda_primary_ctx_audit audit;
-
     create_ctx_rsc();
-    EXPECT_EQ(1u, audit.retain_count());
     EXPECT_EQ(m_cuda_ctx, m_ctx_rsc->primary_ctx);
     EXPECT_EQ(m_device, m_ctx_rsc->cuda_device);
 
     destroy_ctx_rsc();
-    EXPECT_EQ(1u, audit.release_count());
 }
 
 UCS_TEST_F(test_cuda_ctx_cleanup, event_cleanup_after_primary_ctx_release)
@@ -382,11 +224,7 @@ UCS_TEST_F(test_cuda_ctx_cleanup, event_cleanup_after_primary_ctx_release)
 
     release_primary_ctx();
 
-    scoped_cuda_destroy_audit audit;
     ucs_mpool_cleanup(&m_ctx_rsc->event_mp, 1);
-    EXPECT_LT(0u, audit.ctx_push_count());
-    EXPECT_LT(0u, audit.event_destroy_count());
-
     free_ctx_rsc();
 }
 
@@ -401,11 +239,7 @@ UCS_TEST_F(test_cuda_ctx_cleanup, stream_cleanup_after_primary_ctx_release)
 
     release_primary_ctx();
 
-    scoped_cuda_destroy_audit audit;
     uct_cuda_base_queue_desc_destroy(m_ctx_rsc, &qdesc);
-    EXPECT_EQ(1u, audit.ctx_push_count());
-    EXPECT_EQ(1u, audit.stream_destroy_count());
-
     destroy_ctx_rsc();
 }
 
@@ -422,11 +256,7 @@ UCS_TEST_F(test_cuda_ctx_cleanup, event_cleanup_after_primary_ctx_reset)
 
     reset_primary_ctx();
 
-    scoped_cuda_destroy_audit audit;
     ucs_mpool_cleanup(&m_ctx_rsc->event_mp, 1);
-    EXPECT_EQ(0u, audit.ctx_push_count());
-    EXPECT_EQ(0u, audit.event_destroy_count());
-
     free_ctx_rsc();
 }
 
@@ -441,10 +271,6 @@ UCS_TEST_F(test_cuda_ctx_cleanup, stream_cleanup_after_primary_ctx_reset)
 
     reset_primary_ctx();
 
-    scoped_cuda_destroy_audit audit;
     uct_cuda_base_queue_desc_destroy(m_ctx_rsc, &qdesc);
-    EXPECT_EQ(0u, audit.ctx_push_count());
-    EXPECT_EQ(0u, audit.stream_destroy_count());
-
     destroy_ctx_rsc();
 }
