106f9e05e6be20751d722e1ffb00a68231df9b9f
Zihao Zhao <zizhao@nvidia.com>
2026-06-10 06:23:10 +0300

    UCT/PLUGIN: address comments

--- src/uct/ib/mlx5/ib_mlx5_ext.c
+++ src/uct/ib/mlx5/ib_mlx5_ext.c
@@ -11,12 +11,10 @@
 #include "ib_mlx5_ext.h"
 
 #include <ucs/datastruct/list.h>
-#include <ucs/debug/assert.h>
 #include <ucs/debug/log.h>
 #include <ucs/debug/memtrack_int.h>
 #include <ucs/sys/compiler.h>
 #include <ucs/sys/stubs.h>
-#include <ucs/type/spinlock.h>
 
 typedef struct uct_ib_mlx5_ext_plugin {
     ucs_list_link_t       list;
@@ -24,9 +22,6 @@ typedef struct uct_ib_mlx5_ext_plugin {
 } uct_ib_mlx5_ext_plugin_t;
 
 UCS_LIST_HEAD(uct_ib_mlx5_ext_plugins);
-
-static ucs_spinlock_t uct_ib_mlx5_ext_lock;
-static int            uct_ib_mlx5_ext_initialized;
 
 static ucs_status_t
 uct_ib_mlx5_ext_iface_query_default(uct_ib_mlx5_ext_iface_query_attr_t *attr)
@@ -96,20 +91,15 @@ uct_ib_mlx5_ext_iface_query(uct_iface_h iface,
         return UCS_ERR_INVALID_PARAM;
     }
 
-    ucs_spin_lock(&uct_ib_mlx5_ext_lock);
-
     ucs_list_for_each(plugin, &uct_ib_mlx5_ext_plugins, list) {
         if (ucs_unlikely(uct_ib_mlx5_ext_is_unsupported_op(
                     (const void*)plugin->ops.iface_query))) {
             continue;
         }
 
-        status = plugin->ops.iface_query(iface, attr);
-        ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
-        return status;
+        return plugin->ops.iface_query(iface, attr);
     }
 
-    ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
     return uct_ib_mlx5_ext_iface_query_default(attr);
 }
 
@@ -129,59 +119,30 @@ ucs_status_t uct_ib_mlx5_ext_qp_query(struct ibv_qp *qp,
         return status;
     }
 
-    ucs_spin_lock(&uct_ib_mlx5_ext_lock);
-
     ucs_list_for_each(plugin, &uct_ib_mlx5_ext_plugins, list) {
         if (ucs_unlikely(uct_ib_mlx5_ext_is_unsupported_op(
                     (const void*)plugin->ops.qp_query))) {
             continue;
         }
 
-        status = plugin->ops.qp_query(qp, devx_obj, attr);
-        ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
-        return status;
+        return plugin->ops.qp_query(qp, devx_obj, attr);
     }
 
-    ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
     return UCS_ERR_UNSUPPORTED;
 }
 
 void uct_ib_mlx5_ext_init(void)
 {
-    ucs_status_t status;
-
-    if (uct_ib_mlx5_ext_initialized) {
-        return;
-    }
-
-    status = ucs_spinlock_init(&uct_ib_mlx5_ext_lock, 0);
-    if (status != UCS_OK) {
-        ucs_fatal("failed to initialize mlx5 ext lock: %s",
-                  ucs_status_string(status));
-    }
-
-    uct_ib_mlx5_ext_initialized = 1;
 }
 
 void uct_ib_mlx5_ext_cleanup(void)
 {
     uct_ib_mlx5_ext_plugin_t *plugin, *tmp;
 
-    if (!uct_ib_mlx5_ext_initialized) {
-        return;
-    }
-
-    ucs_spin_lock(&uct_ib_mlx5_ext_lock);
-
     ucs_list_for_each_safe(plugin, tmp, &uct_ib_mlx5_ext_plugins, list) {
         ucs_list_del(&plugin->list);
         ucs_free(plugin);
     }
-
-    ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
-
-    ucs_spinlock_destroy(&uct_ib_mlx5_ext_lock);
-    uct_ib_mlx5_ext_initialized = 0;
 }
 
 ucs_status_t uct_ib_mlx5_ext_register(const uct_ib_mlx5_ext_ops_t *ops)
@@ -204,10 +165,8 @@ ucs_status_t uct_ib_mlx5_ext_register(const uct_ib_mlx5_ext_ops_t *ops)
     plugin->ops                                  = *ops;
     plugin->ops.name[UCT_COMPONENT_NAME_MAX - 1] = '\0';
 
-    ucs_spin_lock(&uct_ib_mlx5_ext_lock);
     ucs_list_add_tail(&uct_ib_mlx5_ext_plugins, &plugin->list);
     num_plugins = ucs_list_length(&uct_ib_mlx5_ext_plugins);
-    ucs_spin_unlock(&uct_ib_mlx5_ext_lock);
 
     ucs_debug("ib mlx5 ext: registered plugin name=%s iface_query=%s "
               "qp_query=%s (total=%u)",
