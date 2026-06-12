9353ebc232bc0e91dd2d1e0e43934f9ce8619c92
Thomas Vegas <tvegas@nvidia.com>
2026-06-12 17:45:44 +0300

    UCP/PROTO: Fix coverity

--- src/ucp/proto/proto_select.c
+++ src/ucp/proto/proto_select.c
@@ -161,6 +161,10 @@ static ucs_status_t ucp_proto_thresholds_next_range(
     UCS_DYNAMIC_BITMAP_FOR_EACH_BIT(proto_idx, proto_mask) {
         proto = &ucs_array_elem(&proto_init->protocols, proto_idx);
         range = ucp_proto_flat_perf_find_lb(proto->flat_perf, msg_length);
+        if ((range == NULL) || (msg_length < range->start)) {
+            status = UCS_ERR_UNSUPPORTED;
+            goto out_unindent;
+        }
 
         *ucs_array_append(perf_list, status = UCS_ERR_NO_MEMORY;
                           goto out_unindent) = range->value;
