9a1e16bd2806194f72aa7c173116f86b69dda739
Michal Shalev <mshalev@nvidia.com>
2026-07-21 10:24:31 -0700

    TOOLS/PERF: Add SGL put tests to ucx_perftest config and CI

--- contrib/test_jenkins.sh
+++ contrib/test_jenkins.sh
@@ -672,6 +672,34 @@ run_ucx_perftest_cuda_device() {
 }
 
 #
+# Run UCX scatter-gather list (SGL) performance test
+#
+run_ucx_perftest_sgl() {
+	ucx_inst_ptest=$ucx_inst/share/ucx/perftest
+	ucx_perftest="$ucx_inst/bin/ucx_perftest"
+
+	if [ "X$have_cuda" != "Xno" ] && [ "$(get_num_gpus)" -gt 0 ]; then
+		cat $ucx_inst_ptest/test_types_ucp_sgl | \
+			sort -R > $ucx_inst_ptest/test_types_sgl_ucp
+	else
+		echo "==== CUDA not available, running host-only SGL tests ===="
+		cat $ucx_inst_ptest/test_types_ucp_sgl | grep -v cuda | \
+			sort -R > $ucx_inst_ptest/test_types_sgl_ucp
+	fi
+
+	ucp_test_args="-b $ucx_inst_ptest/test_types_sgl_ucp -w 1"
+
+	my_devices=$(get_my_tasks $(get_active_ib_devices))
+	for ucx_dev in $my_devices
+	do
+		echo "==== Running ucx_perftest SGL on $ucx_dev ===="
+		export UCX_NET_DEVICES=$ucx_dev
+		run_client_server_app "$ucx_perftest" "$ucp_test_args" "$(hostname)" 0 0
+		unset UCX_NET_DEVICES
+	done
+}
+
+#
 # Test malloc hooks with mpi
 #
 test_malloc_hooks_mpi() {
@@ -1262,6 +1290,7 @@ run_tests() {
 	do_distributed_task 0 4 test_no_cuda_context
 	do_distributed_task 1 4 run_ucx_perftest_with_daemon
 	do_distributed_task 1 4 run_ucx_perftest_cuda_device
+	do_distributed_task 2 4 run_ucx_perftest_sgl
 
 	# long devel tests
 	do_distributed_task 0 4 run_ucp_hello
