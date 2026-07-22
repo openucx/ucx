8294b60cd55c37d056177a11a796e6352dbb8eac
Roie Danino <rdanino@nvidia.com>
2026-07-22 12:12:00 +0300

    UCT/IB/MLX5: Preserve existing fence behavior
    Signed-off-by: Roie Danino <rdanino@nvidia.com>

--- test/gtest/uct/ib/test_rc.cc
+++ test/gtest/uct/ib/test_rc.cc
@@ -1142,7 +1142,7 @@ UCS_TEST_P(test_gga_fence_flags, auto_ooo, "GGA_MLX5_AR_ENABLE=auto",
 {
     ASSERT_UCS_OK(open_iface(UCT_IB_MLX5_DP_ORDERING_OOO_RW, false, false));
     check_enabled(UCT_IB_MLX5_DP_ORDERING_OOO_RW,
-                  UCT_IB_MLX5_WQE_CTRL_FLAG_FENCE);
+                  UCT_IB_MLX5_WQE_CTRL_FLAG_STRONG_ORDER);
 }
 
 UCS_TEST_P(test_gga_fence_flags, none_ooo, "GGA_MLX5_AR_ENABLE=auto",
