ed9b5f722039d000d99b7cc9adcd4ea80ac18472
Zihao Zhao <zizhao@nvidia.com>
2026-08-10 20:59:39 +0300

    UCT/IB/MLX5: address comments

--- src/uct/ib/mlx5/rc/rc_mlx5.h
+++ src/uct/ib/mlx5/rc/rc_mlx5.h
@@ -279,6 +279,8 @@ ucs_status_t uct_rc_mlx5_base_ep_query(uct_ep_h tl_ep, uct_ep_attr_t *ep_attr);
 ucs_status_t uct_rc_mlx5_ep_outstanding_purge(
         uct_ep_h tl_ep, const uct_ep_outstanding_purge_params_t *params);
 
+void uct_rc_mlx5_ep_update_tx_res(uct_ep_h tl_ep);
+
 unsigned uct_rc_mlx5_ep_cleanup_qp(void *arg);
 
 ucs_status_t uct_rc_mlx5_iface_event_fd_get(uct_iface_h tl_iface, int *fd_p);
