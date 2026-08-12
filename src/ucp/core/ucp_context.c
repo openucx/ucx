5e277988336175184be95a52e0bbdaf1cd29de3d
Guy Ealey Morag <gealeymorag@nvidia.com>
2026-08-12 13:24:56 +0000

    UCP/CORE: Refactor to avoid repetitions
    Signed-off-by: Guy Ealey Morag <gealeymorag@nvidia.com>

--- src/ucp/core/ucp_context.c
+++ src/ucp/core/ucp_context.c
@@ -2728,15 +2728,16 @@ ucp_version_check(unsigned api_major_version, unsigned api_minor_version)
     }
 
     if (ucs_log_is_enabled(log_level)) {
+        ucs_string_buffer_appendf(&strb, " (");
+
         ret = dladdr(ucp_init_version, &dl_info);
         if (ret != 0) {
-            ucs_string_buffer_appendf(
-                    &strb, " (loaded from %s, git branch %s, revision %s)",
-                    dl_info.dli_fname, UCT_SCM_BRANCH, UCT_SCM_VERSION);
-        } else {
-            ucs_string_buffer_appendf(&strb, " (git branch %s, revision %s)",
-                                      UCT_SCM_BRANCH, UCT_SCM_VERSION);
+            ucs_string_buffer_appendf(&strb, "loaded from %s, ",
+                                      dl_info.dli_fname);
         }
+
+        ucs_string_buffer_appendf(&strb, "git branch %s, revision %s)",
+                                  UCT_SCM_BRANCH, UCT_SCM_VERSION);
         ucs_log(log_level, "%s", ucs_string_buffer_cstr(&strb));
     }
 
