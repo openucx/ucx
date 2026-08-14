62c564aa1c016952908e5dbb1eae09ac596992b9
Evgeny Leksikov <evgenylek@nvidia.com>
2026-08-14 15:57:14 +0300

    UCP/FT/RMA: do not enable put/am/bcopy under failover
    Avoid selecting software-emulated put/am/bcopy on failover EPs so PUT stays on offload paths instead of aborting with Request canceled.

--- src/ucp/rma/put_am.c
+++ src/ucp/rma/put_am.c
@@ -116,18 +116,6 @@ ucp_proto_put_am_bcopy_probe(const ucp_proto_init_params_t *init_params)
         .opt_align_offs      = UCP_PROTO_COMMON_OFFSET_INVALID
     };
 
-    if (init_params->ep_config_key->err_mode ==
-        UCP_ERR_HANDLING_MODE_FAILOVER) {
-        if (init_params->ep_config_key->dst_version <
-            UCP_WIREUP_LANE_STATE_MIN_VERSION) {
-            return;
-        }
-
-        params.super.flags           |= UCP_PROTO_COMMON_INIT_FLAG_FAILOVER;
-        params.first.tl_v2_cap_flags  = UCT_IFACE_FLAG_V2_QUERY_TOKEN;
-        params.middle.tl_v2_cap_flags = UCT_IFACE_FLAG_V2_QUERY_TOKEN;
-    }
-
     if (!ucp_proto_init_check_op(init_params, UCS_BIT(UCP_OP_ID_PUT))) {
         return;
     }
