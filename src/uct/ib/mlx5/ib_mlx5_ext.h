757166d55eaed54163e077179e1a34eb6db6911f
Michal Shalev <mshalev@nvidia.com>
2026-08-16 07:36:01 -0700

    UCT/IB/MLX5: Append GET SGL zcopy ext ops at end of ops struct

--- src/uct/ib/mlx5/ib_mlx5_ext.h
+++ src/uct/ib/mlx5/ib_mlx5_ext.h
@@ -165,9 +165,9 @@ typedef struct uct_ib_mlx5_ext_ops {
     uct_ib_mlx5_ext_ep_query_func_t                ep_query;                     /**< EP query callback */
     uct_ib_mlx5_ext_max_put_sgl_zcopy_count_func_t max_put_sgl_zcopy_count;      /**< Maximum PUT SGL zero-copy entry count callback */
     uct_ep_put_sgl_zcopy_func_t                    ep_put_sgl_zcopy;             /**< PUT SGL zero-copy callback */
+    uct_ep_outstanding_purge_func_t                ep_outstanding_purge;         /**< Outstanding operation purge callback */
     uct_ib_mlx5_ext_max_get_sgl_zcopy_count_func_t max_get_sgl_zcopy_count;      /**< Maximum GET SGL zero-copy entry count callback */
     uct_ep_get_sgl_zcopy_func_t                    ep_get_sgl_zcopy;             /**< GET SGL zero-copy callback */
-    uct_ep_outstanding_purge_func_t                ep_outstanding_purge;         /**< Outstanding operation purge callback */
 } uct_ib_mlx5_ext_ops_t;
 
 /**
