5a6bbb45d6eba66f960a4909cd14476c686b588e
Guy Ealey Morag <gealeymorag@nvidia.com>
2026-08-24 18:30:34 +0000

    UCS/TOPO: Add missing doc
    Signed-off-by: Guy Ealey Morag <gealeymorag@nvidia.com>

--- src/ucs/sys/topo/base/topo_groups.h
+++ src/ucs/sys/topo/base/topo_groups.h
@@ -98,6 +98,16 @@ ucs_status_t ucs_topo_init_groups(ucs_topo_groups_t *groups_p);
 void ucs_topo_release_groups(ucs_topo_groups_t *groups);
 
 
+/**
+ * Initialize topology groups.
+ *
+ * @param [in]  devices      Array of registered system devices.
+ * @param [in]  num_devices  Number of elements in @a devices.
+ * @param [out] groups_p     Initialized topology groups.
+ *
+ * @return UCS_OK on success, or an error status if topology group
+ *         initialization failed.
+ */
 ucs_status_t
 ucs_topo_init_groups_inner(const ucs_topo_sys_device_info_t *devices,
                            unsigned num_devices, ucs_topo_groups_t *groups_p);
