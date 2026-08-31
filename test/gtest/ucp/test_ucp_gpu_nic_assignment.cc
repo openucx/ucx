c8fa34b843e5fdf44c879fd06bf30da123e890af
Guy Ealey Morag <gealeymorag@nvidia.com>
2026-08-31 09:45:01 +0000

    TEST/UCP: Cleanup gpu nic assignment tests
    Signed-off-by: Guy Ealey Morag <gealeymorag@nvidia.com>

--- test/gtest/ucp/test_ucp_gpu_nic_assignment.cc
+++ test/gtest/ucp/test_ucp_gpu_nic_assignment.cc
@@ -12,37 +12,36 @@
 
 class test_ucp_gpu_nic : public ucs::test {
 public:
-    test_ucp_gpu_nic() : m_assignment_initialized(false)
+    test_ucp_gpu_nic() :
+        m_groups_initialized(false), m_assignment_initialized(false)
     {
-    }
-
-    virtual void init()
-    {
-        ucs::test::init();
-
-        m_groups.type = UCS_TOPO_GROUPS_TYPE_VERA_RUBIN;
-        ucs_array_init_dynamic(&m_groups.groups);
     }
 
     virtual void cleanup()
     {
         if (m_assignment_initialized) {
             ucp_gpu_nic_assignment_release(&m_assignment);
+            m_assignment_initialized = false;
         }
 
-        ucs_topo_release_groups(&m_groups);
+        if (m_groups_initialized) {
+            ucs_topo_release_groups(&m_groups);
+            m_groups_initialized = false;
+        }
+
         ucs::test::cleanup();
     }
 
 protected:
     struct topology_shape_t {
-        const size_t num_groups;
-        const size_t num_gpus_per_group;
-        const size_t num_nics_per_group;
-        const size_t num_gpu_devices;
-        const size_t num_nic_ports;
+        ucs_topo_groups_type_t type;
+        size_t                 num_groups;
+        size_t                 num_gpus_per_group;
+        size_t                 num_nics_per_group;
+        size_t                 num_gpu_devices;
+        size_t                 num_nic_ports;
 
-        size_t       num_gpus() const noexcept
+        size_t                 num_gpus() const noexcept
         {
             return num_groups * num_gpus_per_group;
         }
@@ -75,20 +74,30 @@ protected:
                                              port_idx);
     }
 
-    ucs_status_t build_groups(const topology_shape_t &config)
+    void build_groups(const topology_shape_t &config)
     {
         ucs_topo_group_t *group;
         ucs_topo_gpu_t *gpu;
         ucs_topo_nic_t *nic;
 
+        if (m_groups_initialized) {
+            ucs_topo_release_groups(&m_groups);
+            m_groups_initialized = false;
+        }
+
+        ucs_array_init_dynamic(&m_groups.groups);
+        m_groups.type        = config.type;
+        m_groups_initialized = true;
+
         for (size_t group_idx = 0; group_idx < config.num_groups; ++group_idx) {
             group = ucs_array_append(&m_groups.groups,
-                                     return UCS_ERR_NO_MEMORY);
+                                     FAIL() << "Failed to append group");
             ucs_topo_init_group(group);
 
             for (size_t local_idx = 0; local_idx < config.num_gpus_per_group;
                  ++local_idx) {
-                gpu = ucs_array_append(&group->gpus, return UCS_ERR_NO_MEMORY);
+                gpu = ucs_array_append(&group->gpus,
+                                       FAIL() << "Failed to append GPU");
                 std::memset(gpu, 0, sizeof(*gpu));
                 const size_t gpu_idx = (group_idx * config.num_gpus_per_group) +
                                        local_idx;
@@ -104,7 +113,8 @@ protected:
 
             for (size_t local_idx = 0; local_idx < config.num_nics_per_group;
                  ++local_idx) {
-                nic = ucs_array_append(&group->nics, return UCS_ERR_NO_MEMORY);
+                nic = ucs_array_append(&group->nics,
+                                       FAIL() << "Failed to append NIC");
                 std::memset(nic, 0, sizeof(*nic));
                 const size_t nic_idx = (group_idx * config.num_nics_per_group) +
                                        local_idx;
@@ -117,20 +127,20 @@ protected:
                 }
             }
         }
-
-        return UCS_OK;
     }
 
-    ucs_status_t build_assignment(ucp_gpu_nic_policy_t policy)
+    void build_assignment(ucp_gpu_nic_policy_t policy)
     {
         ucs_status_t status;
 
-        status = ucp_gpu_nic_assignment_build(&m_groups, policy, &m_assignment);
-        if (status == UCS_OK) {
-            m_assignment_initialized = true;
+        if (m_assignment_initialized) {
+            ucp_gpu_nic_assignment_release(&m_assignment);
+            m_assignment_initialized = false;
         }
 
-        return status;
+        status = ucp_gpu_nic_assignment_build(&m_groups, policy, &m_assignment);
+        ASSERT_UCS_OK(status);
+        m_assignment_initialized = true;
     }
 
     void check_nic_owners(const topology_shape_t &config,
@@ -189,46 +199,66 @@ protected:
                           ucp_gpu_nic_policy_t policy,
                           const std::vector<size_t> &expected_owners)
     {
+        ASSERT_NE(config.num_groups, 0);
+        ASSERT_NE(config.num_gpus_per_group, 0);
+        ASSERT_NE(config.num_nics_per_group, 0);
+        ASSERT_NE(config.num_nic_ports, 0);
+        ASSERT_NE(config.num_gpu_devices, 0);
+
         ASSERT_LE(config.num_gpu_devices, UCS_TOPO_MAX_DEVICES_PER_GPU);
         ASSERT_LE(config.num_nic_ports, UCS_TOPO_MAX_PORTS_PER_NIC);
+
         ASSERT_LE(config.first_nic_port_sys_dev() +
                           (config.num_nics() * config.num_nic_ports),
                   UCS_SYS_DEVICE_ID_COUNT);
         ASSERT_EQ(config.num_nics(), expected_owners.size());
+
         for (size_t owner_idx = 0; owner_idx < expected_owners.size();
              ++owner_idx) {
             ASSERT_LT(expected_owners[owner_idx], config.num_gpus());
         }
 
-        ASSERT_UCS_OK(build_groups(config));
-        ASSERT_UCS_OK(build_assignment(policy));
+        build_groups(config);
+        build_assignment(policy);
         check_nic_owners(config, expected_owners);
     }
 
-    void check_vera_assignment(ucp_gpu_nic_policy_t policy,
-                               const std::vector<size_t> &expected_owners)
+    void check_vera_rubin_assignment(ucp_gpu_nic_policy_t policy,
+                                     const std::vector<size_t> &expected_owners)
     {
-        const topology_shape_t config = {
-            2, // num_groups
-            2, // num_gpus_per_group
-            4, // num_nics_per_group
-            2, // num_gpu_devices
-            2, // num_nic_ports
-        };
+        topology_shape_t config;
 
-        check_assignment(config, policy, expected_owners);
+        /* Vera-Rubin topology is built of groups, each with 2 GPUs and 4 NICs 
+         * that are equally distant from each other. */
+        config.type               = UCS_TOPO_GROUPS_TYPE_VERA_RUBIN;
+        config.num_groups         = 2;
+        config.num_gpus_per_group = 2;
+        config.num_nics_per_group = 4;
+
+        /* Each GPU may appear as two devices if MPS MLOPart is enabled. */
+        for (size_t num_gpu_devices = 1; num_gpu_devices <= 2; ++num_gpu_devices) {
+            /* Each NIC may appear as two ports if dual-port mode is enabled. */
+            for (size_t num_nic_ports = 1; num_nic_ports <= 2; ++num_nic_ports) {
+                config.num_gpu_devices = num_gpu_devices;
+                config.num_nic_ports   = num_nic_ports;
+                check_assignment(config, policy, expected_owners);
+            }
+        }
     }
 
 private:
     ucs_topo_groups_t m_groups;
     ucp_gpu_nic_assignment_t m_assignment;
+    bool m_groups_initialized;
     bool m_assignment_initialized;
 };
 
 UCS_TEST_F(test_ucp_gpu_nic, vera_flip) {
-    check_vera_assignment(UCP_GPU_NIC_POLICY_FLIP, {0, 1, 1, 0, 2, 3, 3, 2});
+    check_vera_rubin_assignment(UCP_GPU_NIC_POLICY_FLIP,
+                                {0, 1, 1, 0, 2, 3, 3, 2});
 }
 
 UCS_TEST_F(test_ucp_gpu_nic, vera_alt) {
-    check_vera_assignment(UCP_GPU_NIC_POLICY_ALT, {0, 1, 0, 1, 2, 3, 2, 3});
+    check_vera_rubin_assignment(UCP_GPU_NIC_POLICY_ALT,
+                                {0, 1, 0, 1, 2, 3, 2, 3});
 }
