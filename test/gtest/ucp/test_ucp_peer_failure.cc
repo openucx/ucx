fe8d44745d18ef40eb2a43df77a506d6fe6b54be
Peter Andreas Entschev <peter@entschev.com>
2026-09-02 02:21:21 -0700

    UCP/TEST: Restore invalidation assertion

--- test/gtest/ucp/test_ucp_peer_failure.cc
+++ test/gtest/ucp/test_ucp_peer_failure.cc
@@ -540,9 +540,7 @@ void test_ucp_peer_failure_inval::test_invalidate_rma_bw_lane(
     }
 
     if (force_invalidate_rma) {
-        if (!(config->key.flags & UCP_EP_CONFIG_KEY_FLAG_SELF)) {
-            EXPECT_FALSE(has_no_invalidate_rma);
-        }
+        ASSERT_FALSE(has_no_invalidate_rma);
     } else {
         ASSERT_TRUE(!has_transport("tcp") || has_no_invalidate_rma);
         ASSERT_TRUE(!has_transport("rc_x") || has_invalidate_rma);
