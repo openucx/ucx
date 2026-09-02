d8ab57c7837ebc175fc91602fa46088fc9b878df
Zihao Zhao <zizhao@nvidia.com>
2026-09-02 14:32:35 +0300

    UCT/API: purge cb doc

--- src/uct/api/v2/uct_v2.h
+++ src/uct/api/v2/uct_v2.h
@@ -1916,6 +1916,13 @@ typedef struct uct_ep_op_info {
 /**
  * @ingroup UCT_RESOURCE
  * @brief Callback invoked for each undelivered outstanding operation.
+ *
+ * @param [in] op_info  Descriptor of the undelivered operation. The descriptor
+ *                      is valid only for the duration of the callback. The user
+ *                      must copy it, and any callback-lifetime data it
+ *                      references, if they are needed after the callback
+ *                      returns.
+ * @param [in] arg      User-defined argument.
  */
 typedef void (*uct_ep_outstanding_purge_callback_t)(
         const uct_ep_op_info_t *op_info, void *arg);
