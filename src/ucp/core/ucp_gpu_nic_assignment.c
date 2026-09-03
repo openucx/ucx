e15db1894380d25889843d3a40108bfd1e2c109b
Guy Ealey Morag <gealeymorag@nvidia.com>
2026-09-03 18:05:25 +0000

    UCP/CORE: Add group assignment logs
    Signed-off-by: Guy Ealey Morag <gealeymorag@nvidia.com>

--- src/ucp/core/ucp_gpu_nic_assignment.c
+++ src/ucp/core/ucp_gpu_nic_assignment.c
@@ -10,6 +10,7 @@
 
 #include "ucp_gpu_nic_assignment.h"
 
+#include <ucs/datastruct/string_buffer.h>
 #include <ucs/debug/assert.h>
 #include <ucs/debug/log.h>
 #include <ucs/debug/memtrack_int.h>
@@ -52,6 +53,63 @@ ucp_gpu_nic_bitmap_add_nic(ucp_gpu_nic_sys_dev_bitmap_t *nic_sys_dev_bitmap,
         ucs_assert(*port != UCS_SYS_DEVICE_ID_UNKNOWN);
         UCS_STATIC_BITMAP_SET(nic_sys_dev_bitmap, *port);
     }
+}
+
+/* Log the assignment for a group.
+ * For example:
+ *  `group #0 (2 gpus, 4 nics) assignment: [ 0, 1, 1, 0 ]`
+ *  `group #1 (2 gpus, 4 nics) assignment: [ 0/1, X, X, 0/1 ]`
+ */
+static void
+ucp_gpu_nic_assignment_log_group(const ucp_gpu_nic_assignment_t *assignment,
+                                 const ucs_topo_group_t *group,
+                                 size_t base_bitmap_idx, size_t group_idx)
+{
+    ucs_string_buffer_t strb = UCS_STRING_BUFFER_INITIALIZER;
+    size_t num_gpus          = ucs_array_length(&group->gpus);
+    size_t num_nics          = ucs_array_length(&group->nics);
+    const ucp_gpu_nic_sys_dev_bitmap_t *nic_sys_dev_bitmap;
+    const ucs_topo_nic_t *nic;
+    size_t nic_idx;
+    size_t gpu_idx;
+    int has_owner;
+
+    if (!ucs_log_is_enabled(UCS_LOG_LEVEL_DEBUG)) {
+        return;
+    }
+
+    ucs_string_buffer_appendf(&strb, "[");
+    for (nic_idx = 0; nic_idx < num_nics; ++nic_idx) {
+        nic = &ucs_array_elem(&group->nics, nic_idx);
+
+        if (nic_idx == 0) {
+            ucs_string_buffer_appendf(&strb, " ");
+        } else {
+            ucs_string_buffer_appendf(&strb, ", ");
+        }
+
+        has_owner = 0;
+        for (gpu_idx = 0; gpu_idx < num_gpus; ++gpu_idx) {
+            nic_sys_dev_bitmap =
+                    &assignment->nic_sys_dev_bitmaps[base_bitmap_idx + gpu_idx];
+            if (!ucp_gpu_nic_bitmap_test(nic_sys_dev_bitmap, nic->ports[0])) {
+                continue;
+            }
+
+            ucs_string_buffer_appendf(&strb, "%s%zu", has_owner ? "/" : "",
+                                      gpu_idx);
+            has_owner = 1;
+        }
+
+        if (!has_owner) {
+            ucs_string_buffer_appendf(&strb, "X");
+        }
+    }
+    ucs_string_buffer_appendf(&strb, " ]");
+
+    ucs_debug("group #%zu (%zu gpus, %zu nics) assignment: %s", group_idx,
+              num_gpus, num_nics, ucs_string_buffer_cstr(&strb));
+    ucs_string_buffer_cleanup(&strb);
 }
 
 static void ucp_gpu_nic_assignment_init(ucp_gpu_nic_assignment_t *assignment)
@@ -154,6 +212,9 @@ ucp_gpu_nic_assignment_add_group(ucp_gpu_nic_assignment_t *assignment,
 
         ucp_gpu_nic_assignment_map_gpu(assignment, gpu, bitmap_idx);
     }
+
+    ucp_gpu_nic_assignment_log_group(assignment, group, base_bitmap_idx,
+                                     group_idx);
 }
 
 ucs_status_t
