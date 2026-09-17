70f2a73ca25b75248d614c831253dfd5051a52d9
Guy Ealey Morag <gealeymorag@nvidia.com>
2026-09-17 08:00:03 +0000

    UCT/CUDA: Use num_invisible_gpus for numbering
    Signed-off-by: Guy Ealey Morag <gealeymorag@nvidia.com>

--- src/uct/cuda/base/cuda_util.c
+++ src/uct/cuda/base/cuda_util.c
@@ -221,6 +221,7 @@ static ucs_status_t
 uct_cuda_init_devices_nvml(const ucs_sys_bus_id_t *visible_gpu_bus_ids,
                            unsigned num_visible_gpus)
 {
+    unsigned num_invisible_gpus = 0;
     unsigned nvml_dev_count, i;
     ucs_sys_device_t sys_dev;
     ucs_sys_bus_id_t bus_id;
@@ -264,7 +265,8 @@ uct_cuda_init_devices_nvml(const ucs_sys_bus_id_t *visible_gpu_bus_ids,
             goto out;
         }
 
-        ucs_snprintf_safe(device_name, sizeof(device_name), "UNKN%u", i);
+        ucs_snprintf_safe(device_name, sizeof(device_name), "UNKN%u",
+                          num_invisible_gpus++);
         status = ucs_topo_sys_device_set_name(
                 sys_dev, device_name, UCT_CUDA_SYS_DEVICE_NAME_PRIORITY);
         if (status != UCS_OK) {
