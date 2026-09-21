1a940aa7dc3e9f319afd7f1bcf27728d6792ebf9
Manu Shantharam <manu.shantharam@amd.com>
2026-09-21 21:04:33 +0000

    UCT/ROCM_IPC: Advertise peer failure error handling capability

--- src/uct/rocm/ipc/rocm_ipc_iface.c
+++ src/uct/rocm/ipc/rocm_ipc_iface.c
@@ -121,7 +121,8 @@ static ucs_status_t uct_rocm_ipc_iface_query(uct_iface_h tl_iface,
     iface_attr->device_addr_len         = sizeof(uint64_t);
     iface_attr->ep_addr_len             = 0;
     iface_attr->max_conn_priv           = 0;
-    iface_attr->cap.flags               = UCT_IFACE_FLAG_GET_ZCOPY |
+    iface_attr->cap.flags               = UCT_IFACE_FLAG_ERRHANDLE_PEER_FAILURE |
+                                          UCT_IFACE_FLAG_GET_ZCOPY |
                                           UCT_IFACE_FLAG_PUT_ZCOPY | UCT_IFACE_FLAG_PENDING |
                                           UCT_IFACE_FLAG_CONNECT_TO_IFACE |
                                           UCT_IFACE_FLAG_DEVICE_EP;
